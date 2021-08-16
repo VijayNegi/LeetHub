2828a79a5c1646de1c495263259b7949abba945b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-08-16 15:51:20 +0530

    Time: 24 ms (57.48%), Space: 17.2 MB (31.47%) - LeetHub

--- range-sum-query-immutable/range-sum-query-immutable.cpp
+++ range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,4 +1,4 @@
-class NumArray {
+/*class NumArray {
     vector<int> comm;
 public:
     NumArray(vector<int>& nums) {
@@ -14,6 +14,45 @@ public:
     int sumRange(int left, int right) {
         
         return comm[right+1] - comm[left];
+    }
+};*/
+// BIT based solution
+class NumArray {
+    vector<int> bit;
+    int MaxIdx;
+public:
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        MaxIdx = n +1;
+        bit.assign(n+1,0);
+        
+        for(int i=0;i<n;++i)
+        {
+            update(i+1,nums[i]);
+        }
+    }
+    void update( int idx, int val)
+    {
+        while(idx<MaxIdx)
+        {
+            bit[idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+    int read(int idx)
+    {
+        int sum = 0;
+        while(idx>0)
+        {
+            sum += bit[idx];
+            idx -=(idx & -idx);
+        }
+        return sum;
+    }
+    
+    int sumRange(int left, int right) {
+        
+        return read(right+1) - read(left);
     }
 };
 
