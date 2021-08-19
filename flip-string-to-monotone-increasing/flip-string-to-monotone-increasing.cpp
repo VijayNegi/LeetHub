8e1e4947f199bdf01ef1a63f11bd1731af0e9997
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-08-19 10:48:22 +0530

    Time: 28 ms (64.19%), Space: 11.1 MB (72.30%) - LeetHub

--- flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,40 +1,69 @@
 class Solution {
 public:
-    int minFlipsMonoIncr(string s) {
+    int minFlipsMonoIncr1(string s) {
         int n = s.size();
         vector<int> comm0(n+1,0);
         vector<int> comm1(n+1,0);
         
-        for(int i=0;i<n;++i)
+        if(s[0]=='0')
         {
-            if(i>0)
-            {
-                comm0[i] = comm0[i-1];
-                comm1[i] = comm1[i-1];
-            }
+            comm0[0]++;
+        }
+        else
+            comm1[0]++;
+        
+        for(int i=1;i<n;++i)
+        {
+
+            comm0[i] = comm0[i-1];
+            comm1[i] = comm1[i-1];
             if(s[i]=='0')
-            {
                 comm0[i]++;
-            }
             else
                 comm1[i]++;
         }
         int minF = n;
-        for(int i=0 ; i<n; ++i)
+        minF = min(minF, comm0[n-1]);
+        for(int i=1 ; i<n; ++i)
         {
-            //cout<<" i="<<i<<endl;
-            if(i==0)
-            {
-                minF = min(minF, comm0[n-1]); // flip all zero after it;
-            }
-            else
-            {
-                minF = min(minF, comm1[i-1] + comm0[n-1]- comm0[i-1]);
-            }
-            //else if(i==n-1)
-                minF = min(minF, comm1[n-1]); // flip all ones before it;
-            //cout<<" i="<<i<<" min="<<minF<<endl;
+            minF = min(minF, comm1[i-1] + comm0[n-1]- comm0[i-1]);
         }
+        minF = min(minF, comm1[n-1]); 
         return minF;
+    }
+    int minFlipsMonoIncr2(string s) {
+        int n = s.size();
+        vector<int> comm1(n+1,0);
+        
+        if(s[0]=='1')
+            comm1[0]++;
+        
+        for(int i=1;i<n;++i)
+        {
+            comm1[i] = comm1[i-1];
+            if(s[i]=='1')
+                comm1[i]++;
+        }
+        int minF = n;
+        minF = min(minF, n - comm1[n-1]); // flip all zeros after
+        for(int i=1 ; i<n; ++i)
+        {
+            minF = min(minF, comm1[i-1] +  ((n-i) - (comm1[n-1] - comm1[i-1])));
+        }
+        minF = min(minF, comm1[n-1]); // flip all ones before
+        return minF;
+    }
+        int minFlipsMonoIncr(string S ) 
+        {
+            int counter_one  = 0,counter_flip = 0;
+            for (auto ch : S) {
+            if (ch == '1') {
+                ++counter_one;
+            } else {
+                ++counter_flip;
+            }
+            counter_flip = std::min(counter_one, counter_flip);
+        }
+        return counter_flip;
     }
 };
