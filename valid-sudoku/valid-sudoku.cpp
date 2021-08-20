e43ff81e2e67be4b41cd4d15f26be8d7fa7f746c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-08-20 23:35:26 +0530

    Time: 20 ms (76.48%), Space: 17.9 MB (88.38%) - LeetHub

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
+    //BitMasking
     bool isValidSudoku(vector<vector<char>>& board) {
-        //vector<vector<int>> m(9,vector<int>(9,0));
+        int cols[9] =  {0};
+        int rows[9] = {0};
+        int boxes[9] = {0};
+        for(int i=0;i<9;++i)
+        {
+            for(int j=0 ; j<9 ; ++j )
+            {
+                if(board[i][j]=='.')
+                    continue;
+                int temp = 1 << (board[i][j]-'1');
+                if(rows[i] & temp)
+                    return false;
+                rows[i] |=temp;
+                if(cols[j] & temp)
+                    return false;
+                cols[j] |=temp;
+                int bIdx =  ((i/3) * 3 ) + j/3;
+                if(boxes[ bIdx] & temp)
+                    return false;
+                boxes[ bIdx] |= temp;
+            }
+            
+        }
+        return true;
+    }
+    bool isValidSudoku1(vector<vector<char>>& board) {
         
         for(int i=0;i<9;++i)
         {
