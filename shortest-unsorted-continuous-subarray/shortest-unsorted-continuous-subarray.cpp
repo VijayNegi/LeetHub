69fe44d9b9a42992fda9a002842f74e6c4116c48
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-08-29 20:20:41 +0530

    Time: 32 ms (56.57%), Space: 26.5 MB (89.02%) - LeetHub

--- shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -1,76 +1,20 @@
 class Solution {
 public:
-    
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        int min_idx=0,start=INT_MAX;
-        int max_idx=n-1,end=-1;
-        vector<int> sorted = nums;
-        sort(sorted.begin(),sorted.end());
+        int minV = nums[n-1];
+        int maxV = nums[0];
+        int start= -1;
+        int end = -2;
         
-        while(n--)
+        for(int i=0;i<n;++i)
         {
-            if(nums[min_idx] == sorted[min_idx])
-                min_idx++;
-            if(nums[max_idx] == sorted[max_idx])
-                max_idx--;
+            minV = min(nums[n-1-i],minV);
+            maxV = max(nums[i],maxV);
+            if(nums[i] < maxV) end = i;
+            if(nums[n-1-i]>minV) start = n-1-i;
         }
         
-        if(max_idx>min_idx)
-            return max_idx - min_idx + 1;
-        return 0;
-        
-    }
-    int findUnsortedSubarray2(vector<int>& nums) {
-        int n = nums.size();
-        int min_idx=0,start=INT_MAX;
-        int max_idx=0,end=-1;
-        
-        for(int i=1;i<n;++i)
-        {
-            if(nums[i]<nums[min_idx])
-            {
-                if(start>min_idx)
-                    start = min_idx;
-                if(end<i)
-                    end = i;
-                
-                min_idx = i;
-            }
-            if(nums[i]<nums[max_idx])
-            {
-                if(start>max_idx)
-                    start = max_idx;
-                if(end<i)
-                    end = i;
-            }
-            if(nums[i]>nums[max_idx])
-            {
-                max_idx = i;
-            }
-        }
-        if(end!=-1)
-            return end - start + 1;
-        return 0;
-        
-    }
-    int findUnsortedSubarray1(vector<int>& nums) {
-        int n = nums.size();
-        int start=INT_MAX;
-        int end = -1;
-        for(int i=0;i<n-1;++i)
-        {
-            if(nums[i]>nums[i+1])
-            {
-                if(start>i)
-                    start = i;
-                if(end<i+1)
-                    end = i+1;
-            }
-        }
-        
-        if(end!=-1)
-            return end - start + 1;
-        return 0;
+        return end - start +1;
     }
 };
