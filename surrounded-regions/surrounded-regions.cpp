3844a2c8c3e03be6f18f1200a2743509281fcee1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-02 13:09:43 +0530

    Time: 16 ms (47.65%), Space: 10.2 MB (35.40%) - LeetHub

--- surrounded-regions/surrounded-regions.cpp
+++ surrounded-regions/surrounded-regions.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j)
-    {
-        if(i<0 || j< 0)
-            return;
-        if(i>= board.size() || j>=board[0].size())
-            return;
-        if(board[i][j] == 'X' || visited[i][j])
-            return;
-        visited[i][j] = true;
-        dfs(board,visited,i+1,j);
-        dfs(board,visited,i,j+1);
-        dfs(board,visited,i-1,j);
-        dfs(board,visited,i,j-1);
-        
-    }
     void solve(vector<vector<char>>& board) {
         int rows = board.size();
         int cols = board[0].size();
         vector<vector<bool>> visited(rows,vector<bool>(cols,false));
         
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;++j)
+        static const function<void(int,int)> markBorder = [&](int i,int j)
             {
-                if(i==0 || i == rows -1 || j== 0 || j == cols -1)
-                    dfs(board,visited, i,j);
-            }
+                if(i<0 || i>=rows || j<0 || j>=cols || board[i][j] !='O')
+                    return;
+                board[i][j] = 'B';
+                markBorder(i+1,j);
+                markBorder(i,j+1);
+                markBorder(i-1,j);
+                markBorder(i,j-1);
+            };
+        
+        for(int j=0;j<cols;++j)
+        {
+            markBorder(0,j);
+            markBorder(rows-1,j);
+        }
+        
+        for(int i=0;i<rows;++i)
+        {
+            markBorder(i,0);
+            markBorder(i,cols-1);
         }
         
         for(int i=0;i<rows;i++)
         {
             for(int j=0;j<cols;++j)
             {
-                if(board[i][j] == 'O' && !visited[i][j])
+                if(board[i][j] == 'O')
                     board[i][j] = 'X';
+                else if(board[i][j]== 'B')
+                    board[i][j] = 'O';
             }
         }
         
