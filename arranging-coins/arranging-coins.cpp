d269c59e0ffda7d832d3928107bc5d94e53a6e04
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-05 18:40:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.19%) - LeetHub

--- arranging-coins/arranging-coins.cpp
+++ arranging-coins/arranging-coins.cpp
@@ -1,11 +1,48 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
+    int arrangeCoins1(int n) {
         int count=0;
         int coin = n;
         for(count=1;count<=n && count<=coin ;++count)
             coin-=count;
 
         return --count;
+    }
+    int arrangeCoins2(int n) {
+        int count=-1;
+        
+        long left=0;
+        long right = (n+1.f)/2;
+        
+        while(left<=right)
+        {
+            long mid = left + (right-left)/2;
+            long sum = (mid*(mid+1))/2;
+            if( sum == n)
+            {
+                count = mid;
+                break;
+            }
+            else if(sum < n)
+            {
+                left = mid+1;
+            }
+            else
+            {
+                right = mid-1;
+            }
+        }
+        
+        if(count ==-1)
+            count = --left;
+    
+        return count;
+    }
+    int arrangeCoins(int n) {
+        int count=0;
+        long coin = n;
+       
+        count = sqrt(2*coin + .25) - 0.5;
+        return count;
     }
 };
