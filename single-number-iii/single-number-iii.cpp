436f787120fa2c56b75a949fc5fe9b04594a9c16
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-06 13:31:36 +0530

    Time: 4 ms (98.65%), Space: 10 MB (76.63%) - LeetHub

--- single-number-iii/single-number-iii.cpp
+++ single-number-iii/single-number-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> singleNumber(vector<int>& nums) {
+    // hashmap
+    vector<int> singleNumber1(vector<int>& nums) {
         unordered_map<int,int> m;
         
         for(const auto n:nums)
@@ -13,7 +14,24 @@ public:
             if(n.second<=1)
                 result.push_back(n.first);
         }
-        
         return result;
+    }
+    
+    //bit manupulation
+    vector<int> singleNumber(vector<int>& nums) {
+        int fullXOR(0);
+        
+        for(auto &n:nums)
+            fullXOR ^= n;
+        
+        int mask(1);
+        while(!(fullXOR & mask) ) mask <<=1;
+        int first(0);
+        for(auto &n:nums)
+        {
+            if(n & mask)
+                first ^= n;
+        }
+        return {first , fullXOR ^ first};
     }
 };
