fc032a52469b9db2fab1dead59b308b266905e95
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-07 17:36:05 +0530

    Time: 8 ms (60.22%), Space: 6.7 MB (61.53%) - LeetHub

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        
+    string multiply1(string num1, string num2) {
         function<string(string,string)> bigAdd = [&](string num1,string num2)
         {
             int size1 = num1.size();
@@ -68,4 +67,28 @@ public:
             return "0";
         return bigMul(num1,num2);
     }
+    
+    // Community solution
+    //https://leetcode.com/problems/multiply-strings/discuss/1563507/C%2B%2B-Simple-Easy-and-Short-Solution-Brief-Explanation
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+        
+        vector<int> res(num1.size()+num2.size(), 0);
+        
+        for (int i = num1.size()-1; i >= 0; i--) {
+            for (int j = num2.size()-1; j >= 0; j--) {
+                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+            }
+        }
+        
+        int i = 0;
+        string ans = "";
+        while (res[i] == 0) i++;
+        while (i < res.size()) ans += to_string(res[i++]);
+        
+        return ans;
+    }
+
 };
