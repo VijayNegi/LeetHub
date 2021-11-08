2732f6020a03493214a8499d663705cb4c99ecec
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-08 15:07:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.02%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -24,13 +24,25 @@ public:
         return uniqueBST(n);
     }
     // dp - Tabulation
-    int numTrees(int n) {
+    int numTrees2(int n) {
         vector<int> dp(n+1);
         dp[0] = dp[1] = 1;
         for(int i = 2; i <= n; i++) 
             for(int j = 1; j <= i; j++)
                 dp[i] += dp[j-1] * dp[i-j];
         return dp[n];
+    }
+    // catalan numbers
+    long ncr(int n, int r) {
+        long ans = 1;
+        for(int i = 0; i < r; i++) {
+            ans *= n-i;
+            ans /= i+1;
+        }
+        return ans;   
+    }
+    int numTrees(int n) {
+        return ncr(2*n, n) / (n + 1);
     }
 
 };
