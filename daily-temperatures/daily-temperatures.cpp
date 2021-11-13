ea52e26dc91d3c73d9c67b5a2e2fa688bc2f12f9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-13 20:00:14 +0530

    Time: 204 ms (34.92%), Space: 85.1 MB (86.64%) - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temps) {
+    // backward monotonic this is more efficient then forward monotonic - 144 ms
+    vector<int> dailyTemperatures1(vector<int>& temps) {
         int n = temps.size();
         stack<int> stk;
         vector<int> res(n,0);
@@ -12,6 +13,44 @@ public:
                 res[i] = stk.top() - i;
             
             stk.push(i);
+        }
+        return res;
+    }
+    //forward monotonic - 231 ms
+    vector<int> dailyTemperatures2(vector<int>& temps) {
+        int n = temps.size();
+        stack<int> stk;
+        vector<int> res(n,0);
+        for(int i= 0;i<n ;++i)
+        {
+            while(!stk.empty() && temps[stk.top()]< temps[i])
+            {
+                res[stk.top()] = i - stk.top();
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        return res;
+    }
+    // No stack ~ 151 ms
+    vector<int> dailyTemperatures(vector<int>& temps) {
+        int n = temps.size();
+        stack<int> stk;
+        vector<int> res(n,0);
+        int hottest = 0;
+        for(int i= n-1;i>=0;--i)
+        {
+            if(temps[i]>= hottest)
+            {
+                hottest = temps[i];
+                continue;
+            }
+            int days = 1;
+            while( temps[i + days]<= temps[i])
+            {
+                days+= res[i+days];
+            }
+            res[i] = days;
         }
         return res;
     }
