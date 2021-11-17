6ffd2494229cd4d0f9d12095ef0f4f1947063bcd
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-17 15:21:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.39%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        
-        for(int i=0;i!=m;++i)
-            dp[i][0] = 1;
-        for(int j=0;j!=n;++j)
-            dp[0][j] = 1;
+    // space m*n
+    int uniquePaths1(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,1));
         
         for(int i=1;i!=m;++i)
         {
@@ -15,5 +11,33 @@ public:
         }
         
         return dp[m-1][n-1];
+    }
+    //space 2*n
+    int uniquePaths2(int m, int n) {
+        vector<int> pre(n,1);
+        vector<int> curr(n,1);
+        
+        for(int i=1;i!=m;++i)
+        {
+            for(int j=1;j!=n;++j)
+            {
+                curr[j] = pre[j] + curr[j-1];
+            }
+            swap(curr,pre);
+        }
+        
+        return pre[n-1];
+    }
+    //space n
+    int uniquePaths(int m, int n) {
+        vector<int> curr(n,1);
+        
+        for(int i=1;i!=m;++i)
+        {
+            for(int j=1;j!=n;++j)
+                curr[j] += curr[j-1];
+        }
+        
+        return curr[n-1];
     }
 };
