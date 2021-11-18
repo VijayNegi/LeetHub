939843240e0a85b9f2f386ba571cb78ebc0bfd3c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-18 13:56:46 +0530

    Time: 40 ms (97.58%), Space: 33.7 MB (80.17%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
+    //sorting
+    vector<int> findDisappearedNumbers1(vector<int>& nums) {
         sort(begin(nums),end(nums));
         vector<int> res;
         int k=0;
@@ -12,6 +13,38 @@ public:
                 ++k;
         }
         
+        return res;
+    }
+    // hasmap
+    vector<int> findDisappearedNumbers2(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();++i)
+        {
+            s.insert(nums[i]);
+        }
+        
+        vector<int> res;
+   
+        for(int i = 1; i<= nums.size();++i)
+        {
+           if(s.count(i))
+               continue;
+            res.push_back(i);
+        }
+        
+        return res;
+    }
+    //inplace
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int len = nums.size();
+        for(int i=0; i<len; i++) {
+            int m = abs(nums[i])-1; // index start from 0
+            nums[m] = nums[m]>0 ? -nums[m] : nums[m];
+        }
+        vector<int> res;
+        for(int i = 0; i<len; i++) {
+            if(nums[i] > 0) res.push_back(i+1);
+        }
         return res;
     }
 };
