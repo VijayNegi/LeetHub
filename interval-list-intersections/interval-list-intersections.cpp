5aa53d354dae20a4e9bc1c635ac4a53460a4304b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-24 12:53:23 +0530

    Time: 32 ms (72.52%), Space: 18.7 MB (75.13%) - LeetHub

--- interval-list-intersections/interval-list-intersections.cpp
+++ interval-list-intersections/interval-list-intersections.cpp
@@ -4,6 +4,9 @@ public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstL, vector<vector<int>>& secondL) {
         int fl = firstL.size();
         int sl = secondL.size();
+        vector<vector<int>> res;
+        if(!fl or !sl)
+            return res;
         
         sort(begin(firstL),end(firstL));
         sort(begin(secondL),end(secondL));
@@ -11,7 +14,7 @@ public:
         int currf = 0;
         int currs = 0;
         
-        vector<vector<int>> res;
+        
         while(currf < fl && currs < sl)
         {
             int s = max(firstL[currf][0],secondL[currs][0]);
