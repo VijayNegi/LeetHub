fa70bed8282c8b1648148ed0e0668a495aeb6f3c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-29 13:55:23 +0530

    Time: 92 ms (81.89%), Space: 39.6 MB (59.74%) - LeetHub

--- accounts-merge/accounts-merge.cpp
+++ accounts-merge/accounts-merge.cpp
@@ -35,16 +35,14 @@ public:
             size[px] += size[py];
             size[py] = 0;
         }
-        
-    }
-        
+    }   
 };
 
 
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        unordered_map<string, int> acMail;
+        unordered_map<string, int> mail2acc;
         
         int count = accounts.size();
         DS disjoint(count);
@@ -52,31 +50,38 @@ public:
         {
             for(int j=1;j< accounts[i].size();++j)
             {
-                if(acMail.count(accounts[i][j])) // already exist
+                if(mail2acc.count(accounts[i][j])) // already exist
                 {
-                    disjoint.unite(acMail[accounts[i][j]], i);
+                    disjoint.unite(mail2acc[accounts[i][j]], i);
                 }
                 else
-                    acMail[accounts[i][j]] = i;
+                    mail2acc[accounts[i][j]] = i;
             }
         }
         
         vector<vector<string>> res;
-        unordered_map<int, set<string>> idMail;
+        unordered_map<int, set<string>> acc2MailSet;
         
-        for(int i=0;i<count;++i)
+        // for(int i=0;i<count;++i)
+        // {
+        //     int par = disjoint.find(i);
+        //     for(int j=1;j<accounts[i].size();++j)
+        //     {
+        //          idMail[par].insert(accounts[i][j]);
+        //     }
+        // }
+        
+        for(auto [mail,acc]: mail2acc)
         {
-            int par = disjoint.find(i);
-            for(int j=1;j<accounts[i].size();++j)
-            {
-                 idMail[par].insert(accounts[i][j]);
-            }
+            int par = disjoint.find(acc);
+            acc2MailSet[par].insert(mail);
+            
         }
         
-        for(auto [id,s]: idMail)
+        for(auto [acc,s]: acc2MailSet)
         {
             vector<string> vec;
-            vec.push_back(accounts[id][0]);
+            vec.push_back(accounts[acc][0]);
             vec.insert(vec.end(),s.begin(),s.end());
             res.push_back(vec);
         }
