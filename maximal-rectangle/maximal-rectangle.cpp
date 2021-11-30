0e25b6d58521f5029270148dc57c4d9b7bcb175f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-30 16:28:09 +0530

    Time: 28 ms (84.53%), Space: 11.8 MB (71.13%) - LeetHub

--- maximal-rectangle/maximal-rectangle.cpp
+++ maximal-rectangle/maximal-rectangle.cpp
@@ -6,24 +6,6 @@ public:
             return 0;
         int c = matrix[0].size();
         
-        
-//         vector<vector<int>> matH(r,vector(c,0)),matV(r,vector(c,0));
-        
-//         for(int i=0;i<r;++i)
-//         {
-//             for(int j=0;j<c;++j)
-//             {
-//                 if(matrix[i][j] == '1')
-//                 {
-//                     matH[i][j] = 1;
-//                     matV[i][j] = 1;
-//                     if(i>0)
-//                         matH[i][j] += matH[i-1][j];
-//                     if(j>0)
-//                         matV[i][j] += matV[i][j-1];
-//                 }
-//             }
-//         }
         vector<int> sum(r,0);
         int maxarea = 0;
         
