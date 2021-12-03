ad353c078f01feb431a729f84a88c21fb832d198
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-03 14:31:52 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (52.62%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
+    int maxProduct1(vector<int>& nums) {
         
         int maxP = nums[0];
         int currPos = nums[0];
@@ -44,5 +44,21 @@ public:
             maxP = max(maxP,currPos);
         }
         return maxP;
+    }
+    int maxProduct(vector<int>& nums) {
+        // we keep also the minimum-product subarray for the case of multiplying negative with negative
+        int max_pro = nums[0];
+        int min_save = nums[0];
+        int max_save = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            // if we got a negative element, we will swap maximum with minimum
+            if (nums[i] < 0)
+                swap(min_save, max_save);
+            min_save = min(nums[i], min_save*nums[i]);
+            max_save = max(nums[i], max_save*nums[i]);
+            max_pro = max(max_pro, max_save);
+        }
+        return max_pro;
     }
 };
