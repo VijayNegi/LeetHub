b875f61913a7cb8f742e4bbe5e1e0500bee5578e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-10 17:41:43 +0530

    Time: 4 ms (32.61%), Space: 7.3 MB (19.90%) - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -2,7 +2,7 @@ typedef long long ll;
 const int mod = 1e9+7;
 class Solution {
 public:
-    int numTilings(int n) {
+    int numTilings1(int n) {
         if(n==1)
             return 1;
         else if(n==2)
@@ -17,6 +17,26 @@ public:
             dp[i][0] = ((ll)dp[i-1][0] + dp[i-2][0] + dp[i-1][1] + dp[i-1][2])%mod;
             dp[i][1] = ((ll)dp[i-1][2] + dp[i-2][0])%mod;
             dp[i][2] = ((ll)dp[i-1][1] + dp[i-2][0])%mod;
+        }
+        
+        return dp[n][0];
+    }
+    //space optimization as both incomplete tiles are equal
+    int numTilings(int n) {
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        vector<vector<int>> dp(n+1,vector<int>(2,0)); // complete, empty up, empty down
+        
+        dp[1] = {1,0};
+        dp[2] = {2,1};
+        
+        for(int i = 3;i<=n;++i)
+        {
+            dp[i][0] = ((ll)dp[i-1][0] + dp[i-2][0] + 2 * dp[i-1][1])%mod;
+            dp[i][1] = ((ll)dp[i-1][1] + dp[i-2][0])%mod;
+            //dp[i][2] = ((ll)dp[i-1][1] + dp[i-2][0])%mod;
         }
         
         return dp[n][0];
