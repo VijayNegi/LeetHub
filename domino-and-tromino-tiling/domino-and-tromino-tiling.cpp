f4251d8dd455cfaca3cd25961e25a0513af425e2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-10 17:26:51 +0530

    Time: 4 ms (32.61%), Space: 7.4 MB (17.03%) - LeetHub

---
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,24 @@
+typedef long long ll;
+const int mod = 1e9+7;
+class Solution {
+public:
+    int numTilings(int n) {
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        vector<vector<int>> dp(n+1,vector<int>(3,0)); // complete, empty up, empty down
+        
+        dp[1] = {1,0,0};
+        dp[2] = {2,1,1};
+        
+        for(int i = 3;i<=n;++i)
+        {
+            dp[i][0] = ((ll)dp[i-1][0] + dp[i-2][0] + dp[i-1][1] + dp[i-1][2])%mod;
+            dp[i][1] = ((ll)dp[i-1][2] + dp[i-2][0])%mod;
+            dp[i][2] = ((ll)dp[i-1][1] + dp[i-2][0])%mod;
+        }
+        
+        return dp[n][0];
+    }
+};
