012438cc4221e393eac3ff229334265200892100
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-12 01:02:03 +0530

    Time: 336 ms (64.71%), Space: 149.5 MB (47.06%) - LeetHub

--- sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.cpp
+++ sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.cpp
@@ -1,3 +1,4 @@
+#if 0
 class SORTracker {
     int qc;
     vector<int> maxh;
@@ -86,6 +87,22 @@ public:
         
     }
 };
+#endif
+//using set
+class SORTracker {
+public:
+    set<pair<int, string>> s;
+    set<pair<int, string>>::iterator it = end(s);    
+    void add(string name, int score) {
+        auto it1 = s.insert({-score, name}).first;
+        if (it == end(s) || *it1 < *it)
+            --it;
+    }
+    string get() {
+        return (it++)->second;
+    }
+};
+
 
 /**
  * Your SORTracker object will be instantiated and called as such:
