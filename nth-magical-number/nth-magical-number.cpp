b378b07594743acd8ceea5b4dad0e0acd942406c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-11 14:28:39 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (76.44%) - LeetHub

--- nth-magical-number/nth-magical-number.cpp
+++ nth-magical-number/nth-magical-number.cpp
@@ -9,7 +9,8 @@ public:
         else
             return gcd(b,a%b);
     }
-    int nthMagicalNumber(int n, int a, int b) {
+    //60ms
+    int nthMagicalNumber1(int n, int a, int b) {
         
         int g = gcd(a,b);
         //cout<<"g="<<g<<"\n";
@@ -37,6 +38,40 @@ public:
         sort(begin(first),end(first));
         
         return ((ll)m*lcm + first[r])%mod;
+        
+            
+    }
+    // binary search, 0 ms
+    int nthMagicalNumber(int n, int a, int b) {
+        
+        int g = gcd(a,b);
+        //cout<<"g="<<g<<"\n";
+        int lower = min(a,b);
+        if(g==lower)
+            return ((ll)lower*n)%mod;
+        int lcm = (a*b)/g;
+        //cout<<"lcm="<<lcm<<"\n";
+        int period = (lcm/a) + (lcm/b) - 1;
+        //cout<<"period="<<period<<"\n";
+        int m = n/period;
+        int r = (n%period);
+        //cout<<"m="<<m<<"\n";
+        //cout<<"r="<<r<<"\n";
+        //vector<int> first{0};//(period+1,0);
+        
+        int l = 0;
+        int h = lcm;
+        
+        while(l<h)
+        {
+            int mid = l +(h-l)/2;
+            if(mid/a + mid/b - mid/lcm <r)
+                l = mid +1;
+            else
+                h = mid;
+        }
+        
+        return ((ll)m*lcm + h)%mod;
         
             
     }
