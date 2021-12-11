47089e7b219be4a5c84e111ceed61afcced52ea0
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-12 00:19:32 +0530

    Time: 8 ms (100.00%), Space: 9.5 MB (92.31%) - LeetHub

--- find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -23,9 +23,9 @@ public:
         }
        return result;
     }
-    // split point 
-    // time : O(n)
-    vector<int> maxSubsequence(vector<int>& nums, int k) {
+    // split point ,4 ms
+    // time : O(n) 
+    vector<int> maxSubsequence2(vector<int>& nums, int k) {
         vector<int> tmp = nums;
         nth_element(tmp.begin(), tmp.end()-k, tmp.end());  // sort till k element
         
@@ -41,6 +41,29 @@ public:
                 ans.push_back(x); 
             }
         return ans; 
+    }
+    // optimized sorting : n + klog(k) , 8 ms
+    vector<int> maxSubsequence3(vector<int>& nums, int k) {
+        vector<pair<int, int>> v;
+        for (int i = 0; i < nums.size(); ++i)
+            v.push_back({nums[i], i});
+        nth_element(begin(v), begin(v) + k, end(v), greater<pair<int, int>>());
+        sort(begin(v), begin(v) + k, [](const auto &a, const auto &b){ return a.second < b.second; });
+        vector<int> res;
+        for (int i = 0; i < k; ++i)
+            res.push_back(v[i].first);
+        return res;
+    }
+    // hash solution, 4 ms
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        
+        vector<int> v , temp = nums;
+        sort(nums.begin() , nums.end());
+        unordered_map<int,int> map;
+        
+        for(int index = 0 ; index < k ; index ++) map[nums[nums.size() - 1- index]]++; 
+        for(auto it: temp) if(map[it]-- > 0)v.push_back(it);
+    return v;
     }
 
 };
