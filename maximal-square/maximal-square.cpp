a3d6ec0a444cafbfe12d41382c33bd1a459f76f8
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-17 15:12:09 +0530

    Time: 20 ms (94.40%), Space: 11.2 MB (94.82%) - LeetHub

--- maximal-square/maximal-square.cpp
+++ maximal-square/maximal-square.cpp
@@ -1,7 +1,7 @@
 int mat[300][300];
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
+    int maximalSquare1(vector<vector<char>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
         int maxMat = 0;
@@ -16,6 +16,32 @@ public:
                     mat[r][c] += tm;
                 }
                 maxMat = max(maxMat,mat[r][c]);
+            }
+        }
+        return maxMat*maxMat;
+        
+    }
+    // space optimization
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        vector<int> dp(cols+1,0);
+        int maxMat = 0;
+        int pre = 0;
+        for(int r=0;r<rows;++r)
+        {
+            for(int c=0;c<cols;++c)
+            {
+                int temp = dp[c];
+                if((matrix[r][c] == '1') &&  c!=0)
+                {
+                    dp[c] = min(dp[c], min(dp[c-1], pre)) + 1;
+                }
+                else
+                    dp[c] = (matrix[r][c] == '1');
+                maxMat = max(maxMat,dp[c]);
+                pre = temp;
             }
         }
         return maxMat*maxMat;
