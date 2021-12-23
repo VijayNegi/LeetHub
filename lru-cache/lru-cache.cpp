1ca4187ec984f90eeac5917301b0b271c416df7f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-24 00:48:48 +0530

    Time: 424 ms (56.57%), Space: 173.9 MB (53.02%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -1,20 +1,20 @@
 class LRUCache {
     int size;
-    unordered_map<int,list<pair<int,int>>::iterator> hmap;
-    list<pair<int,int>> que;
+    unordered_map<int,list<pair<int,int>>::iterator> cache;
+    list<pair<int,int>> used;
 public:
     LRUCache(int capacity) {
         size = capacity;
     }
     
     int get(int key) {
-        if(hmap.count(key))
+        if(cache.count(key))
         {
-            auto it = hmap[key];
+            auto it = cache[key];
             int value = (*it).second;
-            que.erase(it);
-            que.push_front({key,value});
-            hmap[key] = que.begin();
+            used.erase(it);
+            used.push_front({key,value});
+            cache[key] = used.begin();
             return value;
         }
         return -1;
@@ -25,24 +25,24 @@ public:
         // if not already in map, check size, 
         // less then size put in cache
         // size more then remove least recently use put.
-        if(hmap.count(key))
+        if(cache.count(key))
         {
-            que.erase(hmap[key]);
-            que.push_front({key,value});
-            hmap[key] = que.begin();
+            used.erase(cache[key]);
+            used.push_front({key,value});
+            cache[key] = used.begin();
             
         }
         else
         {
-            if(que.size()==size)
+            if(used.size()==size)
             {
-                auto k = que.back();
-                que.pop_back();
-                hmap.erase(k.first);
+                auto k = used.back();
+                used.pop_back();
+                cache.erase(k.first);
             }
             
-            que.push_front({key,value});
-            hmap[key] = que.begin();
+            used.push_front({key,value});
+            cache[key] = used.begin();
         }
     }
 };
