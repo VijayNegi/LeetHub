a1674f3c8e3b3c3b34916d1a96438a28f46abd83
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-23 13:22:26 +0530

    Time: 24 ms (47.25%), Space: 15.6 MB (12.26%) - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prereq) {
+    vector<int> findOrder1(int n, vector<vector<int>>& prereq) {
         vector<set<int>> pre(n);
         vector<set<int>> dep(n);
         
@@ -30,6 +30,51 @@ public:
                 {
                     pre[d].erase(c);
                     if(pre[d].empty())
+                        next.insert(d);
+                }
+                
+            }
+            
+            cset = next;
+        }
+        if(res.size()<n)
+            return {};
+        return res;
+         
+    }
+    // without set
+    vector<int> findOrder(int n, vector<vector<int>>& prereq) {
+       
+        vector<set<int>> adjList(n);
+        vector<int> inDegree(n,0);
+        
+        for(auto vec: prereq)
+        {
+            adjList[vec[1]].insert(vec[0]);
+            ++inDegree[vec[0]];
+        }
+        
+        set<int> cset;
+            
+        for(int i=0;i<n;++i)
+        {
+            if(inDegree[i]==0)
+                cset.insert(i);
+        }
+        
+        vector<int> res;
+        
+        while(!cset.empty())
+        {
+            set<int> next;
+            for(auto c: cset)
+            {
+                res.push_back(c);
+                for(auto d:adjList[c])
+                {
+                    --inDegree[d];
+                    
+                    if(inDegree[d]==0)
                         next.insert(d);
                 }
                 
