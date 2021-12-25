d3d8e738234db0c73d153570cc877fcb7f7c1329
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-25 18:36:42 +0530

    Time: 8 ms (87.98%), Space: 7.7 MB (96.70%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.cpp
+++ basic-calculator-ii/basic-calculator-ii.cpp
@@ -15,7 +15,8 @@ public:
         }
         return a;
     }
-    int calculate(string s) {
+    // first solution
+    int calculate1(string s) {
         int n = s.size();
         int idx = 0;
         function<int(string&,int&)> getInt = [](string& s,int& idx)
@@ -69,9 +70,8 @@ public:
                 }
                 else 
                 {
-                    //char oper1 = s[idx++];
+                    
                     int c = solve(s,tempIdx,op[oper]);
-                    //int d = doOp(b,c,oper1);
                     res = doOp(res,c,oper);
                     idx = tempIdx;
                 }
@@ -80,10 +80,63 @@ public:
            
         };
         
-        
         return solve(s,idx,0);               
-               
-    
-        
     }
+    // other approach: 8ms
+    int calculate2(string s) {
+        istringstream in('+' + s + '+');
+        long long total = 0, term = 0, n;
+        char op;
+        while (in >> op) {
+            if (op == '+' or op == '-') {
+                total += term;
+                in >> term;
+                term *= 44 - op;
+            } else {
+                in >> n;
+                if (op == '*')
+                    term *= n;
+                else
+                    term /= n;
+            }
+        }
+        return total;
+    }
+    // LC solution : 8ms
+     int calculate(string s) { 
+         int idx = 0;
+         int n = s.size();
+         int result = 0;
+         int currNum = 0;
+         int lastNum = 0;
+         char lastOp = '+';
+         while(idx<n)
+         {
+             char c = s[idx++];
+             if(isspace(c) && (idx < n))
+                 continue;
+             if(isdigit(c))
+             {
+                 currNum *=10;
+                 currNum += c - '0';
+                 if(idx < n)
+                    continue;
+             }
+             
+             if(lastOp=='+'|| lastOp=='-')
+             {
+                 result += lastNum;
+                 lastNum = (lastOp=='+')? currNum:-currNum;;
+             }
+             else if(lastOp=='*')
+                 lastNum *= currNum;
+             else if(lastOp=='/')
+                 lastNum /= currNum;
+            
+             currNum=0;
+             lastOp = c;
+         }
+         result += lastNum;
+         return result;
+     }
 };
