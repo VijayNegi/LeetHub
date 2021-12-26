f28ee5325c876a9083dc2fb7d4c3399adc965f6d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-26 15:37:52 +0530

    Time: 228 ms (40.75%), Space: 49 MB (98.48%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    vector<vector<int>> kClosest1(vector<vector<int>>& points, int k) {
         int n = points.size();
         vector<pair<int,int>> dist(n);
         for(int i=0;i<n;++i)
@@ -15,5 +15,17 @@ public:
             result.push_back(points[dist[i].second]);
         }
         return result;
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        partial_sort(points.begin(), points.begin() + K, points.end(), [](vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        });
+        return vector<vector<int>>(points.begin(), points.begin() + K);
+    }
+    vector<vector<int>> kClosest2(vector<vector<int>>& points, int K) {
+        nth_element(points.begin(), points.begin() + K - 1, points.end(), [](vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        });
+        return vector<vector<int>>(points.begin(), points.begin() + K);
     }
 };
