107391a0c0ed776b6b2731076c592ea3130ad6c3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-27 01:26:20 +0530

    Time: 12 ms (68.75%), Space: 9.6 MB (81.25%) - LeetHub

---
+++ maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        
+        int res=0;
+        for(auto& s:sentences)
+        {
+            int curr = 0;
+            for(auto c: s)
+                if(isspace(c))
+                    ++curr;
+            
+            res = max(res, curr +1);
+        }
+        return res;
+    }
+};
