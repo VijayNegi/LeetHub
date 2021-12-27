7bce6dfe7dfe1c90f7147b378a3fd5d1002cbebf
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-27 11:18:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.00%) - LeetHub

--- number-complement/number-complement.cpp
+++ number-complement/number-complement.cpp
@@ -3,10 +3,7 @@ public:
     int findComplement(int num) {
         int mask = 1;
         while(mask<num)
-        {
-            mask = mask<<1;
-            mask +=1;
-        }
+            mask = (mask<<1) + 1;
         
         return num ^ mask;
     }
