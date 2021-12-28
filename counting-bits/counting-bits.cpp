5cc1ec4168ba4a196482af5740e2d3d90c6b5696
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-28 21:14:25 +0530

    Time: 4 ms (88.60%), Space: 7.9 MB (71.05%) - LeetHub

--- counting-bits/counting-bits.cpp
+++ counting-bits/counting-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
+    // self : 4ms
+    vector<int> countBits1(int n) {
         vector<int> res(n+1,0);
         if(n==0)
             return res;
@@ -23,4 +24,20 @@ public:
         }
         return res;
     }
+    // shifting by one to right(multiply by 2) doesnt increase bit count. shiftring to left(divide by 2) will only decrease bit count by one if no is odd.
+    vector<int> countBits2(int n) {
+         
+        vector<int> res(n+1,0);
+        for (int i=1; i<=n; i++) 
+            res[i] = res[i >> 1] + (i & 1);
+        return res;
+    }
+    // i&(i-1) will remove lowest set bit, so we have already removed one bit , so just add one bit count with that number.
+    vector<int> countBits(int num) {
+        vector<int> ret(num+1, 0);
+        for (int i = 1; i <= num; ++i)
+            ret[i] = ret[i&(i-1)] + 1;
+        return ret;
+    }
+    
 };
