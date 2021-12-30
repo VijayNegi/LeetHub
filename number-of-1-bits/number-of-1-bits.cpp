a5ad4ef1446107fe8a669554c476e7530ef95941
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 14:25:13 +0530

    Time: 3 ms (46.83%), Space: 6 MB (46.40%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
+    int hammingWeight1(uint32_t n) {
         int count=0;
         while(n)
         {
@@ -8,6 +8,17 @@ public:
                 ++count;
             n = n>>1;
         }
+        return count;
+    }
+    // only remove last set bit
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+
         return count;
     }
 };
