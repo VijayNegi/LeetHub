8e33f01dbc1b2be6932f5d257d07f95dcf2fbacf
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 14:52:42 +0530

    Time: 4 ms (98.38%), Space: 7.3 MB (43.12%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
         vector<int> histogram(26,0);
         for(char c: s)
         {
