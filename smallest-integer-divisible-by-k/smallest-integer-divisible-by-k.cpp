bf665920b0578c6ab426b8f505b937f3815cfd71
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 13:40:12 +0530

    Time: 4 ms (57.14%), Space: 5.9 MB (46.22%) - LeetHub

--- smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -6,30 +6,17 @@ public:
         
         int len = 0;
         unsigned long long n = 0;
-        unsigned long long limit = 11111111111111; //(long long)1<<60;
-        // while(n<k)
-        // {
-        //     n = n*10+1;
-        //     ++len;
-        // }
-        while(len < 999999 )
+     
+        while(len < k+1 )
         {
-           
+            n = n*10+1;
+            ++len;
 
-                n = n*10+1;
-                ++len;
-
-                if(n % k == 0)
-                    return len;
-                // n = n*10+1;
-                // ++len;
-                 n = n%k;
-            
-           
-            // n = n*10+1;
-            // ++len;
+            if(n % k == 0)
+                return len;
+             n = n%k;
         }
-        //cout<<len<<endl;
+       
         return -1;
     }   
 };
