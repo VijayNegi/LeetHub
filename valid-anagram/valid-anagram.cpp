3e38669523d5feb4ccac0cc392367aadfbc141da
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 14:51:05 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (43.12%) - LeetHub

---
+++ valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> histogram(26,0);
+        for(char c: s)
+        {
+            c-='a';
+            histogram[c]++;
+        }
+        for(char c:t)
+        {
+            c-='a';
+            histogram[c]--;
+        }
+        for(int i:histogram)
+            if(i!=0)
+                return false;
+        return true;
+    }
+};
