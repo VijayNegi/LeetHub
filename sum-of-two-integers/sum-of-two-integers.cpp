f1cf823e67a71d191803817830d6f38bfccd07a4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-03 11:20:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.09%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.cpp
+++ sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int getSum(int a, int b) {
+    // mine as input parameters are not big
+    int getSum1(int a, int b) {
         int min = (abs(a)>abs(b))?b:a;
         int max =  (abs(a)>abs(b))?a:b;
         bool op = min>=0? true:false;
@@ -12,6 +13,27 @@ public:
                 --max;
         }
         return max;
-            
     }
+    // bitwise as recursion - https://leetcode.com/problems/sum-of-two-integers/discuss/84277/One-liner-with-detailed-explanation
+    int getSum2(int a, int b) {
+        if(b == 0) return a;
+        int carry = (unsigned int) (a & b) << 1; // avoid left shifting negative values
+        int sum = a ^ b;
+        return getSum(sum, carry);
+    }
+    
+    // bitwise as iteration
+    int getSum(int a, int b) {
+        int sum = a;
+        
+        while (b != 0)
+        {
+            sum = a ^ b;        //calculate sum of a and b without thinking the carry 
+            b = (unsigned int) (a & b) << 1;   //calculate the carry
+            a = sum;            //add sum(without carry) and carry
+        }
+        
+        return sum;
+    }
+    
 };
