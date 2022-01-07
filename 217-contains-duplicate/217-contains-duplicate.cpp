dd27d20698aa067566baf8c7fa4c9e3b2a25f6f9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-07 20:25:53 +0530

    Time: 109 ms (21.88%), Space: 55.3 MB (5.50%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -22,6 +22,6 @@ public:
     }
     
     bool containsDuplicate(vector<int>& nums) {
-        return set<int>(nums.begin(), nums.end()).size() < nums.size();
+        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
     }
 };
