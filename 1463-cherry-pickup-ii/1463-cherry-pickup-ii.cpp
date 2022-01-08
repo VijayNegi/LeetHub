40fb9513e832c56e53a61c86574a229432e2f66d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-08 17:14:24 +0530

    Time: 101 ms (26.61%), Space: 14.2 MB (41.33%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
+    // dp top down : 91 ms , note few restrictions
+    int cherryPickup1(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        
         vector<vector<vector<int>>> dp(rows,vector<vector<int>>(cols,vector<int>(cols,-1)));
         
         function<int(int,int,int)> dfs = [&](int r,int c1, int c2){
@@ -23,5 +23,44 @@ public:
         };
         
         return dfs(0,0,cols-1);
+    }
+    
+    int cherryPickup(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<vector<int>>> dp(rows,vector<vector<int>>(cols,vector<int>(cols,-1)));
+        
+        
+        for(int r=rows-1;r>=0;--r)
+        {
+            for(int c1=0;c1<cols-1;++c1)
+            {
+                for(int c2 = c1+1;c2<cols;++c2)
+                {
+                    if(r==rows-1)
+                    {
+                        dp[r][c1][c2] = grid[r][c1] + grid[r][c2];
+                    }
+                    else
+                    {
+                        int minC = max(c1-1,0);
+                        int maxC = min(c2+1,cols-1);
+                        int result = 0;
+                        
+                        for(int i = c1-1; i<=c1+1;i++)
+                            for(int j = c2-1;j<=c2+1;j++)
+                            {
+                                if(i<0 || j>=cols || i>=j)
+                                    continue;
+                                result = max(result, dp[r+1][i][j]);
+                            }
+                               
+                        dp[r][c1][c2] =  grid[r][c1] + grid[r][c2] + result;
+                    }
+                }
+            }
+        }
+        
+        return dp[0][0][cols-1];
     }
 };
