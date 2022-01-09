17d262fe579f9658d558f75d9e0fa08e1904ee48
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-09 11:22:22 +0530

    Time: 80 ms (100.00%), Space: 80.5 MB (100.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    int minSwaps(vector<int>& nums) {
-        int maxs = 0;
-        int totals = 0;
+    //contest 120 ms
+    int minSwaps1(vector<int>& nums) {
         int n = nums.size();
         int totalOne = 0;
         for(auto k:nums)
@@ -33,5 +32,19 @@ public:
         }
         
         return totalOne - maxones;
+    }
+    //cleaner 
+    int minSwaps(vector<int>& nums) {
+        int n = size(nums);
+        int ones = count(begin(nums), end(nums), 1);
+        int cnt = count(begin(nums), begin(nums) + ones, 1);
+        int ans = ones - cnt;
+        for(int i = 1; i < n; i++)
+        {
+            cnt += nums[(i + ones - 1) % n] - nums[i - 1];
+            ans = min(ans, ones - cnt);
+        }
+            
+        return ans;
     }
 };
