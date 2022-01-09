a7d0fa592e96666d30210d0c022c3648c00fcd67
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-09 11:07:42 +0530

    Time: 169 ms (64.71%), Space: 34.5 MB (58.82%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool checkValid(vector<vector<int>>& matrix) {
+    bool checkValid1(vector<vector<int>>& matrix) {
         int n = matrix.size();
         vector<vector<bool>> count(2*n,vector<bool>(n,false));
         
@@ -19,6 +19,27 @@ public:
             for(auto p:vec)
                 if(!p)
                     return false;
+        }
+        return true;
+    }
+    //https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/discuss/1676940/XOR-Property-oror-Space-Complexity-O(1)-oror-Easy-Understanding
+    bool checkValid(vector<vector<int>> &v) {
+        int xor1 = 0, xor2 = 0,n = v.size();
+        if(n<=1) {
+            return true;
+        }
+        for(int i=0;i<n;i++) {
+            xor1=0;    xor2=0;
+            for(int j=0;j<n;j++) {
+				// Checking xor column wise
+                xor1 = xor1^(v[i][j]^(j+1));
+				// Checking xor row wise
+                xor2 = xor2^(v[j][i]^(j+1));
+            }
+			// If any of them are not equal to Zero 
+            if(xor1 || xor2) {
+                return false;
+            }
         }
         return true;
     }
