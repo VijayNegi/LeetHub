668cb4220fda4a33c71176217c0b9a1669322e8c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-11 23:47:25 +0530

    Time: 292 ms (8.35%), Space: 38.8 MB (9.48%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
+    // This will give incorrect ans as water just not only flows up and left for pacific and vice versa for atlantic
     vector<vector<int>> pacificAtlantic1(vector<vector<int>>& heights) {
         
         int rows = heights.size();
         int cols = heights[0].size();
         
         vector<vector<int>> reach(rows,vector(cols,0));
-        
-        //vector<int> rmax(rows,0);
-        //vector<int> cmax(cols,0);
+    
         for(int r=0;r<rows;++r)
         {
             reach[r][0] |= 1;
             reach[r][cols-1] |= 2;
-        }
-        
+        } 
         for(int c=0;c<cols;++c)
         {
             reach[0][c] |= 1;
             reach[rows-1][c] |= 2;
         }
-        
         for(int r=1;r<rows;++r)
         {
             for(int c=1;c<cols;++c)
             {
                 if( ((heights[r][c] >= heights[r-1][c]) &&  (reach[r-1][c] & 1)   )  || ((heights[r][c] >= heights[r][c-1]) &&  (reach[r][c-1] & 1)  ) )
-                //if(((heights[r][c] >= heights[r-1][c]) || (heights[r][c] >= heights[r][c-1])) && ((reach[r-1][c] && 1) || (reach[r][c-1] && 1) ))
                     reach[r][c] |= 1;
             }
         }
@@ -36,13 +32,11 @@ public:
             for(int c=cols-2;c>=0;--c)
             {
                 if( ((heights[r][c] >= heights[r+1][c]) &&  (reach[r+1][c] & 2)   )  || ((heights[r][c] >= heights[r][c+1]) &&  (reach[r][c+1] & 2)  ) )
-                //if((heights[r][c] >= heights[r+1][c] || heights[r][c] >= heights[r][c+1]) && ((reach[r+1][c] && 2) || (reach[r][c+1] && 2) ))
                     reach[r][c] |= 2;
             }
         }
         
         vector<vector<int>> res;
-        
         for(int r=0;r<rows;++r)
         {
             for(int c=0;c<cols;++c)
@@ -54,61 +48,42 @@ public:
         
         return res;
     }
-    
-        vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+    // DFS : 269 ms
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         
         int rows = heights.size();
         int cols = heights[0].size();
         
-        vector<vector<int>> reachp(rows,vector(cols,-1));
-        vector<vector<int>> reacha(rows,vector(cols,-1));
+        vector<vector<bool>> reachp(rows,vector(cols,false));
+        vector<vector<bool>> reacha(rows,vector(cols,false));
         
-        function<void(int,int,int)> dfsa = [&](int r,int c,int val)
+        function<void(int,int,int, vector<vector<bool>>&)> dfs = [&](int r,int c,int val , vector<vector<bool>>& visited)
         {
-            if(r<0 || c<0 || c>=cols || r>=rows || heights[r][c] < val ||  reacha[r][c] != -1)
+            if(r<0 || c<0 || c>=cols || r>=rows || heights[r][c] < val ||  visited[r][c] == true)
                 return;
-            reacha[r][c] = 1;
+            visited[r][c] = 1;
             vector<vector<int>> dir{{0,-1},{-1,0},{1,0},{0,1} };
-            
             for(int i=0;i<4;++i)
             {
                 int r1 = r + dir[i][0];
                 int c1 = c + dir[i][1];
-                
-                dfsa(r1,c1, heights[r][c]);
-            }
-        };
-            
-        function<void(int,int,int)> dfsp = [&](int r,int c,int val)
-        {
-            if(r<0 || c<0 || c>=cols || r>=rows || heights[r][c] < val ||  reachp[r][c] != -1)
-                return;
-            reachp[r][c] = 1;
-            vector<vector<int>> dir{{0,-1},{-1,0},{1,0},{0,1} };
-            
-            for(int i=0;i<4;++i)
-            {
-                int r1 = r + dir[i][0];
-                int c1 = c + dir[i][1];
-                
-                dfsp(r1,c1, heights[r][c]);
+                dfs(r1,c1, heights[r][c],visited);
             }
         };
           
         for(int r=0;r<rows;++r)
         {
-            dfsp(r,0,0);
-            dfsa(r,cols-1,0);
+            dfs(r,0,0,reachp);
+            dfs(r,cols-1,0,reacha);
         }
         
         for(int c=0;c<cols;++c)
         {
-            dfsp(0,c,0);
-            dfsa(rows-1,c,0);
+            dfs(0,c,0,reachp);
+            dfs(rows-1,c,0,reacha);
         }
             
         vector<vector<int>> res;
-        
         for(int r=0;r<rows;++r)
         {
             for(int c=0;c<cols;++c)
