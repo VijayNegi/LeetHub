cea2308e59b1f1904c91856d794746ce52c05e3b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-12 18:19:21 +0530

    Time: 0 ms (100.00%), Space: 7 MB (28.65%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -4,35 +4,29 @@ public:
         
         int rows = matrix.size();
         int cols = matrix[0].size();
-        
-        int r=0,c=0;
-        
         vector<vector<int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
-        
+        int r=0,c=0;
         int cdir = 0;
         int dist = 0;
         vector<int> result;
-        
-        
         while(rows>0 && cols>0)
         {
            
             result.push_back(matrix[r][c]);
             ++dist;
-            if(cdir==0 && dist==cols)
+            if(cdir%2==0 && dist==cols)
+            {
                 ++cdir,rows-=1, dist=0;
-            else if(cdir==1 && dist==rows)
-                ++cdir,cols-=1, dist=0;
-            else if(cdir==2 && dist==cols)
-                ++cdir,rows-=1, dist=0;
-            else if(cdir==3 && dist==rows)
-                cdir=0,cols-=1, dist=0;
+            }
+            else if(cdir%2==1 && dist==rows)
+            {
+                cdir= (cdir+1)%4,cols-=1, dist=0;
+            }
             
             r += dir[cdir][0];
             c += dir[cdir][1];
         }
         
         return result;
-        
     }
 };
