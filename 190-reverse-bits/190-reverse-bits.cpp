a62653367ce676ed7c23651d29ff79a82ec6e3e0
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-12 13:08:10 +0530

    Time: 7 ms (7.92%), Space: 5.9 MB (81.40%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
+    uint32_t reverseBits1(uint32_t n) {
         uint32_t rev=0;
         int i=0;
         while(i++ < 32)
@@ -9,5 +9,14 @@ public:
             n = n>>1;
         }
         return rev;
+    }
+    // O(1) : abcdefgh -> efghabcd -> ghefcdab -> hgfedcba
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
     }
 };
