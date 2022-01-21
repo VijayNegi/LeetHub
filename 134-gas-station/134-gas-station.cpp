1aeda1f23acc79498d945086527ebb8da9519cda
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-21 20:06:31 +0530

    Time: 82 ms (35.85%), Space: 49.9 MB (45.28%) - LeetHub

---
+++ 134-gas-station/134-gas-station.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int tcost = std::accumulate(cost.begin(), cost.end(), 0);
+        int tgas = std::accumulate(gas.begin(), gas.end(), 0);
+        if(tgas<tcost)
+            return -1;
+        int n = gas.size();
+        int index = 0;
+        int sum = 0;
+        int minSum = INT_MAX;
+        for(int i=0;i<n*2;i++)
+        {
+            int mi=i%gas.size();
+            int temp = gas[mi]-cost[mi];
+            sum +=temp;
+            if(sum<=minSum)
+            {
+                minSum = sum;
+                index = mi;
+            }
+        }
+        return (index+1)%n;
+    }
+};
