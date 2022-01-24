28fa568bad94fac13d91f6ee9e8434220f249d23
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-25 01:27:09 +0530

    Time: 334 ms (53.96%), Space: 120 MB (60.37%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int constrainedSubsetSum(vector<int>& nums, int k) {
+    // with priority queue : 372ms
+    int constrainedSubsetSum1(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> dp(n+1,0);
         int sum=INT_MIN;
-        // Using lambda to compare elements.
         auto cmp = [&](int left, int right) { return dp[left] < dp[right]; };
         std::priority_queue<int, std::vector<int>, decltype(cmp)> pq(cmp);
         
@@ -19,6 +19,30 @@ public:
                 dp[i] = max(dp[i],dp[pq.top()]+nums[i]);
             }
             pq.push(i);
+            sum = max(sum,dp[i]);
+        }
+        return sum;
+    }
+    //with monotonic queue
+    int constrainedSubsetSum(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        vector<int> dp = nums;
+        int sum=INT_MIN;
+        deque<int> dq;
+        for(int i=0;i<n;++i)
+        {
+            
+            if(!dq.empty())
+            {
+                while(dq.front()< i-k)
+                    dq.pop_front();
+                
+                dp[i] = max(dp[i],dp[dq.front()]+dp[i]);
+            }
+            while(!dq.empty() && dp[dq.back()]<= dp[i])
+                dq.pop_back();
+            dq.push_back(i);
             sum = max(sum,dp[i]);
         }
         return sum;
