329a1e36897843f4ab2c28d40324b9853406f1c4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-24 13:21:09 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.00%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool detectCapitalUse(string word) {
+    bool detectCapitalUse1(string word) {
         int n = word.size();
         int caps = 0;
         for(int i=0;i<n;++i)
@@ -16,4 +16,26 @@ public:
         
         return true;
     }
+    //another way
+    bool detectCapitalUse2(string word) {
+        int cnt=0;
+        int loc;
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                cnt++;
+                loc = i;
+            }
+        }
+        if(cnt == word.length() || cnt == 0 || (cnt==1 && loc==0))
+            return true;
+        return false;
+    }
+    //another
+    bool detectCapitalUse(string word) {
+    for(int i = 1; i < word.size(); i++){
+        if(isupper(word[1]) != isupper(word[i]) || islower(word[0]) && isupper(word[i])) 
+            return false;
+    }        
+    return true;
+}
 };
