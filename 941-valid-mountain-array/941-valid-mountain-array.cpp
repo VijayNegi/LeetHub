ba9c13bf58f5557c2d5c7e7f797dffd22451c406
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-25 17:19:41 +0530

    Time: 31 ms (54.96%), Space: 22.4 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool validMountainArray(vector<int>& arr) {
+    bool validMountainArray1(vector<int>& arr) {
         int n = arr.size();
         if(n<3)
             return false;
@@ -22,6 +22,12 @@ public:
                 break;
         }
         return downhill && (count +1 == n);
-        
+    }
+    // 
+     bool validMountainArray(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
     }
 };
