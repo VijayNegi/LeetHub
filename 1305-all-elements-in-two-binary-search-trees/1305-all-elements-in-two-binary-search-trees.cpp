be2280440669aa17101e40dd13b7f7f2781f7b9f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-26 18:50:20 +0530

    Time: 306 ms (7.68%), Space: 84.5 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+    // 297 ms , inorder traversal, merge sort.
+    vector<int> getAllElements1(TreeNode* root1, TreeNode* root2) {
         
         vector<int> res;
         
@@ -41,6 +42,26 @@ public:
             res.insert(res.end(),t1.begin()+i1,t1.end());
         if(i2<t2.size())
             res.insert(res.end(),t2.begin()+i2,t2.end());
+        return res;
+    }
+    
+    //stack solution
+    void pushLeft(stack<TreeNode*> &s, TreeNode* n) {
+        while (n != nullptr) 
+            s.push(exchange(n, n->left));
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> res;
+        stack<TreeNode*> s1, s2;
+        pushLeft(s1, root1);
+        pushLeft(s2, root2);
+        while (!s1.empty() || !s2.empty()) {
+            stack<TreeNode*> &s = s1.empty() ? s2 : s2.empty() ? s1 : 
+                s1.top()->val < s2.top()->val ? s1 : s2;
+            auto n = s.top(); s.pop();
+            res.push_back(n->val);
+            pushLeft(s, n->right);
+        }
         return res;
     }
 };
