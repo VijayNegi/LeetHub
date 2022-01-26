d65fc4917915f496d27970b717f0146c981b7582
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-26 18:01:29 +0530

    Time: 106 ms (40.59%), Space: 9.6 MB (33.48%) - LeetHub

--- 664-strange-printer/664-strange-printer.cpp
+++ 664-strange-printer/664-strange-printer.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // top down;
+    //my top down; WA
     int strangePrinter1(string s) {
         int n = s.size();
         vector<vector<int>> dp(n,vector(n,0));
@@ -80,23 +80,8 @@ public:
         
         return dfs(0,n-1);
     }
-    // bottom up
-//      int strangePrinter(string s) {
-//         int n = s.size();
-//         vector<vector<int>> dp(n,vector(n,0));
-//         for(int i=0;i<n;++i)
-//             dp[i][i] = 1;
-         
-//          for(int len=1;len<n;++len)
-//          {
-//              for(int start = len;start < n-len;++start)
-//              {
-                 
-//              }
-//          }
-//      }
-    // make it symatric
-    int strangePrinter(string s) {
+    // bottom up and make it symatric
+    int strangePrinter2(string s) {
         int n = s.size();
         string p{s[0]};
         for(int i=1;i<n;++i)
@@ -124,5 +109,31 @@ public:
         
         return dp[0][n-1];
     }
+    // top down : with above approach 
+    int strangePrinter(std::string s)
+    {   
+        int n = s.size();
+        vector<vector<int>> f(n,vector(n,0));
+        
+        function<int(int,int)> dfs = [&]( int l, int r)
+        {
+            if (l > r) return 0;
+            if (f[l][r]) return f[l][r];
+            f[l][r] = dfs(l, r - 1) + 1;
+            for (int i = l; i < r; ++i)
+            {
+                if (s[i] == s[r])
+                {
+                    f[l][r] = std::min(f[l][r], dfs(l, i) + dfs(i + 1, r - 1));
+                }
+            }
+            return f[l][r];
+        };
+
+        return dfs( 0, n - 1);
+    }
+    
+    
+    
     
 };
