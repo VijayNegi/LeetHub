c282e7fe72d024c5f7dd771369c39f3a2a3672dc
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-29 12:25:56 +0530

    Time: 361 ms (5.07%), Space: 93.8 MB (5.16%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
+    // mono stack
+    int largestRectangleArea1(vector<int>& heights) {
         // int , int , fromIndex and minHeight
         list<pair<int,int>> mqueue;
         int n= heights.size();
@@ -23,6 +24,32 @@ public:
             int area = mqueue.back().second * (n-mqueue.back().first);
             mqueue.pop_back();
             result = max(result,area);
+        }
+        return result;
+    }
+    // mono stack again but somewhat cleaner
+    int largestRectangleArea(vector<int>& heights) {
+        list<int> s;
+        int n= heights.size();
+        int result=0;
+        for(int i=0;i<n;++i)
+        { 
+            
+            while(!s.empty() && heights[s.back()] >= heights[i])
+            {
+                int H = heights[s.back()];
+                s.pop_back();
+                int W =  (s.empty()? i: i- s.back()-1);
+                result = max(result,H*W);
+            }
+            s.push_back(i);
+        }
+        while(!s.empty())
+        {
+            int H = heights[s.back()];
+            s.pop_back();
+            int W =(s.empty()? n: n- s.back()-1);
+            result = max(result,H*W);
         }
         return result;
     }
