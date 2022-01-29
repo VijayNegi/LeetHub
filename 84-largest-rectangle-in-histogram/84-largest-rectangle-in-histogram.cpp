079b06c473c013fd4536ca2aa82cceee15b87740
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-29 12:36:22 +0530

    Time: 195 ms (35.68%), Space: 66.3 MB (36.96%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // mono stack
+    // mono stack: 389ms
     int largestRectangleArea1(vector<int>& heights) {
         // int , int , fromIndex and minHeight
         list<pair<int,int>> mqueue;
@@ -27,8 +27,8 @@ public:
         }
         return result;
     }
-    // mono stack again but somewhat cleaner
-    int largestRectangleArea(vector<int>& heights) {
+    // mono stack again but somewhat cleaner : 361ms
+    int largestRectangleArea2(vector<int>& heights) {
         list<int> s;
         int n= heights.size();
         int result=0;
@@ -52,5 +52,41 @@ public:
             result = max(result,H*W);
         }
         return result;
+    }
+    
+    // dp - https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28902/5ms-O(n)-Java-solution-explained-(beats-96)
+    // 
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> lessFromLeft(n,0); // idx of the first bar the left that is lower than current
+        vector<int> lessFromRight(n,0); // idx of the first bar the right that is lower than current
+        
+        lessFromRight[n - 1] = n;
+        lessFromLeft[0] = -1;
+
+        for (int i = 1; i < n; i++) {
+            int p = i - 1;
+
+            while (p >= 0 && heights[p] >= heights[i]) {
+                p = lessFromLeft[p];
+            }
+            lessFromLeft[i] = p;
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            int p = i + 1;
+
+            while (p < n && heights[p] >= heights[i]) {
+                p = lessFromRight[p];
+            }
+            lessFromRight[i] = p;
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            maxArea = max(maxArea, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+
+        return maxArea;
     }
 };
