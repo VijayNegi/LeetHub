3992e703f9c932946e7f3addd4c30ba78867a550
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-30 11:41:09 +0530

    Time: 60 ms (14.44%), Space: 24.9 MB (92.88%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int gcd (int a, int b) 
+    {
+        while (b) {
+            a %= b;
+            swap(a, b);
+        }
+        return a;
+    }
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        while(k>0)
+        {
+            int p = k;
+            
+            while(gcd(n,p)!=1)
+                --p;
+            k -=p;
+            
+            int idx = (p)%n;
+            int value = nums[0];
+            int counter = n;
+            while(counter-->=0)
+            {
+                swap(nums[idx],value);
+                idx = (idx+p)%n;
+            }
+        }
+        
+        
+        return;
+    }
+};
