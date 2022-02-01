05bedb6b761893fd1f473df073ca86d1240ece87
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-02 00:14:37 +0530

    Time: 192 ms (93.07%), Space: 7.9 MB (87.59%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
+    bool exist1(vector<vector<char>>& board, string word) {
         int rows = board.size();
         int cols = board[0].size();
         
@@ -28,8 +28,6 @@ public:
             board[r][c] = ch;
             return false;
         };
-        
-        
         for(int r = 0;r<rows;++r)
         {
             for(int c = 0;c<cols;++c)
@@ -40,4 +38,34 @@ public:
         }
         return false;
     }
+    // Backtracking implmentation
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); ++i)
+        {
+            for(int j = 0; j < board[0].size(); ++j)
+            {
+                if(adjacentSearch(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    
+    }
+protected:
+    bool adjacentSearch(vector<vector<char>>& board, const string& word, int i, int j, int index)
+    {
+        if(index == word.size()) return true; // end condition
+        if(i < 0 || j < 0 || i > board.size()-1 || j > board[0].size()-1) return false; // boundary of matrix
+        if(board[i][j] != word[index]) return false; // do not match
+        // match!
+        board[i][j] = '*'; // change the content, to avoid duplicated search
+        bool furtherSearch =  adjacentSearch(board, word, i+1, j, index+1) || // up
+                              adjacentSearch(board, word, i-1, j, index+1) || // down
+                              adjacentSearch(board, word, i, j-1, index+1) || // left
+                              adjacentSearch(board, word, i, j+1, index+1);   // right
+        
+        board[i][j] = word[index]; // modify it back!
+        return furtherSearch;
+    }
+
 };
