ab5b48e658654373e5ad7a075d07f696e866fe13
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-01 13:41:50 +0530

    Time: 243 ms (16.52%), Space: 93.3 MB (53.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    // finding vally then peak. 232 ms
-    int maxProfit1(vector<int>& prices) {
-        int maxp=0;
-        int minseen = prices[0];
-        
-        for(auto p:prices)
-        {
-            maxp = max(maxp, p - minseen);
-            minseen = min(minseen,p);
-        }
-        return maxp;
-    }
-    // adding single day profits, kadanes algo
     int maxProfit(vector<int>& prices) {
-        int maxCur = 0, maxSoFar = 0;
-        for(int i = 1; i < prices.size(); i++) {
-            maxCur = max(0, maxCur += prices[i] - prices[i-1]);
-            maxSoFar = max(maxCur, maxSoFar);
+        int mProfit = 0;
+        int currProfit = 0;
+        
+        for(int i=1;i<prices.size();++i)
+        {
+            currProfit = max(0, currProfit += prices[i] - prices[i-1] );
+            mProfit = max(mProfit,currProfit);
         }
-        return maxSoFar;
+        return mProfit;
     }
 };
