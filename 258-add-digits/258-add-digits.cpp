b4824ee6fe789ab2c036a41214c944cbd5fce9c7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-08 14:19:48 +0530

    Time: 5 ms (17.94%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int addDigits(int num) {
+    int addDigits1(int num) {
         
         int res=0;
         while(num)
@@ -14,5 +14,10 @@ public:
             }
         }
         return res;
+    }
+     int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
     }
 };
