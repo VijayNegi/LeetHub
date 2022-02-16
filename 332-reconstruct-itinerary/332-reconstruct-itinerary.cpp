feb51998b554db999c6a4d702f3db1d63561e2d3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-17 00:54:16 +0530

    Time: 35 ms (46.56%), Space: 14.2 MB (85.85%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string,int> id2AP;
+        unordered_map<int,string> AP2id;
+        int id = 0;
+        vector<vector<int>> adj(300);
+        int n = tickets.size();
+        for(int i=0;i<n ; ++i)
+        {
+            if(id2AP.count(tickets[i][0])==0)
+            {
+                id2AP[tickets[i][0]] = id++;
+                AP2id[id-1] = tickets[i][0];
+            }
+                
+            if(id2AP.count(tickets[i][1])==0)
+            {
+                id2AP[tickets[i][1]] = id++;
+                AP2id[id-1] = tickets[i][1];
+            }
+                
+            adj[id2AP[tickets[i][0]]].push_back(i);
+            //adj[idAP[t[1]]].push_back(t[0]);
+        }
+        auto cmp = [&](int l,int r){return (tickets[l][1]< tickets[r][1]);};
+        for(auto& a: adj)
+            sort(a.begin(),a.end(), cmp);
+        
+        int beginId = id2AP["JFK"];
+        vector<bool> visited(n,false);
+        vector<string> result;
+        
+        function<void(int ap)> dfs = [&](int ap)
+        {
+           
+            for(auto tic : adj[ap])
+            {
+                if(!visited[tic])
+                {
+                    visited[tic] = true;
+                    dfs(id2AP[tickets[tic][1]]);
+                }
+            }
+            result.push_back(AP2id[ap] ) ;  
+            
+        };
+        
+        dfs(beginId);
+        
+        reverse(result.begin(),result.end());
+        return result;
+        
+    }
+};
