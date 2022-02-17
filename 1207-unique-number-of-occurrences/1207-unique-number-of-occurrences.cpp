4f6ea73be45e165a04e0d010e3315b22ca30a2cc
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-17 10:37:22 +0530

    Time: 5 ms (36.46%), Space: 8 MB (92.21%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -25,14 +25,23 @@ public:
           for (auto& p : m) s.insert(p.second);
           return m.size() == s.size();
     }
-    // improve avg case : 
-    bool uniqueOccurrences(vector<int>& arr) 
+    // improve avg case : 4ms
+    bool uniqueOccurrences3(vector<int>& arr) 
     {
       unordered_map<int, int> m;
       unordered_set<int> s;
       for (auto n : arr) ++m[n];
       for (auto& p : m)
           if (!s.insert(p.second).second) return false;
+      return true;
+    }
+    // count sort twice : 
+    bool uniqueOccurrences(vector<int>& arr) 
+    {
+      short m[2001] = {}, s[1001] = {};
+      for (auto n : arr) ++m[n + 1000];
+      for (auto i = 0; i < 2001; ++i)
+          if (m[i] && ++s[m[i]] > 1) return false;
       return true;
     }
 };
