883e8bcaf0f7cf281cae48bb9b2731de626a1c9c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-17 10:34:19 +0530

    Time: 4 ms (64.96%), Space: 8.2 MB (27.39%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) {
+    // count sort and hashmap
+    bool uniqueOccurrences1(vector<int>& arr) {
         uint32_t count[2001] = {0};
-        
         for(auto a:arr)
             count[a+1000]++;
-        
         unordered_map<int,int> occ_seen;
         for(int i=0;i<2001;++i)
         {
             if(count[i]>0)
             {
-                if(count[i] > 0 && occ_seen[count[i]])
-                    return false;
+                if(occ_seen[count[i]])  return false;
                 occ_seen[count[i]]++;
             }
         }
         return true;
+    }
+    // map and set
+    bool uniqueOccurrences(vector<int>& arr) 
+    {
+      unordered_map<int, int> m;
+      unordered_set<int> s;
+      for (auto n : arr) ++m[n];
+      for (auto& p : m) s.insert(p.second);
+      return m.size() == s.size();
     }
 };
