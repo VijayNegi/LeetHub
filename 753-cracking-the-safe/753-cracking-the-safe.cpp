9c0e765971011a20931161c27d347d8d4246bd2c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-18 16:19:06 +0530

    Time: 4 ms (89.89%), Space: 8.4 MB (68.26%) - LeetHub

--- 753-cracking-the-safe/753-cracking-the-safe.cpp
+++ 753-cracking-the-safe/753-cracking-the-safe.cpp
@@ -1,6 +1,12 @@
+// https://en.wikipedia.org/wiki/De_Bruijn_sequence
 class Solution {
 public:
-    string crackSafe(int n, int k) {
+    // 16ms : https://leetcode.com/problems/cracking-the-safe/discuss/112966/C%2B%2B-greedy-loop-from-backward-with-explaination
+    // An explanation is provided in the solution as to why post order-
+    // we'll need to visit in a sort of "post-order", recording the answer after visiting the edge. This is to prevent getting stuck. For example, with k = 2, n = 2, we have the nodes '0', '1'. 
+    // If we greedily visit complete edges '00', '01', '10', we will be stuck at the node '0' prematurely. However, if we visit in post-order, we'll end up visiting '00', '01', '11', '10' correctly.
+    // Another explanation for preorder - https://leetcode.com/problems/cracking-the-safe/discuss/112966/C++-greedy-loop-from-backward-with-explaination/991570
+    string crackSafe1(int n, int k) {
         unordered_set<string> seen;
         string pass(n,'0'),result;
         result += pass;
@@ -24,4 +30,25 @@ public:
         }
         return result;
     }
+    
+    string crackSafe(int n, int k) {
+        if (k == 1) return string(n, '0');
+        string sequence;
+        int v = pow(k,n-1);
+        vector<vector<bool> > visited(v,vector(k,false));
+        function<void(int)> dfs = [&](int u) 
+        {
+            for (int i = 0; i < k; ++i) {
+                if (!visited[u][i]) {
+                    visited[u][i] = true;
+                    dfs((u * k + i) % v);
+                    sequence.push_back('0' + i);
+                }
+            }
+        };
+        dfs(0);
+        return sequence + sequence.substr(0, n - 1);
+    }
+    
+    
 };
