7d76c4b9bfc26d44dccc636517e7875402d0347b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-18 22:14:25 +0530

    Time: 1689 ms (20.28%), Space: 312.1 MB (30.32%) - LeetHub

---
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        unordered_map<int,vector<int>> adj;
+        unordered_map<int,int> inOutdegree;
+        for(auto p:pairs)
+        {
+            adj[p[0]].push_back(p[1]);
+            inOutdegree[p[0]]++;
+            inOutdegree[p[1]]--;
+        }
+        int start=-1;
+        for(auto it:inOutdegree)
+        {
+            if(it.second>0)
+                start = it.first;
+        }
+        if(start==-1)
+            start = pairs[0][0];
+        
+        vector<vector<int>> result;
+        function<void(int)> dfs = [&](int node)
+        {
+            while(adj[node].size())
+            {
+                int next = adj[node].back();
+                adj[node].pop_back();
+                dfs(next);
+                result.push_back({node,next});
+            }
+        };
+        
+        dfs(start);
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
