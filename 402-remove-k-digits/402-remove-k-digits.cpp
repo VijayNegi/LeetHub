62462008f8be003bdd3228f229a6405e5f06272d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-18 14:06:50 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (35.82%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -7,22 +7,19 @@ public:
         
         for(auto c:num)
         {
-            if(k!=0 && result.size())
+            
+            while(result.size() && result.back() > c && k )
             {
-                while(result.size() && result.back() > c && k )
-                {
-                    result.pop_back();
-                    --k;
-                }
-                result.append(1,c);
+                result.pop_back();
+                --k;
             }
-            else
-                result.append(1,c);
-                
+            result.append(1,c);  
         }
-       int s=0;
+        int s=0;
+        // remove leading zeros
         while(s<result.size() && result[s++]=='0');
-         result = result.substr(s-1);
+        result = result.substr(s-1);
+        // remove remaining digits from back.
         while(result.size() && k--)
             result.pop_back();
        
