b4e2cf1934f1936f0b19157f451581684b26cdba
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-23 17:55:26 +0530

    Time: 12 ms (29.56%), Space: 8.9 MB (45.65%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> copy;
+        unordered_map<Node*,bool> visited;
+        
+        function<Node*(Node*)> dfs = [&](Node* root) -> Node*
+        {
+            if(!root)
+                return nullptr;
+            if(visited[root])
+                return copy[root];
+            visited[root] = true;
+            if(copy.find(root) == copy.end())
+                copy[root] = new Node(root->val);
+            
+            Node* curr = copy[root];
+            for(auto n:root->neighbors)
+                curr->neighbors.push_back(dfs(n));
+            
+            return curr;
+        };
+        
+        return dfs(node);
+    }
+};
