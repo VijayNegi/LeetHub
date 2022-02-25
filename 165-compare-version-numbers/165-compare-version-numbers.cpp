0248ec11961aa48e5c889a32613176a782c7640c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-25 12:09:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
+    int compareVersion1(string version1, string version2) {
         std::istringstream ss1(version1), ss2(version2);
         int ans = 0;
         string s1,s2;
@@ -19,5 +19,18 @@ public:
                return n1>n2?1:-1;
         }
         return 0;
+    }
+    int compareVersion(string version1, string version2) {
+        for(auto& w : version1) if (w == '.') w=' ';
+        for(auto& w : version2) if (w == '.') w=' ';
+        istringstream s1(version1), s2(version2);
+        while(1) {
+            int n1,n2;
+            if (not(s1 >> n1) ) n1 = 0;
+            if (not(s2 >> n2) ) n2 = 0;
+            if (not s1 and not s2) return 0;
+            if (n1<n2) return -1;
+            if (n1>n2) return 1;
+        }
     }
 };
