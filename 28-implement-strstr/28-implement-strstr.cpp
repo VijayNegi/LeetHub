cc6abc92f00eb971e254b9ec41aaf2c50a91c829
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-25 10:58:12 +0530

    Time: 18 ms (64.23%), Space: 7.6 MB (25.99%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -37,7 +37,7 @@ public:
         
     }
     // KMP
-    int strStr(string haystack, string needle) {
+    int strStr2(string haystack, string needle) {
         int h = haystack.size(), n = needle.size();
         if(n==0)
             return 0;
@@ -51,6 +51,43 @@ public:
                 k = pi[k-1];
             if(needle[k]==haystack[i])
                 ++k;
+            if(k==n)
+            {
+                return i-n+1;
+            }
+        }
+        return -1;
+    }
+    
+    // start with -1
+    vector<int> prefix_function2(string s) {
+        int n = s.size();
+        vector<int> pi(n + 1, 0);
+        pi[0] = -1;
+        int k = -1;
+              
+        for (int i = 0; i < n; i++) {
+            while(k >= 0 && s[k] != s[i]) 
+                k = pi[k];
+            pi[i+1] = ++k;
+        }
+        
+        return pi;
+    }
+    
+    int strStr(string haystack, string needle) {
+        int h = haystack.size(), n = needle.size();
+        if(n==0)
+            return 0;
+        if(n>h)
+            return -1;
+        vector<int> pi = prefix_function2(needle);
+        int k=0;
+        for(int i=0;i<h;++i)
+        {
+            while(k>=0 && needle[k] != haystack[i])
+                k = pi[k];
+            ++k;
             if(k==n)
             {
                 return i-n+1;
