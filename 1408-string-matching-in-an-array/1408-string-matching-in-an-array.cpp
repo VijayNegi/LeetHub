59d25e3758d7351fc9263a0e1963f4a2c5d6194e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-26 23:18:11 +0530

    Time: 10 ms (45.08%), Space: 8.5 MB (43.04%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> prefix_function(string s)
+    {
+        int n = s.size();
+        vector<int> pi(n,0);
+        int k=0;
+        for(int i=1;i<n;++i)
+        {
+            while(k>0 && s[k]!=s[i])
+                k = pi[k-1];
+            if(s[k]==s[i])
+                ++k;
+            pi[k] = k;
+        }
+        return pi;
+    }
+    bool match(string& pattern, string& t, vector<int>& pre)
+    {
+        int p = pattern.size();
+        int n = t.size();
+        int k=0;
+        for(int i=0;i<n;++i)
+        {
+            while(k>0 && pattern[k] != t[i])
+                k = pre[k-1];
+            if(pattern[k] == t[i])
+                ++k;
+            if(k==p)
+                return true;
+        }
+        return false;
+    }
+    vector<string> stringMatching(vector<string>& words) {
+        int n = words.size();
+        vector<vector<int>> pre(n);
+        for(int i=0;i<n;++i)
+            pre[i] = prefix_function(words[i]);
+        
+        vector<string> result;
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(i==j || (words[j].size() < words[i].size()))
+                    continue;
+                if(match(words[i],words[j],pre[i]))
+                {
+                    result.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+            
+        return result;
+    }
+};
