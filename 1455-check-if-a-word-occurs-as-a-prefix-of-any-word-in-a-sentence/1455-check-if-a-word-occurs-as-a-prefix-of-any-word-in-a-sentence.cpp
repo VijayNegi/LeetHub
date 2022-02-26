0a134edbba1ac4c44eb1f34dff8a48143dd5b148
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-27 00:45:15 +0530

    Time: 2 ms (35.23%), Space: 6.1 MB (91.98%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -40,13 +40,25 @@ public:
         return search_word(" "+searchWord," "+sentence);
     }
     
-    // std search
-    int isPrefixOfWord(string sentence, string searchWord) {
+    // std search 0 ms
+    int isPrefixOfWord2(string sentence, string searchWord) {
         auto sent = " " + sentence, word = " " + searchWord;
         auto pos = sent.find(word);
         if (pos != string::npos)
             return count(begin(sent), begin(sent) + pos + 1, ' ');
         return -1;
     }
-    
+    // KMP type as our work only start with space
+    int isPrefixOfWord(string sentence, string searchWord) {
+        auto sent = " " + sentence, word = " " + searchWord;
+        int word_cnt = 0, j = 0;
+        for (auto i = 0; i < sent.size() && j < word.size(); ++i) {
+            word_cnt += sent[i] == ' ';
+            if (sent[i] == word[j])
+                ++j;
+            else
+                j = sent[i] == word[0] ? 1 : 0;
+        }
+        return j == word.size() ? word_cnt : -1;
+    }
 };
