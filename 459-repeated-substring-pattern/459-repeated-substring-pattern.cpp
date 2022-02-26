cf907ec70d5c877824b6fc8cd4d92f3f01273d36
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-26 13:31:20 +0530

    Time: 20 ms (82.86%), Space: 13.5 MB (37.51%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -17,7 +17,7 @@ public:
         return pi;
     }
     // 8 ms
-     bool repeatedSubstringPattern(string s) {
+     bool repeatedSubstringPattern1(string s) {
          int n = s.size();
          vector<int> pi = prefix_function(s);
          int patternLen = n- pi[n-1];
@@ -25,5 +25,10 @@ public:
              return false;
 
          return true;
+    }
+    //
+    //https://leetcode.com/problems/repeated-substring-pattern/discuss/826135/C%2B%2B-O(N)-time-or-One-liner-without-KMP-Explained-or-Beats-99
+    bool repeatedSubstringPattern(string s) {
+        return (s + s).substr(1, 2*s.size()-2).find(s) != -1;
     }
 };
