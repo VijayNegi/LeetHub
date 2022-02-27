509ea9ab5a2bb369e00944ae672c5dacfc10b4bb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-27 20:54:01 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.92%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -7,7 +7,6 @@ public:
         int is=0,ip=0;
         while(is<ns && ip<np)
         {
-            //if( (isupper(p[ip]) && islower(s[is])) || ( (islower(p[ip]) && islower(s[is])) &&  s[is]!=p[ip]))
             if(islower(s[is]) && (isupper(p[ip]) || s[is]!=p[ip]))
             {
                 ++is;
@@ -27,7 +26,7 @@ public:
             return true;
         return false;
     }
-    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+    vector<bool> camelMatch1(vector<string>& queries, string pattern) {
         int n = queries.size();
         vector<bool> result(n);
         for(int i=0;i<n;++i)
@@ -35,5 +34,20 @@ public:
             result[i] = camelMatch(queries[i],pattern);
         }
         return result;
+    }
+    
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        vector<bool> res;
+        for (string &query : queries) res.push_back(isMatch(query, pattern));
+        return res;
+
+    }
+
+    bool isMatch(string query, string pattern) {
+        int i = 0;
+        for (char & c : query)
+            if (i < pattern.length() && c == pattern[i]) i++;
+            else if (c < 'a') return false;
+        return i == pattern.length();
     }
 };
