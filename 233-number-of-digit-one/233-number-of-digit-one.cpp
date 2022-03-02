796159ea60d2cba45f82d594f6e73bc2dfc019ca
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-02 23:55:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.19%) - LeetHub

--- 233-number-of-digit-one/233-number-of-digit-one.cpp
+++ 233-number-of-digit-one/233-number-of-digit-one.cpp
@@ -1,12 +1,13 @@
 class Solution {
     vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp2;
 public:
-    int countDigitOne(int n) {
+    // digit dp: 26 ms
+    int countDigitOne1(int n) {
         dp = vector(10,vector(82,vector(2,-1)));
         string s = to_string(n);
         return digit_dp(s,0,0,1);
     }
-    
     int digit_dp(string s,int pos,int sum,int edge)
     {
         if(pos == s.size())
@@ -27,7 +28,47 @@ public:
             else
                 result += digit_dp(s,pos+1,sum, lmt);
         }
-        
         return dp[pos][sum][edge] = result;
+    }
+    //digit dp 2 states
+     int countDigitOne(int n) {
+        dp2 = vector(10,vector(82,-1));
+        string s = to_string(n);
+         int len = s.size();
+         int sum = 0;
+         int result = 0;
+         for(int i=0;i<len;++i)
+         {
+             int limit = s[i]-'0';
+             for(int j=0;j<limit;++j)
+             {
+                 int tsum = sum + (j==1);
+                 result += digit_dp2(s,i+1,tsum);
+             }
+             if(limit==1)
+                 ++sum;
+         }
+        return result + sum;
+    }
+    
+    int digit_dp2(string s,int pos,int sum)
+    {
+        if(pos == s.size())
+            return sum;
+        if(dp2[pos][sum] != -1)
+            return dp2[pos][sum];
+        int limit = 9;
+        int result = 0;
+        for(int i=0;i<=limit;++i)
+        {
+            
+            if(i==1)
+            {
+                result += digit_dp2(s,pos+1,sum+1);
+            }
+            else
+                result += digit_dp2(s,pos+1,sum);
+        }
+        return dp2[pos][sum] = result;
     }
 };
