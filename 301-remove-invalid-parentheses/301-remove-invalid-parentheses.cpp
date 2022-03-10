39302df15d1df625335c95be7f862235ed7f1f26
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-10 23:39:51 +0530

    Time: 8 ms (86.21%), Space: 7.9 MB (86.00%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,89 +1,50 @@
+// https://leetcode.com/problems/remove-invalid-parentheses/discuss/75046/C%2B%2B-Depth-limited-DFS-3ms.-Eliminate-duplicates-without-hashmap.
 class Solution {
-    set<string> res;
+private:
+    bool isValid(string s) {
+        int sum = 0;
+        for(char &c : s) {
+            if(c == '(') ++sum;
+            else if(c == ')') --sum;
+            if(sum < 0) return false;
+        }
+        return sum == 0;
+    }
 public:
-    //  (( )( ) 
-    void removeOpen(string s,int i, vector<int> inO,vector<int> inC)
-    {
-        //cout<<"rOpen "<<s<<endl;
-        if(i>= inO.size())
-        {
-            removeClose(s,0,inO,inC);
-        }
-        else
-        {
-            int ii = inO[i];
-            //ii += i;
-            for(int k=s.size()-1;k>=ii;--k)
-            {
-                if(s[k]=='(')
-                {
-                    string p = s;
-                     removeOpen(p.erase(k,1),i+1,inO,inC); 
-                }
-            }
-        }
-    }
-    
-    void removeClose(string s,int i, vector<int> inO,vector<int> inC)
-    {
-        //cout<<"rClose "<<s<<endl;
-        if(i>= inC.size())
-            res.insert(s);
-        else
-        {
-            int ii = inC[i];
-            ii -= i;
-            for(int k=0;k<=ii;++k)
-            {
-                if(s[k]==')')
-                {
-                    string p = s;
-                    p.erase(k,1);
-                     removeClose(p,i+1,inO,inC); 
-                }
-            }
-        }
-            
-    }
-    
     vector<string> removeInvalidParentheses(string s) {
-        int n = s.size();
-        vector<int> opn;
-        vector<int> clo;
-        
-        
-        int count=0;
-        // invalid closes
-        for(int i=0;i<n;++i)
-        {
-            if(s[i]=='(')
-                ++count;
-            else if(s[i]==')')
-                --count;
-            if(count<0)
-            {
-                clo.push_back(i);
-                count = 0;
-            }    
+        int num1 = 0, num2 = 0;
+        for(char &c : s) {
+            num1 += c == '(';
+            if (num1 == 0) {
+                num2 += c == ')';
+            } else {
+                num1 -= c == ')';
+            }
         }
-        // invalid open
-        count=0;
-        for(int i=n-1;i>=0;--i)
-        {
-            if(s[i]=='(')
-                ++count;
-            else if(s[i]==')')
-                --count;
-            if(count>0)
-            {
-                opn.push_back(i);
-                count = 0;
-            }   
+        vector<string> ret;
+        dfs(s, 0, num1, num2, ret);
+        return ret;
+    }
+    void dfs(string s, int beg, int num1, int num2, vector<string> &ret) {
+        if(num1 == 0 && num2 == 0) {
+            if(isValid(s))
+                ret.push_back(s);
+        } else {
+            for(int i = beg; i < s.size(); ++i) {
+                string tmp = s;
+                if(num2 == 0 && num1 > 0 && tmp[i] == '(') {
+                    if(i == beg || tmp[i] != tmp[i - 1]) {
+                        tmp.erase(i, 1);
+                        dfs(tmp, i, num1 - 1, num2, ret);
+                    }
+                }
+                if(num2 > 0 && tmp[i] == ')') {
+                    if(i == beg || tmp[i] != tmp[i - 1]) {
+                        tmp.erase(i, 1);
+                        dfs(tmp, i, num1, num2 - 1, ret);
+                    }
+                }
+            }
         }
-        //cout<<opn.size()<<" -- "<<clo.size()<<endl;
-        removeOpen(s,0, opn,clo);
-        
-        vector<string> res1(res.begin(),res.end());
-        return res1;
     }
 };
