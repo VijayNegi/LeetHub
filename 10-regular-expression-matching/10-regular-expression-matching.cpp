d37fafc0a9e0f9f75dbcb1deb7c5f025b6f32301
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-16 23:17:32 +0530

    Time: 86 ms (21.66%), Space: 28.9 MB (5.24%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+        return match(s,s.size()-1,p,p.size()-1);
+    }
+    
+    bool match(string s,int si, string p,int pi)
+    {
+        //cout<<si<<" "<<pi<<endl;
+        if(si==-1 && pi==-1)
+            return true;
+        if(pi==-1)
+            return si==-1;
+        if(p[pi]=='*')
+        {
+            if(match(s,si,p,pi-2))
+                return true;
+            while(si>=0 && (s[si]==p[pi-1] || p[pi-1]=='.'))
+                if(match(s,--si,p,pi-2))
+                    return true;
+            return false;
+        }
+        else if(si>=0)
+        {
+            if((s[si]==p[pi] || p[pi]=='.') && match(s,si-1,p,pi-1) )
+                return true;
+        }
+        return false;
+        
+    }
+};
