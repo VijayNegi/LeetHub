1462d7cac80735231e698b8165895bf37d5bee97
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-17 00:05:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (46.10%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1,-1);
+        return numdp(s,0,dp);
+    }
+    int numdp(string& s,int i,vector<int>& dp)
+    {
+        int n = s.size();
+        if(i>=n)
+            return 1;
+        if(dp[i]!=-1)
+            return dp[i];
+        int res = 0;
+        if(s[i]=='0')
+            return dp[i]=0; // no number can start from 0
+        res = numdp(s,i+1,dp);
+        if( i < n-1 && (s[i]=='2' && s[i+1]<'7'))
+           res += numdp(s,i+2,dp);
+         if( i < n-1 && (s[i]<'2'))
+             res += numdp(s,i+2,dp);
+        
+        return dp[i] = res;
+    }
+};
