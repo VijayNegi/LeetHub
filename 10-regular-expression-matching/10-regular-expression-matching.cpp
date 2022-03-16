5aa376475b35b00a00bf8e78eb4e9dc5d1af5fa8
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-16 23:25:35 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (36.88%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        
-        return match(s,s.size()-1,p,p.size()-1);
+        vector<vector<int>> dp(s.size()+1,vector(p.size()+1,-1));
+        return match(s,s.size()-1,p,p.size()-1,dp);
     }
-    bool match(string& s,int si, string& p,int pi)
+    bool match(string& s,int si, string& p,int pi, vector<vector<int>>& dp)
     {
         //cout<<si<<" "<<pi<<endl;
         if(si==-1 && pi==-1)
             return true;
         if(pi==-1) // pattern could have redundent match so wait like a* at beginning
             return si==-1;
+        if(dp[si+1][pi+1]!= -1)
+            return dp[si+1][pi+1];
         if(p[pi]=='*')
         {
-            if(match(s,si,p,pi-2))
-                return true;
+            if(match(s,si,p,pi-2,dp))
+                return dp[si+1][pi+1] = true;
             while(si>=0 && (s[si]==p[pi-1] || p[pi-1]=='.'))
-                if(match(s,--si,p,pi-2))
-                    return true;
-            return false;
+                if(match(s,--si,p,pi-2,dp))
+                    return dp[si+1][pi+1] = true;
+            return dp[si+1][pi+1] =false;
         }
         else if(si>=0)
         {
-            if((s[si]==p[pi] || p[pi]=='.') && match(s,si-1,p,pi-1) )
-                return true;
+            if((s[si]==p[pi] || p[pi]=='.') && match(s,si-1,p,pi-1,dp) )
+                return dp[si+1][pi+1] = true;
         }
-        return false;
+        return dp[si+1][pi+1] = false;
         
     }
 };
