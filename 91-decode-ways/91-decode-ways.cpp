213d08f71e7e0ba7c4a68875686392ecc192966b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-17 00:21:27 +0530

    Time: 3 ms (53.37%), Space: 6.1 MB (92.46%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numDecodings(string s) {
+    int numDecodings1(string s) {
         vector<int> dp(s.size()+1,-1);
         return numdp(s,0,dp);
     }
@@ -18,4 +18,18 @@ public:
         
         return dp[i] = res;
     }
+    //https://leetcode.com/problems/decode-ways/discuss/30451/Evolve-from-recursion-to-dp
+    // dp with constant memory
+    int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+    
+    
 };
