eb703a435ee3425a8f475c4f3332244ebc36177c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-18 00:01:11 +0530

    Time: 16 ms (97.84%), Space: 19.2 MB (83.79%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int d,n,count;
-        d=count=0;
+        int d,n;
+        d=0;
         n = nums.size();
         for(int i=0;i<n;++i)
         {
             if(nums[i]!=0)
                 nums[d++] = nums[i];
-            else
-                ++count;
         }
-        for(int i=n-1;i>=n-count;--i)
+        for(int i=d;i<n;++i)
             nums[i] = 0;
     }
 };
