57e611dd45083c5185451b5d2c27838c30e4f166
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-18 23:54:42 +0530

    Time: 3 ms (73.49%), Space: 6.6 MB (66.29%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        string t = s;
-        // sort followed by unique, to remove all duplicates
-        //std::sort(t.begin(), t.end()); 
-        //auto last = std::unique(t.begin(), t.end());
-        //t.erase(last, t.end());
-        //cout <<t<<endl;
-        vector<int> map(26,0);
-        vector<bool> seen(26,0);
+        vector<int> count(27,0);
+        vector<bool> visited(27,false);
         
-        for(char c:s)
-            map[c-'a']++;
-        
-        string result;
-        int idx = 0;
+        for(char c: s)
+         count[c-'a']++;
+        string result("");
         for(char c:s)
         {
-        
-            while(!seen[c-'a'] && !result.empty() && (result.back()>c) && (map[result.back() - 'a'] >= 1) )
+            count[c-'a']--;
+            
+            if(visited[c-'a'])
+                continue;
+            
+            while(!result.empty() && result.back()>c && count[ result.back() - 'a']>0)
             {
-                //cout<<"pop"<<endl;
-                seen[result.back() - 'a'] = false;
+                visited[result.back() - 'a'] = false;
                 result.pop_back();
-                
             }
-            if(!seen[c-'a'])
-            {
-                result.push_back(c);
-                seen[c-'a'] = true;
-            }
-            //cout<<c<<" "<<result<<endl;
-            --map[c-'a'];
+            result +=c;
+            visited[c-'a'] = true; 
         }
+        
         return result;
     }
 };
