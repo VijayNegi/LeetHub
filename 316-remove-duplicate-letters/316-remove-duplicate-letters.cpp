401bb3fdfa1d9cf760838e3397f2577bc3f0af99
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-19 00:05:46 +0530

    Time: 4 ms (61.69%), Space: 6.6 MB (66.29%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string removeDuplicateLetters(string s) {
+    string removeDuplicateLetters1(string s) {
         vector<int> count(27,0);
         vector<bool> visited(27,false);
         
@@ -15,6 +15,30 @@ public:
                 continue;
             
             while(!result.empty() && result.back()>c && count[ result.back() - 'a']>0)
+            {
+                visited[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+            result +=c;
+            visited[c-'a'] = true; 
+        }
+        
+        return result;
+    }
+    // instead of count we can also use last index
+    string removeDuplicateLetters(string s) {
+        vector<int> last(27,0);
+        vector<bool> visited(27,false);
+        for(int i=0;i<s.size();++i)
+            last[s[i]-'a'] = i;
+        string result("");
+        for(int i=0;i<s.size();++i)
+        {
+            char c = s[i];
+            if(visited[c-'a'])
+                continue;
+            
+            while(!result.empty() && result.back()>c && last[ result.back() - 'a']>i)
             {
                 visited[result.back() - 'a'] = false;
                 result.pop_back();
