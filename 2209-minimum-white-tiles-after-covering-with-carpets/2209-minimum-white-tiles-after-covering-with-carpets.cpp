5ac393357fac2daf32991ec8700d311fd801823d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-20 00:28:11 +0530

    Time: 796 ms (11.11%), Space: 254.3 MB (11.11%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int minimumWhiteTiles(string floor, int nC, int cLen) {
+    int minimumWhiteTiles1(string floor, int nC, int cLen) {
         int n = floor.size();
         if(nC*cLen>=n)
             return 0;
@@ -22,36 +22,33 @@ public:
             }
         }
         int result = white;
-        function<void(int,int,int)> bt = [&](int pos,int visible,int remain)
-        {
-            
-            if(pos>=n || remain==0)
-            {
-                result = min(result,visible);
-                return;
-            }
-            while(pos<n)
-            {
-                if(sw[pos-cLen+1]>0)
-                    bt(pos+cLen,visible-sw[pos-cLen+1],remain-1);
-                ++pos;
-            } 
-        };
         vector<vector<int>> dp(n+1,vector(nC+1,-1));
-         function<int(int,int)> bt1 = [&](int pos,int remain)
+        function<int(int,int)> bt1 = [&](int pos,int remain)
         {
-            
             if(pos>=n || remain==0)
                 return 0 ;
              
              if(dp[pos][remain]!=-1)
                  return dp[pos][remain];
-             
-             int covered = max(sw[pos-cLen+1]+bt1(pos+cLen,remain-1), bt1(pos+1,remain));
+            int covered = max(sw[pos-cLen+1]+bt1(pos+cLen,remain-1), bt1(pos+1,remain));
             return dp[pos][remain] = covered;
         };
-        //bt(cLen-1,white,nC);
+
         int cov = bt1(cLen-1,nC);
         return white-cov;
     };
+    // DP bottom up: 
+    int minimumWhiteTiles(string s, int nc, int l) {
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(nc + 1));
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 0; k <= nc; ++k) {
+                int jump = dp[i - 1][k] + s[i - 1] - '0';
+                int cover = k > 0 ? dp[max(i - l, 0)][k - 1] : 1000;
+                dp[i][k] = min(cover, jump);
+            }
+        }
+        return dp[n][nc];
+    }
+
 };
