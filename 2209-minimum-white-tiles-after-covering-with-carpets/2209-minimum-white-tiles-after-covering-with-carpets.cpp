6b663df923ba453347eb3a5f9af6cb291c156e6d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-20 00:43:52 +0530

    Time: 1654 ms (11.11%), Space: 254.9 MB (11.11%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -37,18 +37,41 @@ public:
         int cov = bt1(cLen-1,nC);
         return white-cov;
     };
-    // DP bottom up: 
-    int minimumWhiteTiles(string s, int nc, int l) {
+    // DP bottom up: 786ms
+    //https://leetcode.com/problems/minimum-white-tiles-after-covering-with-carpets/discuss/1863955/JavaC%2B%2BPython-DP-solution
+    int minimumWhiteTiles2(string s, int nc, int l) {
         int n = s.size();
         vector<vector<int>> dp(n + 1, vector<int>(nc + 1));
         for (int i = 1; i <= n; ++i) {
             for (int k = 0; k <= nc; ++k) {
-                int jump = dp[i - 1][k] + s[i - 1] - '0';
-                int cover = k > 0 ? dp[max(i - l, 0)][k - 1] : 1000;
+                int jump = dp[i - 1][k] + s[i - 1] - '0';  // count white tile if you skip this tile
+                int cover = k > 0 ? dp[max(i - l, 0)][k - 1] : 1000; // if you place tile end at current tile
                 dp[i][k] = min(cover, jump);
             }
         }
         return dp[n][nc];
     }
+    
+    
+    int minimumWhiteTiles(string s, int nc, int l) {
+        vector<vector<int>> mem(s.size()+1,vector(nc+1,-1));
+        function<int(int,int)> dp = [&](int pos,int t)
+        {
+            if(t<0)             return INT_MAX;
+            if(pos<0)           return 0;
+            if(mem[pos][t]!=-1) return mem[pos][t];
+            return mem[pos][t] = min(dp(pos-l,t-1),dp(pos-1,t)+ s[pos]-'0');
+        };
+        return dp(s.size()-1,nc);
+    }
+    
+//     def minimumWhiteTiles(self, floor, k, L):
+//         @lru_cache(None)
+//         def dp(i, t):
+//             if t < 0: return float("inf")
+//             if i < 0: return 0
+//             return min(dp(i - L, t - 1), dp(i - 1, t) + int(floor[i] == "1"))
+        
+//         return dp(len(floor) - 1, k)
 
 };
