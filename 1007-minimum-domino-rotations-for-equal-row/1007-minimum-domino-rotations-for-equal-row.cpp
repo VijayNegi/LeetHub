432d3c1d8c26c21f78edda9da24be0595e3d7d26
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-20 10:35:03 +0530

    Time: 120 ms (87.18%), Space: 111.7 MB (29.27%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n = tops.size();
+        vector<int> freqt(7,0),freqb(7,0);
+        for(auto& n :  tops)
+            ++freqt[n];
+        for(auto& n : bottoms )
+            ++freqb[n];
+        int face= -1;
+        int maxf = 0;
+        for(int i=1;i<=6;++i)
+            if(freqt[i]+freqb[i]>maxf)
+            {
+                maxf = freqt[i]+freqb[i];
+                face = i;
+            }
+        
+        if(maxf<n)
+            return -1;
+        int result=0;
+        
+        for(int i=0;i<n;++i)
+        {
+            if(freqt[face]>freqb[face])
+            {
+                if(tops[i]!=face)
+                {
+                    if(bottoms[i]==face)
+                        ++result;
+                    else
+                        return -1;
+                }
+            }
+            else
+            {
+                if(bottoms[i]!=face)
+                {
+                    if(tops[i]==face)
+                        ++result;
+                    else
+                        return -1;
+                }
+            }
+        }
+
+        return result;
+    }
+};
