4f03e0473db3b5b0a18fb7b3ed178e15a0703369
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-22 13:44:12 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (49.28%) - LeetHub

---
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        stringstream ss(input);
+        int longpath = 0;
+        
+        
+        stack<int> stk;
+        
+        string s;
+        while(!ss.eof())
+        {
+            int tabs=0;
+            getline(ss,s,'\n');
+           
+            while(s[tabs]=='\t')
+                ++tabs;
+            s = s.substr(tabs);
+            cout<<tabs<<" "<<s<<endl;
+            
+            while(stk.size()> tabs)
+                stk.pop();
+            
+            if(s.find('.')!= string::npos) //found a file
+            {
+                int path = stk.empty()? 0:stk.top() + stk.size();
+                path += s.size();
+                if(path>longpath)
+                    longpath = path;
+            }
+            else // found a folder
+            {
+                int parent = stk.empty()?0:stk.top();
+                stk.push(parent + s.size());
+            }
+        }
+        
+        return longpath;
+    }
+};
