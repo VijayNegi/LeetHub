683abf3450a75f450fa2cb67b57383b4891cc5b3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-23 13:16:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -13,4 +13,18 @@ public:
         }
         return ans + startValue-target;
     }
+    //https://leetcode.com/problems/broken-calculator/discuss/234484/JavaC%2B%2BPython-Change-Y-to-X-in-1-Line
+    // startvalue to target
+    int brokenCalc1(int X, int Y) {
+        int multiple = 1, res = 0;
+        while (X * multiple < Y)
+            multiple <<= 1, res++;
+        int diff = X * multiple - Y;
+        while (diff > 0) {  
+            res += diff / multiple;
+            diff -= diff / multiple * multiple;
+            multiple >>= 1;
+        }
+        return res;
+    }
 };
