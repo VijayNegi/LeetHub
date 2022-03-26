8296bd62a318fddfd730c178f61264e1392913ab
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-26 11:08:56 +0530

    Time: 50 ms (54.94%), Space: 27.5 MB (63.85%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    // using stl
+    int search1(vector<int>& nums, int target) {
         auto it = lower_bound(begin(nums),end(nums),target);
         if(it != nums.end())
         {
@@ -8,5 +9,22 @@ public:
                 return distance(nums.begin(),it);
         }
          return -1;   
+    }
+    // binary search
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,mid;
+        while(l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+            {
+                r = mid-1;
+            }
+            else
+                l = mid+1;
+        }
+        return -1;
     }
 };
