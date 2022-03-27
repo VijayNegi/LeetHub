c625d12de6cbb3ba6d05d5ee23935e63e9f8ad80
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-27 09:43:14 +0530

    Time: 26 ms (15.39%), Space: 10.6 MB (19.79%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,7 +1,7 @@
 using pi = pair<int,int>;
 class Solution {
 public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+    vector<int> kWeakestRows1(vector<vector<int>>& mat, int k) {
         int rows = mat.size();
         int cols = mat[0].size();
         auto cmp = [](pi& left, pi& right){ if(left.first == right.first) 
@@ -13,7 +13,6 @@ public:
         {
             int sum = 0;
             sum = accumulate(begin(mat[i]),end(mat[i]),sum);
-            //cout<<sum<<endl;
             q.push({sum,i});
         }
         vector<int> result;
@@ -21,9 +20,32 @@ public:
         {
             auto t = q.top();
             q.pop();
-            //cout<<t.first<<" "<<t.second<<endl;
             result.push_back(t.second);
         }
         return result;
+    }
+    
+    // use binary search for each row as solders are sorted
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
+    {
+      set<pair<int, int>> strength_index; // pairs of (strength, row index)
+      for (int i = 0; i < mat.size(); ++i) { // O(M)
+        strength_index.emplace(getStrength(mat[i]), i); // O(logN + logK) = O(log(NK))
+        if (strength_index.size() > k) // remove last item if exceeding size k
+          strength_index.erase(--strength_index.end());  
+      }  
+      
+      // form result array
+      vector<int> res;
+      for (auto& p : strength_index)
+        res.push_back(p.second);
+      return res;
+    }
+    
+    // get strength of a row: time complexity O(logN)
+    int getStrength(vector<int> & row) {
+      // find position of first 0 by binary search  
+      auto first0 = lower_bound(row.begin(), row.end(), 0, greater<int>());
+      return first0 - row.begin();  
     }
 };
