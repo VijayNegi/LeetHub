c5ea53671ded4851798782ee9b2a4f5604435b94
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-29 20:09:37 +0530

    Time: 4 ms (54.29%), Space: 8.7 MB (41.59%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
+    // AC but it makes more sense to sort citations in reverse
+    int hIndex2(vector<int>& citations) {
         int n = citations.size();
         sort(begin(citations),end(citations));
         int result=0;
@@ -10,5 +11,27 @@ public:
                 result = max(result,n-i);
         }
         return result;
+    }
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort(begin(citations),end(citations), greater<int>());
+        int result=0;
+        for(int i=0;i<n;++i)
+        {
+            if(citations[i] >= i+1)
+                result = max(result,i+1);
+        }
+        return result;
+    }
+    int hIndex1(vector<int>& citations) {
+        int n = citations.size(), h = 0;
+        int* counts = new int[n + 1]();
+        for (int c : citations)
+            counts[min(c, n)]++;
+        for (int i = n; i; i--) {
+            h += counts[i];
+            if (h >= i) return i;
+        }  
+        return h;
     }
 };
