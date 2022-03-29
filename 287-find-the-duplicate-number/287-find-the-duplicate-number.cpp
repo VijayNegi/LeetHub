b21b1d6c40959a61ca674a6565744d0e1136ec0c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-29 15:03:11 +0530

    Time: 135 ms (71.91%), Space: 61.3 MB (26.65%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // property of n natual numbers
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate1(vector<int>& nums) {
         int ans = -1;
         
         for(auto& a:nums)
@@ -19,5 +19,26 @@ public:
                 a = -1*a;
         
         return ans;
+    }
+    int findDuplicate(vector<int>& nums) {
+        int slow(0),fast(0);
+        
+        while(true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast)
+                break;
+        }
+        
+        slow = 0;
+        while(true)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+            if(slow==fast)
+                break;
+        }
+        return slow;
     }
 };
