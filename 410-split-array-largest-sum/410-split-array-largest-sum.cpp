77f77099e502a2efe1ddd74ebab2a29914e9dcb7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-01 01:24:25 +0530

    Time: 97 ms (23.77%), Space: 7.5 MB (21.98%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int m) {
+    // top down dp 296ms
+    int splitArray1(vector<int>& nums, int m) {
         int n = nums.size();
         vector<vector<int>> mem(n+1,vector(m+1,-1));
         function<int(int,int,int)> dfs = [&](int idx,int split,int sum)
@@ -23,5 +24,52 @@ public:
         };
         int sum = accumulate(begin(nums),end(nums),0);
         return dfs(0,m,sum);
+    }
+    
+    // bottom up 
+    
+    int memo[1001][51];
+    
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        
+        // Store the prefix sum of nums array
+        vector<int> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        
+        for (int subarrayCount = 1; subarrayCount <= m; subarrayCount++) {
+            for (int currIndex = 0; currIndex < n; currIndex++) {
+               // Base Case: If there is only one subarray left, then all of the remaining numbers
+               // must go in the current subarray. So return the sum of the remaining numbers.
+                if (subarrayCount == 1) {
+                    memo[currIndex][subarrayCount] = prefixSum[n] - prefixSum[currIndex];
+                    continue;
+                }
+
+                // Otherwise, use the recurrence relation to determine the minimum largest subarray
+                // sum between currIndex and the end of the array with subarrayCount subarray remaining.
+                int minimumLargestSplitSum = INT_MAX;
+                for (int i = currIndex; i <= n - subarrayCount; i++) {
+                    // Store the sum of the first subarray.
+                    int firstSplitSum = prefixSum[i + 1] - prefixSum[currIndex];
+
+                    // Find the maximum subarray sum for the current first split.
+                    int largestSplitSum = max(firstSplitSum, memo[i + 1][subarrayCount - 1]);
+
+                    // Find the minimum among all possible combinations.
+                    minimumLargestSplitSum = min(minimumLargestSplitSum, largestSplitSum);
+
+                    if (firstSplitSum >= minimumLargestSplitSum) {
+                        break;
+                    }
+                }
+
+                memo[currIndex][subarrayCount] = minimumLargestSplitSum;
+            }
+        }
+        
+        return memo[0][m];
     }
 };
