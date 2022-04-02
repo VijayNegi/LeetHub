adb9948f40dad64f0295c444864545e18d88b9bd
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-02 14:13:39 +0530

    Time: 57 ms (85.99%), Space: 19.4 MB (95.14%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool validPalindrome(string s) {
+    bool validPalindrome1(string s) {
         int l=0,r=s.size()-1;
         while(l<r)
             if(s[l]==s[r])
@@ -27,5 +27,15 @@ public:
         if(l1>=r1)
             return true;
         return false;
+    }
+    bool validPalindrome(string s) {
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+            if (s[i] != s[j]) {
+                int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+                while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
+                while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
+                return i1 >= j1 || i2 >= j2;
+            }
+        return true;
     }
 };
