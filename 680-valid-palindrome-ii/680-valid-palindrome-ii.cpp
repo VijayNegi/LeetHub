3c317f60b74981014dbe25542e7278f012f145d2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-02 14:11:21 +0530

    Time: 78 ms (34.22%), Space: 19.7 MB (71.13%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    bool validPalindrome1(string s) {
-        int n = s.size();
-        int i=0,j=n-1;
-        int k=1;
-        while(i<j)
-        {
-            if(s[i]==s[j])
-            {
-                ++i;--j;
-            }
-            else if(k>0)
-                ++i,--k;
-            else 
+    bool validPalindrome(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r)
+            if(s[l]==s[r])
+                ++l,--r;
+            else
                 break;
-                
-        }
-        if(i>=j)
+        if(l>=r)
             return true;
-        i=0,j=n-1,k=1;
-        while(i<j)
-        {
-            if(s[i]==s[j])
-            {
-                ++i;--j;
-            }
-           else if(k>0)
-                --j,--k;
-            else 
+        int l1=l+1;
+        int r1=r;
+        while(l1<r1)
+            if(s[l1]==s[r1])
+                ++l1,--r1;
+            else
                 break;
-        }
-        if(i>=j)
+        if(l1>=r1)
+            return true;
+        l1=l;r1=r-1;
+        while(l1<r1)
+            if(s[l1]==s[r1])
+                ++l1,--r1;
+            else
+                break;
+        if(l1>=r1)
             return true;
         return false;
-    }
-    bool validPalindrome(string s) {
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
-            if (s[i] != s[j]) {
-                int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
-                while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
-                while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
-                return i1 >= j1 || i2 >= j2;
-            }
-        return true;
     }
 };
