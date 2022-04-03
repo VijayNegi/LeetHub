65f0324c1267ccc102f1c8f62a765b3dca0202b7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-03 16:06:58 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     // 7 ms
-    void nextPermutation1(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i=n-2;
-        for(i=n-2;i>=0;--i)
-        {
-            if(nums[i]<nums[i+1])
+        int i = n-2;
+        for(;i>=0;--i)
+            if(nums[i+1]>nums[i])
             {
                 int j=i+1;
                 for(;j<n;++j)
@@ -15,7 +14,6 @@ public:
                 swap(nums[i],nums[j-1]);
                 break;
             }
-        }
         reverse(begin(nums)+i+1,end(nums));
     }
     // 3 ms
@@ -23,7 +21,7 @@ public:
       std::next_permutation(nums.begin(), nums.end());
     }
     // some more STL
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation3(vector<int>& nums) {
       auto dest = std::is_sorted_until(nums.rbegin(), nums.rend());
       if (dest != nums.rend()) {
         auto target = std::upper_bound(nums.rbegin(), dest, *dest);
