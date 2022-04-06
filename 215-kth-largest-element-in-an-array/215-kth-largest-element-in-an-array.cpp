7309327c29b2d04d3e81f125ca427b70bcf35c0f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-07 02:06:39 +0530

    Time: 169 ms (7.89%), Space: 10 MB (85.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -22,7 +22,7 @@ public:
         return pq.top();
     }
     // min heap multiset :  4 ms
-    int findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest4(vector<int>& nums, int k) {
         multiset<int> mset;
         for (int num : nums) {
             mset.insert(num);
@@ -31,6 +31,40 @@ public:
             }
         }
         return *mset.begin();
+    }
+    // quick select : 
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1, kth;
+        while (true) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                kth = nums[idx];
+                break;
+            }
+            if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return kth;
+    }
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) {
+                l++;
+            }
+            if (nums[r] <= pivot) {
+                r--;
+            }
+        }
+        swap(nums[left], nums[r]);
+        return r;
     }
 
 };
