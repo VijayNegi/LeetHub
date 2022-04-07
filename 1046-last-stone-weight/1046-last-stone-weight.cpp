b49b34eab6323c06c5c2dd2abb61ae713d78a0da
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-07 16:23:45 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -6,10 +6,8 @@ public:
         {
             int s1 = pq.top(); pq.pop();
             int s2 = pq.top(); pq.pop();
-            if(s1==s2)
-                continue;
-            else
-                pq.push(s1-s2);
+            if(s1>s2)
+                pq.push(s1-s2);    
         }
         return pq.size()? pq.top():0;
     }
