2677cc25a9d4713f81a266863f889bf011bf6211
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-09 01:52:50 +0530

    Time: 104 ms (51.78%), Space: 44 MB (9.86%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
+    // 90 ms
+    // https://leetcode.com/problems/task-scheduler/discuss/104504/C%2B%2B-8lines-O(n)
+    int leastInterval1(vector<char>& tasks, int n) {
         unordered_map<char,int> m;
         int mx = 0;
         for(auto& c: tasks)
@@ -12,5 +14,37 @@ public:
         for(auto it:m)
             if(it.second == mx) ++result;
         return max(result,(int)tasks.size());
+    }
+    
+    // emulate 
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> counts;
+        for (char t : tasks) {
+            counts[t]++;
+        }
+        priority_queue<int> pq;
+        for (pair<char, int> count : counts) {
+            pq.push(count.second);
+        }
+        int alltime = 0;
+        int cycle = n + 1;
+        while (!pq.empty()) {
+            int time = 0;
+            vector<int> tmp;
+            for (int i = 0; i < cycle; i++) {
+                if (!pq.empty()) {
+                    tmp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for (int cnt : tmp) {
+                if (--cnt) {
+                    pq.push(cnt);
+                }
+            }
+            alltime += !pq.empty() ? cycle : time;
+        }
+        return alltime;
     }
 };
