47048ea649b2415a1d2f1b56c1ad5046df195b8d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-09 16:53:34 +0530

    Time: 42 ms (5.89%), Space: 13.5 MB (89.57%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,5 +1,84 @@
 class Solution {
 public:
+    // nlogn
+    vector<int> topKFrequent1(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        
+        for(auto& n:nums)
+            count[n]++;
+        function<bool(int,int)> cmp = [&](int a,int b){ if(count[a]<count[b])
+                                                  return true;
+                                                 return false; };
+        priority_queue<int,vector<int>, decltype(cmp)> q(cmp);
+        
+        for(auto& [n,c]:count)
+        {
+            q.push(n);
+        }
+        
+        vector<int> result;
+        while(k--)
+        {
+            result.push_back(q.top());
+            q.pop();
+        }
+        
+        return result;
+        
+    }
+    // nlogk: 16ms
+    vector<int> topKFrequent2(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for(auto& n:nums)
+            count[n]++;
+        function<bool(int,int)> cmp = [&](int a,int b){ if(count[a]>count[b])
+                                                  return true;
+                                                 return false; };
+        priority_queue<int,vector<int>, decltype(cmp)> q(cmp);
+        
+        for(auto& [n,c]:count)
+        {
+            q.push(n);
+            if(q.size()>k)
+                q.pop();
+        }
+        
+        vector<int> result;
+        while(!q.empty())
+        {
+            result.push_back(q.top());
+            q.pop();
+        }
+        
+        return result;
+    }
+    // O(n) : 12 ms , note : other solution is quickselect which is also O(n) but little complex to impl
+    vector<int> topKFrequent3(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for(auto& n:nums)
+            count[n]++;
+        
+        unordered_map<int,vector<int>> freq;
+        
+        for(auto& [n,c]:count)
+        {
+            freq[c].push_back(n);
+        }
+        vector<int> result;
+        for(int i=nums.size() ;i>0;--i)
+        {
+            if(freq.count(i))
+            {
+                result.insert(result.end(),begin(freq[i]),end(freq[i]));
+                if(result.size()>=k)
+                    break;
+            }
+        }
+        
+        return result;
+    }
+    
+    //quick-select: 20ms
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
         for(auto n:nums)
@@ -13,12 +92,9 @@ public:
         while(true)
         {
             int p = partition(t,l,r,mp);
-            if(p==k-1)
-                break;
-            else if(p>k-1)
-                r=p-1;
-            else
-                l=p+1;
+            if(p==k-1)          break;
+            else if(p>k-1)      r = p-1;
+            else                l = p+1;
         }
         t.resize(k);
         return t;
@@ -33,12 +109,12 @@ public:
         {
             if(mp[v[l]]< mp[v[pivot]] && mp[v[r]] > mp[v[pivot]])
                 swap(v[l++],v[r--]);
-            else if(mp[v[l]] >= mp[v[pivot]])
-                ++l;
-            else if(mp[v[r]] <= mp[v[pivot]])
-                --r;
+            else if(mp[v[l]] >= mp[v[pivot]])     ++l;
+            else if(mp[v[r]] <= mp[v[pivot]])     --r;
         }
         swap(v[r],v[pivot]);
         return r;
     }
+    
 };
+
