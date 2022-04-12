d2314e713b42bb7baa862e42b2a7a77cc7985f1f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-12 19:59:48 +0530

    Time: 0 ms (100.00%), Space: 7 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+     // Space O(1)
     void gameOfLife(vector<vector<int>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        vector<vector<int>> dir{{-1,-1},{-1,0},{-1,+1}, 
+                                {0,-1},{0,+1},
+                                {1,-1},{1,0},{1,+1}};
+        for(int i=0;i < rows;++i)
+            for(int j=0;j<cols;++j)
+            {
+                int neighbours = 0;
+                for(auto& n:dir)
+                {
+                    int r = i+n[0],c = j+n[1];
+                    if(r>=0 && r<rows && c>=0 && c<cols)
+                        neighbours += board[r][c] & 1;
+                }
+                if(neighbours == 2)
+                    board[i][j] |= board[i][j]<<1;
+                else if(neighbours== 3)
+                    board[i][j] |= 2;
+            }
+        for(int i=0;i<rows;++i)
+            for(int j=0;j<cols;++j)
+                board[i][j] = board[i][j]>>1;
+        
+    }
+    // Space: O(n)
+    void gameOfLife1(vector<vector<int>>& board) {
         int rows = board.size();
         int cols = board[0].size();
         vector<vector<int>> count(rows,vector(cols,0));
@@ -39,6 +67,6 @@ public:
                 else if(count[i][j]>3)
                     board[i][j]=0;
             }
-        
     }
+   
 };
