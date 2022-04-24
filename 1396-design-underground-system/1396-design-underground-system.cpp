dda4e0583f293875be9ed978c6a85a9e93497326
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-24 15:03:36 +0530

    Time: 169 ms (75.17%), Space: 58.8 MB (70.77%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,42 +1,24 @@
-using psi = pair<string,int>;
 class UndergroundSystem {
-    unordered_map<int,psi> tmap;
-    unordered_map<string,vector<double>> avg;
+    unordered_map<int,pair<string,int>> journy_start;
+    unordered_map<string,pair<double,int>> journy_avg;
 public:
     UndergroundSystem() {
-        
     }
     
     void checkIn(int id, string stationName, int t) {
-        tmap[id] = {stationName,t};
+        journy_start[id] = make_pair(stationName,t);
     }
     
     void checkOut(int id, string stationName, int t) {
-        string str = tmap[id].first+":"+stationName;
-        double time = t - tmap[id].second;
-        if(avg[str].size())
-        {
-            avg[str][0] += time;
-            avg[str][1]++;
-        }
-        else
-        {
-            avg[str] = {time,1};
-        }
-        
+        int travel_time = t - journy_start[id].second;
+        string key = journy_start[id].first + "," + stationName;
+        journy_avg[key].first += travel_time;
+        journy_avg[key].second += 1;
+        journy_start.erase(id);
     }
     
     double getAverageTime(string startStation, string endStation) {
-        string str = startStation+":"+endStation;
-        double total = avg[str][0];
-        return total/avg[str][1];
+        string key = startStation + "," + endStation;
+        return journy_avg[key].first/ journy_avg[key].second;
     }
 };
-
-/**
- * Your UndergroundSystem object will be instantiated and called as such:
- * UndergroundSystem* obj = new UndergroundSystem();
- * obj->checkIn(id,stationName,t);
- * obj->checkOut(id,stationName,t);
- * double param_3 = obj->getAverageTime(startStation,endStation);
- */
