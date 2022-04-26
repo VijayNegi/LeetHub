89831ef4f7079abc218cca254e675a9fc8b1e4e6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-26 17:38:30 +0530

    Time: 185 ms (86.64%), Space: 42.1 MB (79.88%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,8 +1,8 @@
-using wto = pair<int,int>;
+
 class Solution {
 public:
-    // MST : 1811ms
-    int minCostConnectPoints(vector<vector<int>>& points) {
+    // MST : using vector :1811ms , using pair<int,int>: 181ms
+    int minCostConnectPoints1(vector<vector<int>>& points) {
         int n = points.size();
         vector<int> visited(n,false);
         //priority_queue<vector<int>,vector<vector<int>>, greater<>> pq;
@@ -15,7 +15,6 @@ public:
             auto v = pq.top();
             pq.pop();
             int& node = v.second;
-            //int& node = v[1];
             if(visited[node])
                 continue;
            
@@ -33,48 +32,25 @@ public:
                     pq.push({d,i});
                 }
             }
-            
         }
         return cost;
     }
-    // MST: 194ms
-     int minCostConnectPoints1(vector<vector<int>>& points) {
-        int n = points.size();
-        auto cmd = [&](wto& left, wto& right){ return left.second > right.second;};
-        priority_queue<wto,vector<wto>, decltype(cmd)> pq(cmd);
-        vector<bool> visited(n,false);
-        int count =0;
-        int p = 0; // point index
-         pq.push({0,0});
-        function<void(int)> pushnegighbours = [&](int node)
-        {
-            for(int i=0;i<n;++i)
-            {
-                if(!visited[i] && i!=node)
-                {
-                    int dist = abs(points[node][0] - points[i][0] ) + abs(points[node][1] - points[i][1]);
-                    pq.push({i,dist});
-                }
-            }
-            
-        };
-        int cost = 0;
-        while(!pq.empty())
-        {
-            auto currp = pq.top();
+    // Prims's clean 
+    int minCostConnectPoints(vector<vector<int>>& ps) {
+        int n = ps.size(), res = 0, i = 0, connected = 0;
+        vector<bool> visited(n);
+        priority_queue<pair<int, int>> pq;
+        while (++connected < n) {
+            visited[i] = true;
+            for (int j = 0; j < n; ++j)
+                if (!visited[j])
+                    pq.push({-(abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1])), j});
+            while (visited[pq.top().second])
+                pq.pop();
+            res -= pq.top().first;
+            i = pq.top().second;
             pq.pop();
-            
-            if(visited[currp.first])
-                continue;
-            
-            visited[currp.first] = true;
-            cost += currp.second;
-            ++count;
-            if(count==n)
-                break;
-            
-            pushnegighbours(currp.first);
         }
-        return cost;
+        return res;
     }
 };
