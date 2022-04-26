2fdd67d088f89ccaea7878b280cefee3dd16cbde
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-26 17:27:02 +0530

    Time: 194 ms (85.37%), Space: 42.1 MB (82.77%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,6 +1,8 @@
+using wto = pair<int,int>;
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
+    // MST : 1811
+    int minCostConnectPoints1(vector<vector<int>>& points) {
         int n = points.size();
         vector<int> visited(n,false);
         priority_queue<vector<int>,vector<vector<int>>, greater<>> pq;
@@ -27,6 +29,46 @@ public:
             ++count;
             if(count==n)
                 break;
+        }
+        return cost;
+    }
+    
+     int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        auto cmd = [&](wto& left, wto& right){ return left.second > right.second;};
+        priority_queue<wto,vector<wto>, decltype(cmd)> pq(cmd);
+        vector<bool> visited(n,false);
+        int count =0;
+        int p = 0; // point index
+         pq.push({0,0});
+        function<void(int)> pushnegighbours = [&](int node)
+        {
+            for(int i=0;i<n;++i)
+            {
+                if(!visited[i] && i!=node)
+                {
+                    int dist = abs(points[node][0] - points[i][0] ) + abs(points[node][1] - points[i][1]);
+                    pq.push({i,dist});
+                }
+            }
+            
+        };
+        int cost = 0;
+        while(!pq.empty())
+        {
+            auto currp = pq.top();
+            pq.pop();
+            
+            if(visited[currp.first])
+                continue;
+            
+            visited[currp.first] = true;
+            cost += currp.second;
+            ++count;
+            if(count==n)
+                break;
+            
+            pushnegighbours(currp.first);
         }
         return cost;
     }
