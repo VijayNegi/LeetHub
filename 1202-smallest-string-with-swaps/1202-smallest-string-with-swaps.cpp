5bfe1dd592f61e2d01ac44cb7b81b39bcb6abdea
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-27 18:35:29 +0530

    Time: 189 ms (78.63%), Space: 49.1 MB (81.57%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -26,7 +26,8 @@ struct DSU
 
 class Solution {
 public:
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+    // my solution: 263ms
+    string smallestStringWithSwaps1(string s, vector<vector<int>>& pairs) {
         int n = s.size();
         DSU dset(n);
         for(auto v:pairs)
@@ -45,5 +46,30 @@ public:
             gmap[dset.find(i)].erase(it);
         }
         return result;
+    }
+    
+    int find(vector<int>& ds, int i) {
+      return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
+    }
+    // avoiding multiset
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+      vector<int> ds(s.size(), -1);
+      vector<vector<int>> m(s.size());
+      for (auto& p : pairs) {
+        auto i = find(ds, p[0]), j = find(ds, p[1]);
+        if (i != j) 
+            ds[j] = i;
+      }
+      for (auto i = 0; i < s.size(); ++i) 
+          m[find(ds, i)].push_back(i);
+      for (auto &ids : m) {
+        string ss = "";
+        for (auto id : ids) 
+            ss += s[id];
+        sort(begin(ss), end(ss));
+        for (auto i = 0; i < ids.size(); ++i) 
+            s[ids[i]] = ss[i];
+      }
+      return s;
     }
 };
