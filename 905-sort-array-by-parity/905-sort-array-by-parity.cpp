ef810e05904885ee6c8abead12a6c4299bc1cec4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-02 16:43:06 +0530

    Time: 8 ms (88.01%), Space: 16.3 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
+    // stl : 13 ms
+    vector<int> sortArrayByParity1(vector<int>& nums) {
         auto cmp = [](int l,int r){if ((l & 1) < (r&1))
                                    return true;
                                    return false;};
         sort(begin(nums),end(nums),cmp);
+        return nums;
+    }
+    //two-pointers: 
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size()-1;
+        int l=0,r=n;
+        while(l<r)
+        {
+            while(l<r && (nums[l] &1) ==0)
+                ++l;
+            while(l<r && (nums[r] &1) ==1)
+                --r;
+            swap(nums[l],nums[r]);
+            ++l,--r;
+        }
         return nums;
     }
 };
