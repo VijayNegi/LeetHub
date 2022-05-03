dcca0e180353a5b9221bec46bb2122c0986b3170
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-04 00:39:28 +0530

    Time: 36 ms (73.26%), Space: 26.5 MB (68.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findUnsortedSubarray(vector<int>& nums) {
+    int findUnsortedSubarray1(vector<int>& nums) {
         int n = nums.size();
         if(n==1)
             return 0;
@@ -23,9 +23,30 @@ public:
             else
                 l=i;
         }
-        //cout<<l<<" "<<r<<endl;
         if(l<r)
             return r-l+1;
         return 0;
+    }
+    // Think of problem as - In ideal sorted array. 
+    // Every element from right to left will be smaller.
+    // Every element from left to right will be larger.
+    // finding atual position of Min by going right to left you will get start.
+    // finding atual position of max by going left to right you will get end.
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int minV = nums[n-1];
+        int maxV = nums[0];
+        int start= -1;
+        int end = -2;
+        
+        for(int i=0;i<n;++i)
+        {
+            minV = min(nums[n-1-i],minV);
+            maxV = max(nums[i],maxV);
+            if(nums[i] < maxV) end = i;
+            if(nums[n-1-i]>minV) start = n-1-i;
+        }
+        
+        return end - start +1;
     }
 };
