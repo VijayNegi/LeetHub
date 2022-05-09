3801b7a978774259523597f17d56249963cc0170
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-09 23:52:15 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
+    vector<string> letterCombinations1(string digits) {
+        vector<string> m{"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> result;
+        string s;
+        int n = digits.size();
+        if(!n)  return result;
+        function<void(int)> dfs = [&](int pos)
+        {
+            if(pos==n)
+            {
+                result.push_back(s);
+                return;
+            }
+            int i = digits[pos]-'0';
+            for(auto ch: m[i])
+            {
+                s.push_back(ch);
+                dfs(pos+1);
+                s.pop_back();
+            }
+        };
+        dfs(0);
+        return result;
+    }
+    
     vector<string> letterCombinations(string digits) {
-        if(digits.empty())
-            return {};
+        if(digits.empty())  return {};
         vector<string> mp{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string> result{""};
-        for(auto c:digits)
+        for(auto d:digits)
         {
-            int d = c-'0';
             vector<string> temp;
-            for(auto& ch:mp[d])
+            for(auto& ch:mp[d-'0'])
                 for(auto& s:result)
                     temp.push_back(s+ch);
-            swap(result,temp);
+            result = move(temp);
         }
         return result;
     }
