b4c585b3134c9bf9bf8ecb42fbe481ce2c7f0918
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-09 23:48:35 +0530

    Time: 5 ms (16.72%), Space: 6.5 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    vector<string> letterCombinations1(string digits) {
-        vector<string> m{"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string> result;
-        string s;
-        int n = digits.size();
-        if(!n)  return result;
-        function<void(int)> dfs = [&](int pos)
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        vector<string> mp{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> result{""};
+        for(auto c:digits)
         {
-            if(pos==n)
-            {
-                result.push_back(s);
-                return;
-            }
-            int i = digits[pos]-'0';
-            for(auto ch: m[i])
-            {
-                s.push_back(ch); //s.append(1,ch);
-                dfs(pos+1);
-                s.pop_back();
-            }
-        };
-        dfs(0);
+            int d = c-'0';
+            vector<string> temp;
+            for(auto& ch:mp[d])
+                for(auto& s:result)
+                    temp.push_back(s+ch);
+            swap(result,temp);
+        }
         return result;
     }
-    vector<string> letterCombinations(string digits) {
-            if(digits == "") return {};
-            vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-            for(auto digit : digits){
-                vector<string> extendCombination;
-                for(auto& currentCombination : ans)
-                    for(auto newChar : mappings[digit - '2'])
-                        extendCombination.push_back(currentCombination + newChar);                            
-                ans = move(extendCombination);
-            }
-            return ans;
-        }
 };
