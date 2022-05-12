f1722cd2f8470b665c1c8b74947cfc36459cb70c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-12 18:56:46 +0530

    Time: 18 ms (40.84%), Space: 9.3 MB (56.98%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> st;
+    //stl: 3 ms
+    vector<vector<int>> permuteUnique1(vector<int>& nums) {
         vector<vector<int>> result;
         std::sort(nums.begin(), nums.end());
         do {
             result.push_back(nums);
         } while(std::next_permutation(nums.begin(), nums.end()));
         return result;
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        set<vector<int>> st;
+        vector<int> mp(21,0);
+        for(auto n:nums)
+            mp[n+10]++;
+        
+        vector<int> res;
+        function<void(int)> dfs = [&](int n){
+            if(n==0)
+            {
+                st.insert(res);
+                return;
+            }
+            
+            for(int i=0;i<=20;++i)
+            {
+                if(mp[i]!=0)
+                {
+                    res.push_back(i-10);
+                    mp[i]--;
+                    dfs(n-1);
+                    mp[i]++;
+                    res.pop_back();
+                }
+            }
+        };
+        dfs(nums.size());
+        return vector(st.begin(),st.end());
     }
 };
