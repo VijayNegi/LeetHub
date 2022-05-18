2e941bdf7be4105ba18df3dc0aa6918bb65351b9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-18 14:52:57 +0530

    Time: 683 ms (78.43%), Space: 181.1 MB (45.79%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
+    // Tarjans algorithm
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        // build adjacency list
         vector<vector<int>> adj(n);
-        for(auto ed:connections) {
-            adj[ed[0]].push_back(ed[1]);
-            adj[ed[1]].push_back(ed[0]);
+        for(auto c: connections)
+        {
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
         }
-        
-        vector<int> idx(n,-1);
-        vector<int> low(n,-1);
-        int id = 0;
-        vector<vector<int>> result;
-        function<void(int,int)> dfs = [&](int node,int parent) {
-            
-            if(idx[node] != -1)
+        int t=0;
+        vector<int> id(n,-1), low(n,-1);
+        vector<vector<int>> critical;
+        function<void(int,int)> dfs_targen = [&](int v,int parent)
+        {
+            if(id[v]>=0)
                 return;
-            //cout<<node<<endl;
-            idx[node] = id++;
-            low[node] = idx[node];// lowest self rechable
-            for(auto ed:adj[node]) {
-                if(ed == parent)
+            id[v] = t++;
+            low[v] = id[v];
+            for(auto n: adj[v])
+            {
+                if(n==parent)
                     continue;
-                dfs(ed,node);
-                low[node] = min(low[node],low[ed]);
-                if(idx[node] < low[ed])
-                    result.push_back({node,ed});
+                dfs_targen(n,v);
                 
+                low[v] = min(low[v],low[n]);
+                if(id[v] < low[n])     // important as child was never able to reach back i.e. new SSC and count it ( no back link )
+                    critical.push_back({v,n});
             }
-            //cout<<node<<" "<<low[node]<<endl;
         };
-        dfs(0,-1);
-        return result;
+
+        dfs_targen(0,-1);
+        return critical;
     }
 };
