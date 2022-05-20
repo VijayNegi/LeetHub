ebcf28261a26af64629de55718b818553d5fb23c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-20 11:31:18 +0530

    Time: 3 ms (80.74%), Space: 7.8 MB (41.40%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obsG) {
+        int rows = obsG.size();
+        int cols = obsG[0].size();
+        // if(rows==1 && cols==1)
+        //     return !obsG[0][0];
+        vector<vector<int>> dp(rows,vector(cols,0));
+        if(obsG[0][0]==0)
+            dp[0][0] = 1;
+        for(int r= 0;r<rows;++r)
+            for(int c=0;c<cols;++c)
+            {
+                if(obsG[r][c]==1)
+                    continue;
+                if(c-1>=0)
+                    dp[r][c] += dp[r][c-1];
+                 if(r-1>=0)
+                    dp[r][c] += dp[r-1][c];
+            }
+        return dp[rows-1][cols-1];
+        
+    }
+};
