a90a37ecea7fa9bbf6f597372dca12ba9c0cabb3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-20 11:33:51 +0530

    Time: 3 ms (80.74%), Space: 7.7 MB (60.56%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obsG) {
+    // dp n*n
+    int uniquePathsWithObstacles1(vector<vector<int>>& obsG) {
         int rows = obsG.size();
         int cols = obsG[0].size();
-        // if(rows==1 && cols==1)
-        //     return !obsG[0][0];
         vector<vector<int>> dp(rows,vector(cols,0));
         if(obsG[0][0]==0)
             dp[0][0] = 1;
@@ -18,7 +17,23 @@ public:
                  if(r-1>=0)
                     dp[r][c] += dp[r-1][c];
             }
-        return dp[rows-1][cols-1];
-        
+        return dp[rows-1][cols-1];   
+    }
+    // dp n
+     int uniquePathsWithObstacles(vector<vector<int>>& obsG) {
+        int rows = obsG.size();
+        int cols = obsG[0].size();
+        vector<int> dp(cols,0);
+        if(obsG[0][0]==0)
+            dp[0] = 1;
+        for(int r= 0;r<rows;++r)
+            for(int c=0;c<cols;++c)
+            {
+                if(obsG[r][c]==1)
+                    dp[c] = 0;
+                else if(c-1>=0)
+                    dp[c] += dp[c-1];
+            }
+        return dp[cols-1];   
     }
 };
