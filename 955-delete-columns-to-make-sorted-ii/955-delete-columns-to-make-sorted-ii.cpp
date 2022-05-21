267fb13b43369cb84ae52c57aec93c3a028d8b3d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-21 21:08:57 +0530

    Time: 6 ms (90.71%), Space: 10 MB (69.95%) - LeetHub

--- 955-delete-columns-to-make-sorted-ii/955-delete-columns-to-make-sorted-ii.cpp
+++ 955-delete-columns-to-make-sorted-ii/955-delete-columns-to-make-sorted-ii.cpp
@@ -1,15 +1,16 @@
 // does not work
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
+    // self : 4ms
+    int minDeletionSize1(vector<string>& strs) {
         int n = strs[0].size();
         int strn = strs.size();
-        vector<int> skip(strn,false);
+        vector<int> skip(strn,false); // skip comparison
         int result=0;
         for(int i=0;i<n;++i) {
             int j;
             bool next = false;
-            vector<int> temp(strn,false);
+            vector<int> temp(strn,false); 
             for(j=1;j<strs.size();++j) {
                 if(skip[j])
                     continue;
@@ -23,14 +24,32 @@ public:
                 else
                     temp[j] = true;
             }
-            if(j==strn) {
+            if(j==strn) {   // comparison result of this position only matters if this position was not removed 
                 for(int t=1;t<strn;++t) {
                     skip[t] = skip[t] || temp[t]; 
                 }     
             }  
-            if(!next)
+            if(!next)   //  already lexicographic
                 break;
         }
         return result;
+    }
+    // just cleaner: 
+    int minDeletionSize(vector<string>& A) {
+        int res = 0, n = A.size(), m = A[0].length(), i, j;
+        vector<bool> sorted(n - 1, false);
+        for (j = 0; j < m; ++j) {
+            for (i = 0; i < n - 1; ++i) {
+                if (!sorted[i] && A[i][j] > A[i + 1][j]) {
+                    res++;
+                    break;
+                }
+            }
+            if (i < n - 1) continue;
+            for (i = 0; i < n - 1; ++i) {
+                sorted[i] = sorted[i] || A[i][j] < A[i + 1][j];
+            }
+        }
+        return res;
     }
 };
