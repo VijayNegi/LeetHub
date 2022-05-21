dffc091bde10bfb4cb74aa99d903f148628f769c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-21 16:39:14 +0530

    Time: 4 ms (60.99%), Space: 6.6 MB (49.88%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> mp(256,0);
+        for(auto& c:s)
+            mp[c]++;
+        bool odd = false;
+        int count = 0;
+        for(auto k:mp) {
+            count += (k/2)*2;
+            if(k%2)
+                odd = true;
+        }
+        return count + odd;
+    }
+};
