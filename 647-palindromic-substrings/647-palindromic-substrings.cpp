65e0a32091bceb09b2ebec1f6b39fadc3be8b703
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-22 20:53:38 +0530

    Time: 7 ms (72.70%), Space: 6.9 MB (63.59%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -21,7 +21,8 @@ public:
         }
         return result;
     }
-    int countSubstrings(string s) {
+    //cleaner
+    int countSubstrings2(string s) {
         int counter = 0, n = s.size();
         for (int i = 0; i < n; i++) {
             for (int l = i, r = i; l >= 0 && r < n && s[l] == s[r]; l--, r++) {
@@ -33,4 +34,34 @@ public:
         }
         return counter;
     }
+    int countSubstrings(string s) {
+        string t;
+        for(auto c: s) {
+            t += string("#") + c;
+        }
+        auto res = manacher_odd(t + "#");
+        int result = 0;
+        for(auto p:res)
+            result += p/2; //this is need because of current implimentation
+        return result;
+    }
+    
+    vector<int> manacher_odd(string s) {
+        int n = s.size();
+        string str = "^"+s+"$";
+        int l=1,r=1;
+        vector<int> p(n+2);
+        for(int i=1;i<=n;++i) {
+            p[i] = max(0, min(r-i,p[l+(r-i)]));
+            while(str[i-p[i]] == str[i+p[i]])
+                ++p[i];
+            
+            if(i+p[i]>r) {
+                l = i-p[i];
+                r = i+p[i];
+            }
+        }
+        return vector<int>(begin(p) + 1, end(p) - 1);
+    }
+    
 };
