f4e24ca1c6c866a5e7d4bc311401dfa67fd943b1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-25 01:22:03 +0530

    Time: 3 ms (89.02%), Space: 7.3 MB (67.96%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -24,8 +24,8 @@ public:
         }
         return result;
     }
-    // stack
-    int longestValidParentheses(string s) {
+    // stack : 7ms
+    int longestValidParentheses2(string s) {
         stack<int> stk;
         stk.push(-1);
         int maxL=0;
@@ -42,4 +42,24 @@ public:
         }
         return maxL;
     }
+    // dp :
+    int longestValidParentheses(string s) {
+        int maxans = 0;
+        int n = s.size();
+        vector<int> dp(n,0);
+        for (int i = 1; i < n; i++) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } 
+                else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    
+    }
+    
 };
