aec74bb9cefedffb326b47be58decb52f9c9e32f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-25 01:14:24 +0530

    Time: 7 ms (47.28%), Space: 7.3 MB (67.96%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) {
+    // self : 12ms
+    int longestValidParentheses1(string s) {
         int n = s.size();
         vector<int> pos;
         int result = 0;
@@ -22,5 +23,23 @@ public:
             }
         }
         return result;
+    }
+    // stack
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int t=stk.top();
+            if(t!=-1&&s[i]==')'&&s[t]=='(')
+            {
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else
+                stk.push(i);
+        }
+        return maxL;
     }
 };
