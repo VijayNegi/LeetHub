5c37ef1e131e2a0dd3d145f3e7e5f498eb806664
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-26 01:06:29 +0530

    Time: 525 ms (42.35%), Space: 77.6 MB (44.26%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     // lis
+    //https://leetcode.com/problems/russian-doll-envelopes/discuss/82796/A-Trick-to-solve-this-problem.
+    // https://leetcode.com/problems/russian-doll-envelopes/discuss/82763/Java-NLogN-Solution-with-Explanation
     int maxEnvelopes(vector<vector<int>>& env) {
         sort(begin(env),end(env),[](vector<int>& l,vector<int>& r){
             if(l[0]==r[0])
@@ -17,5 +19,17 @@ public:
                 *it = e[1];
         }
         return lis.size();
+    }
+    
+    // DP n^2 : TLE
+    int maxEnvelopes1(vector<pair<int, int>>& envelopes) {
+        if (envelopes.empty()) return 0;
+        sort(envelopes.begin(), envelopes.end());
+        vector<int> dp(envelopes.size(), 1);
+        for (int i = 0; i < envelopes.size(); ++i)
+            for (int j = 0; j < i; ++j)
+                if (envelopes[j].first < envelopes[i].first && envelopes[j].second < envelopes[i].second)
+                    dp[i]  = max(dp[i] , dp[j] + 1);
+        return *max_element(dp.begin(), dp.end());
     }
 };
