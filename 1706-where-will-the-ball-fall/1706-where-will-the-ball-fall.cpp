8b0aef93d98cae8ab83f3d980fa42fa017fe961b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-28 14:37:39 +0530

    Time: 23 ms (91.34%), Space: 13.1 MB (84.25%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<int> result(cols,-1);
+        
+        for(int c=0;c<cols;++c) {
+            int c1 = c;
+            int r1 = 0;
+            for(;r1<rows;++r1) {
+                if(grid[r1][c1] == 1) {
+                    if(c1<cols-1 && grid[r1][c1+1]== 1)
+                        ++c1;
+                    else break;
+                }
+                else if(grid[r1][c1] == -1) {
+                    if(c1>0 && grid[r1][c1-1]==-1)
+                        --c1;
+                    else break;
+                }
+            }
+            if(r1==rows)
+                result[c] = c1;
+        }
+        return result;
+    }
+};
