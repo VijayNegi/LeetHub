11bebe825e538c15b2aaaba18963c052e5dc9684
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-31 13:21:21 +0530

    Time: 3 ms (67.47%), Space: 6.2 MB (5.18%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -42,7 +42,7 @@ public:
         return (A > 0) == (B > 0) ? res : -res;
     }
 
-    int divide(int dividend, int divisor) {
+    int divide3(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
         }
@@ -58,5 +58,53 @@ public:
             ans += m;
         }
         return sign * ans;
+    }
+    // official solution
+    int divide(int dividend, int divisor) {
+                if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        
+        int negative = 2;
+        if(dividend > 0)
+        {
+            dividend = -dividend;
+            negative--;
+        }
+        
+        if(divisor > 0)
+        {
+            divisor = -divisor;
+            negative--;
+        }
+        
+        int quotient = 0;
+        
+        vector<int> powOfTwos;
+        vector<int> values;
+        
+        int powOfTwo = -1;
+        int value = divisor;
+        while(dividend <= value)
+        {
+            values.push_back(value);
+            powOfTwos.push_back(powOfTwo);
+            if (value < INT_MIN/2) {
+                break;
+            }
+            value *= 2;
+            powOfTwo *= 2;
+        }
+        
+        for(int i=values.size()-1; i>=0;i--)
+        {
+            if(dividend <= values[i])
+            {
+                dividend -= values[i];
+                quotient += powOfTwos[i];
+            }
+        }
+        
+        return (negative!= 1 ? -quotient: quotient);
     }
 };
