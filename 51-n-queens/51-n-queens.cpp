3f7edd6c3f7f05aa266c36013493349012922755
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-04 21:52:05 +0530

    Time: 7 ms (75.47%), Space: 7.2 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -2,39 +2,34 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> result;
-        vector<string> r(n,string(n,'.'));
+        vector<string> board(n,string(n,'.'));
         function<bool(int,int)> valid = [&]( int l,int i) {
             // check down
             for(int k=l-1;k>=0;--k)
-                if(r[k][i]=='Q') {
-
+                if(board[k][i]=='Q') 
                     return false;
-                }
             //check down right
             for(int k=l-1,c=i+1;k>=0 && c<n;--k,++c)
-                if(r[k][c]=='Q') {
+                if(board[k][c]=='Q') 
                     return false;
-                }
             // check down left
             for(int k=l-1,c=i-1;k>=0 && c>=0;--k,--c)
-                if(r[k][c]=='Q') {
+                if(board[k][c]=='Q') 
                     return false;
-                }
-
             return true;
         };
         function<void(int)> dfs = [&](int l) {
             if(l==n) {
-                result.push_back(r);
+                result.push_back(board);
                 return;
             }
 
             for(int i=0;i<n;++i) {
                 if(!valid(l,i))
                     continue;
-                r[l][i]='Q';
+                board[l][i]='Q';
                 dfs(l+1);
-                r[l][i]='.';
+                board[l][i]='.';
             }   
         };
         
