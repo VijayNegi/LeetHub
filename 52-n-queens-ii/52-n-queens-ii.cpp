03bdbb47761fe9d4c8571ef1c83a055a332c7ed6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-05 16:05:48 +0530

    Time: 3 ms (86.53%), Space: 6.1 MB (61.83%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int totalNQueens(int n) {
+    int totalNQueens1(int n) {
         int result=0;
         vector<string> board(n,string(n,'.'));
         function<bool(int,int)> valid = [&]( int l,int i) {
@@ -33,6 +33,32 @@ public:
             }   
         };
         
+        dfs(0);
+        return result;
+    }
+    // valid check optimized
+    int totalNQueens(int n) {
+        int result=0;
+        vector<int> cols(n,0);
+        vector<int> ldiag(2*n,0);
+        vector<int> rdiag(2*n,0);
+
+        function<void(int)> dfs = [&](int row) {
+            for(int i=0;i<n;++i)
+            {
+                if(!cols[i] && !ldiag[n+ row - i] && !rdiag[row+i])
+                {
+                    if(row==n-1)
+                        ++result;
+                    else
+                    {
+                        cols[i] = ldiag[n+ row - i] = rdiag[row+i] = 1;
+                        dfs(row+1);
+                        cols[i] = ldiag[n+ row - i] = rdiag[row+i] = 0;
+                    }
+                }
+            }
+        };
         dfs(0);
         return result;
     }
