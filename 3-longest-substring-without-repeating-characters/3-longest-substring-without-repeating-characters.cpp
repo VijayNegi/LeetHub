d2dd6ad297da56cbd3f1505fde6428b55ecf8d03
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-11 01:00:46 +0530

    Time: 6 ms (93.03%), Space: 8.3 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring1(string s) {
         vector<bool> hash(256,false);
         int result=0;
         int n = s.size();
@@ -15,6 +15,17 @@ public:
             }
         }
         return result;
-        
+    }
+    // keep track of previous occurence.
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
     }
 };
