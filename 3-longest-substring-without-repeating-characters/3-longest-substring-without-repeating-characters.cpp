3ee650b7d982a2a6e839be643f31998138fa6226
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-11 00:55:06 +0530

    Time: 10 ms (84.07%), Space: 6.9 MB (91.23%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    //sliding window : 12 ms
-    int lengthOfLongestSubstring1(string s) {
+    int lengthOfLongestSubstring(string s) {
+        vector<bool> hash(256,false);
+        int result=0;
         int n = s.size();
-        int l=0,r;
-        int u = 0;
-        vector<int> dp(256,0);
-        for(r=0;r<n;++r)
-        {
-            char c = s[r];
-            ++dp[c];
-            if(dp[c]==1)
-                ++u;
-            
-            if(r+1-l > u)
-            {
-                if(--dp[s[l++]] == 0)
-                    --u;
+        int l=0,r=0;
+        while(r<n) {
+            if(!hash[s[r]]) {
+                hash[s[r++]] = true;
+                result = max(result, r-l);
+            }
+            else {
+                hash[s[l++]] = false;
             }
         }
+        return result;
         
-        return r-l;
-    }
-    // keep track of previous occurence.
-    int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
-        }
-        return maxLen;
     }
 };
