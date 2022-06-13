32f8d31715b124f0e05ea7bad151efc3b8f2bda5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-14 00:11:07 +0530

    Time: 20 ms (5.45%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,7 +3,6 @@ public:
     // 16ms
     int minimumTotal1(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        //vector<int> res = triangle[0];
         for(int i=1;i<n;++i) {
             triangle[i][0] += triangle[i-1][0];
             for(int j=1;j<i;++j) {
