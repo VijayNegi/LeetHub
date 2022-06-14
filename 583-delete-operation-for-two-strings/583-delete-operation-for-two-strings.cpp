369ca859e0631b99d97b8a2db92811824c8acd11
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-15 00:16:03 +0530

    Time: 35 ms (39.89%), Space: 12.2 MB (49.22%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     // DP: try to maintain a table of number of character that are matched till now
-    int minDistance(string word1, string word2) {
+    // self : 61ms
+    int minDistance1(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
         vector<vector<int>> dp(n1,vector(n2,0));
@@ -20,5 +21,22 @@ public:
             }
         }
         return n1+ n2- 2* dp[n1-1][n2-1];
+    }
+    // same as above
+    int minDistance(string s1, string s2) {
+         int n1 = s1.size();
+        int n2 = s2.size();
+        vector<vector<int>> dp(n1+1,vector(n2+1,0));
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) {
+                if (i == 0 || j == 0)
+                    continue;
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return n1 + n2 - 2 * dp[n1][n2];
     }
 };
