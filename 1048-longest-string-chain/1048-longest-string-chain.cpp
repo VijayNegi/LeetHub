89c276b9092d683309d489069b89376e165a1e47
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-15 23:54:10 +0530

    Time: 193 ms (31.00%), Space: 13.1 MB (83.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
+    int longestStrChain1(vector<string>& words) {
         int n = words.size();
         sort(words.begin(),words.end(),[](string& l,string& r){return l.size()>r.size();});
         int result=1;
-        //vector<int> chain(n,0);
         unordered_map<string,int> s;
         for(auto& w:words)
             s[w]=1;
@@ -17,6 +16,72 @@ public:
                     result = max(result,s[temp]);
                 }
             }
+        }
+        return result;
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        int result=1;
+        sort(words.begin(),words.end(),[](string &a, string &b){return a.size()<b.size();});
+        int len = words.size();
+        vector<int> dp(len,1);
+        int currlen = -2;
+        int preLen = -1;
+        int preS=-2,preE=-2;
+        for(int i=0;i<len;i++)
+        {
+            if(currlen != words[i].size())
+            {
+                
+                preLen = currlen;
+                preS = preE+1;
+                preE = i-1;
+                currlen = words[i].size();
+                //cout<<"prelen="<<preLen<<" currlen="<<currlen<<" =["<<preS<<","<<preE<<"]"<<endl;
+                // search current word in previous list
+                if(preLen == currlen-1)
+                {
+                    string ss = words[i];
+                    
+                    for(int j=0;j<currlen;j++)
+                    {
+                        swap(ss[j],ss[0]);
+                        
+                        for(int k=preS;k<=preE;k++)
+                        {
+                            if(ss.substr(1,preLen)== words[k])
+                            {
+                                //cout<<"ss="<<ss.substr(1,preLen)<<" w="<<words[k]<<endl;
+                                dp[i] = max(dp[i],dp[k]+1);
+                                result = max(dp[i],result);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(preLen == currlen-1)
+                {
+                    string ss = words[i];
+                    
+                    for(int j=0;j<currlen;j++)
+                    {
+                        swap(ss[j],ss[0]);
+                        
+                        for(int k=preS;k<=preE;k++)
+                        {
+                            if(ss.substr(1,preLen)== words[k])
+                            {
+                                //cout<<"ss="<<ss.substr(1,preLen)<<" w="<<words[k]<<endl;
+                                dp[i] = max(dp[i],dp[k]+1);
+                                result = max(dp[i],result);
+                            }
+                        }
+                    }
+                }
+            }
+            
         }
         return result;
     }
