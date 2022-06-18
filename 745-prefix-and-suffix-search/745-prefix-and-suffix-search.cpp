eb882a32f91cb09b50108dd3761c986d22083a19
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-18 21:10:36 +0530

    Time: 563 ms (70.90%), Space: 185.2 MB (40.67%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,55 @@
+#define TRIE_LEN 27
+
+struct Trie {
+    Trie* child[TRIE_LEN];
+    int hIdx;
+    Trie() {
+        fill(begin(child), end(child),nullptr);
+        hIdx=-1;
+    }
+    void insert(string s,int idx) {
+        Trie* node = this;
+        for(auto c:s) {
+           if(!node->child[c-'a'])
+               node->child[c-'a'] = new Trie();
+            node = node->child[c-'a'];
+            node->hIdx = idx;
+        }
+    }
+    int search(string s) {
+        Trie* node = this;
+        for(auto c:s) {
+           if(!(node->child[c-'a']))
+               return -1;
+            node = node->child[c-'a'];
+        }
+        return node->hIdx;
+    }
+};
+
+class WordFilter {
+    Trie root;
+public:
+    WordFilter(vector<string>& words) {
+        int idx=0;
+        for(auto& s:words) {
+            string suff;
+            int n = s.size();
+            for(int i=0;i<n;++i) {
+                suff = s[n-i-1]+suff;
+                root.insert(suff+"{"+s,idx);
+            }
+            ++idx;
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        return root.search(suffix+ "{"+prefix);
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
