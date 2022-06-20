1809c6c84ee2f0f2ed294cb0208ed7acedb49296
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-20 23:50:21 +0530

    Time: 62 ms (77.23%), Space: 16.7 MB (85.15%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // self : 93 ms
-    int minimumLengthEncoding(vector<string>& words) {
+    int minimumLengthEncoding1(vector<string>& words) {
         unordered_set<string> s(words.begin(), words.end());
         for (string w : s)
             for (int i = 1; i < w.size(); ++i)
@@ -9,5 +9,14 @@ public:
         int res = 0;
         for (string w : s) res += w.size() + 1;
         return res;
+    }
+    // use string view
+    int minimumLengthEncoding(vector<string>& words) {
+        unordered_set<string_view> S(words.begin(), words.end());
+        for (auto& w : words) {
+            string_view sv(w);
+            for (int i = 1; i < w.size(); i++) S.erase(sv.substr(i));
+        }
+        return accumulate(S.begin(), S.end(), 0, [](int total, const string_view& w){ return total + w.size() + 1; });
     }
 };
