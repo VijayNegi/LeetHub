b45e37e8e47f008fa0f00d000d206daecdf8d865
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-21 23:59:33 +0530

    Time: 229 ms (12.80%), Space: 54.1 MB (14.85%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -27,24 +27,66 @@ public:
         }
         return i;
     }
-    // cleaner
-    int furthestBuilding(vector<int>& hs, int bricks, int ladders) {
-    priority_queue<int> pq;
-    for (int i = 1; i < hs.size(); ++i) {
-        int d = hs[i] - hs[i - 1];
-        if (d > 0) {
-            pq.push(d);
-            bricks -= d;
-            if (bricks < 0) {
-                --ladders;
-                bricks += pq.top();
-                pq.pop();
-                if (bricks < 0 || ladders < 0)
-                    return i - 1;
+    // cleaner 221 ms
+    int furthestBuilding2(vector<int>& hs, int bricks, int ladders) {
+        priority_queue<int> pq;
+        for (int i = 1; i < hs.size(); ++i) {
+            int d = hs[i] - hs[i - 1];
+            if (d > 0) {
+                pq.push(d);
+                bricks -= d;
+                if (bricks < 0) {
+                    --ladders;
+                    bricks += pq.top();
+                    pq.pop();
+                    if (bricks < 0 || ladders < 0)
+                        return i - 1;
 
+                }
             }
         }
+        return hs.size() - 1;
+    }  
+    // self old
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        
+        int curr=0;
+        int len = heights.size();
+        long sum=0;
+        vector<int> incDiff;
+        while(curr<len-1)
+        {
+            int diff = heights[curr+1] - heights[curr];
+            if(diff<=0)
+            {
+                ++curr;
+                continue;
+            }
+            else
+            {
+                incDiff.push_back(diff);
+                sum+=diff;
+                push_heap(incDiff.begin(), incDiff.end());
+                ++curr;
+                if(sum>bricks)
+                {
+                    if(ladders)
+                    {
+                        int bri = incDiff.front();
+                        pop_heap(incDiff.begin(), incDiff.end());
+                        incDiff.pop_back();
+                        sum-=bri;
+                        --ladders;
+                    }
+                    else
+                    {
+                        --curr;
+                        break;
+                    }
+                    
+                }
+            }
+        }
+        return curr;
     }
-    return hs.size() - 1;
-}  
 };
