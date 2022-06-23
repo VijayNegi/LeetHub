fed83658e66e6898d5b0594f2273d491f044bb9b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-24 00:43:51 +0530

    Time: 444 ms (29.51%), Space: 63.6 MB (21.80%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        int n = courses.size();
-        sort(courses.begin(),courses.end(),[](vector<int>& left, vector<int>& right){return left[1]<right[1];});
+        sort(courses.begin(),courses.end(),[](vector<int>& left, vector<int>& right) { return left[1]<right[1];});
         priority_queue<int> pq;
-        int curr_day=0;
-        int result=0;
-        for(int i=0;i<n;++i) {
-            if(curr_day+courses[i][0] <= courses[i][1] ) { // course can be completed 
-                pq.push(courses[i][0]);
-                curr_day+= courses[i][0];
-                ++result;
+        int day=0;
+        int count=0;
+        for(auto c:courses) {
+            if(day + c[0] <= c[1] ) { // course can be completed 
+                pq.push(c[0]);
+                day += c[0];
+                ++count;
             }
-            else if(pq.size() && (pq.top()>courses[i][0]) && (curr_day - pq.top() + courses[i][0] <= courses[i][1])) { // improve the result
-                curr_day -= pq.top() - courses[i][0];
+            else if(pq.size() && (pq.top() > c[0]) ) { // improve the day
+                day -= pq.top() - c[0];
                 pq.pop();
-                pq.push(courses[i][0]);
+                pq.push(c[0]);
             }   
         }
-        return result;
+        return count;
     }
 };
