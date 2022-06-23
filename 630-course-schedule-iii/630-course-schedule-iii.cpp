85d48e10d7f6dbafcab13759397407f47a7e0f52
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-24 00:50:30 +0530

    Time: 274 ms (81.64%), Space: 68.1 MB (16.72%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -19,8 +19,8 @@ public:
         }
         return count;
     }
-    
-    int scheduleCourse(vector<vector<int>>& courses) {
+    // greedy, more clean
+    int scheduleCourse2(vector<vector<int>>& courses) {
         sort(courses.begin(), courses.end(), [](vector<int>& a, vector<int>& b){return a[1] < b[1];});
         priority_queue<int> pq;
         int now = 0;
@@ -31,5 +31,58 @@ public:
                 now -= pq.top(), pq.pop();
         }
         return pq.size();
+    }
+    // very old solution self
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(),courses.end(),[](const std::vector<int>& a, const std::vector<int>& b) {
+  return a[1] < b[1];
+}); // ???? short by end time
+        
+        int len = courses.size();
+        
+        vector<vector<int>> dp(len,vector<int>(2,0)); // no of course complete, and minimum end date/ start for next work.
+        vector<int> max_heap;
+        
+        
+        for(int i=0;i<len;i++)
+        {
+            if(courses[i][1]>=courses[i][0])
+            {
+                if(i==0)
+                {
+                    dp[i][0] = 1; // one course can be completed
+                    dp[i][1] = courses[i][0];
+                    max_heap.push_back(courses[i][0]);
+                }
+                else
+                {
+                    if((dp[i-1][1] + courses[i][0]) <= courses[i][1]) // course can be completed
+                    {
+                        dp[i][0]= dp[i-1][0]+1;
+                        dp[i][1]= dp[i-1][1] + courses[i][0];
+                        max_heap.push_back(courses[i][0]);
+                        push_heap(max_heap.begin(),max_heap.end());
+                    }
+                    else if(max_heap.front() > courses[i][0]) // improve finish time
+                    {
+                        dp[i][0]= dp[i-1][0]; // same
+                        dp[i][1]= dp[i-1][1] + courses[i][0] - max_heap.front();
+                        
+                        pop_heap(max_heap.begin(),max_heap.end());
+                        max_heap[max_heap.size()-1] = courses[i][0];
+                        push_heap(max_heap.begin(),max_heap.end());
+                    }
+                    else // keep previous timing
+                    {
+                        dp[i][0]= dp[i-1][0];
+                        dp[i][1]= dp[i-1][1];
+                    }
+                   
+                }
+            }
+        }
+        
+        return dp[len-1][0];
+        
     }
 };
