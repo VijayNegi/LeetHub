d79040b7abb54a2a00a9feb53326dda397bcdfea
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-24 00:35:13 +0530

    Time: 423 ms (33.28%), Space: 56.3 MB (58.52%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         int n = courses.size();
-        sort(courses.begin(),courses.end(),[](vector<int>& left, vector<int>& right){if(left[1]==right[1]) return left[0]<right[0]; return left[1]<right[1];});
+        sort(courses.begin(),courses.end(),[](vector<int>& left, vector<int>& right){return left[1]<right[1];});
         priority_queue<int> pq;
         int curr_day=0;
         int result=0;
         for(int i=0;i<n;++i) {
-            if(curr_day+courses[i][0] <= courses[i][1] ) {
+            if(curr_day+courses[i][0] <= courses[i][1] ) { // course can be completed 
                 pq.push(courses[i][0]);
                 curr_day+= courses[i][0];
                 ++result;
             }
-            else if(pq.size() && (pq.top()>courses[i][0]) && (curr_day - pq.top() + courses[i][0] <= courses[i][1])) {
+            else if(pq.size() && (pq.top()>courses[i][0]) && (curr_day - pq.top() + courses[i][0] <= courses[i][1])) { // improve the result
                 curr_day -= pq.top() - courses[i][0];
                 pq.pop();
                 pq.push(courses[i][0]);
