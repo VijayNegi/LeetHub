6a351c48bdb33e8703dc79d9d5d8e7ba92962eb4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-24 00:49:27 +0530

    Time: 375 ms (46.39%), Space: 56.1 MB (85.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -21,7 +21,7 @@ public:
     }
     
     int scheduleCourse(vector<vector<int>>& courses) {
-        sort(courses.begin(), courses.end(), [](vector<int> a, vector<int> b){return a[1] < b[1];});
+        sort(courses.begin(), courses.end(), [](vector<int>& a, vector<int>& b){return a[1] < b[1];});
         priority_queue<int> pq;
         int now = 0;
         for(auto& c:courses) {
