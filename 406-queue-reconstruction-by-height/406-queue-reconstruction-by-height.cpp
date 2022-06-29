0edc84f447327af9cfc8ee3e4d8faecbfbdb4656
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-06-30 00:06:06 +0530

    Time: 77 ms (83.73%), Space: 12.3 MB (43.11%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+    // self :230ms
+    vector<vector<int>> reconstructQueue1(vector<vector<int>>& people) {
         sort(people.begin(),people.end(),[](vector<int>& l,vector<int>& r){
                         if(l[0]==r[0])
                             return l[1]<r[1];
@@ -10,6 +11,36 @@ public:
         for(auto& p:people) {
             result.insert(next(result.begin(), p[1]),p);
         }
+        return result;
+    }
+    // self prev : 
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        list<pair<int,int>> _sort;
+        int size = people.size();
+        vector<vector<int>> result(size,vector(2,-1));
+        sort(people.begin(),people.end());
+        int lastHeight=-1;
+        int sub = 0;
+        for(int i=0;i<people.size();i++)
+        //for(vector<int> p : people)
+        {
+            vector<int> p = people[i];
+            int loc=0,fre=0;
+            if(p[0]==lastHeight)
+                sub--;
+            else
+                sub = 0;
+            
+            int add = 1 + sub;
+            while(fre!=p[1]+add)
+            {
+                if(result[loc++][0]==-1)
+                    fre++;
+            }
+            lastHeight = p[0];
+            result[loc-1] = p;
+        }
+        
         return result;
     }
 };
