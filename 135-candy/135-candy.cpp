39b25d68baa7771d05fec4fdf64a897f845b6466
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-04 23:32:04 +0530

    Time: 26 ms (77.35%), Space: 17.1 MB (96.69%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
+    // self : 47ms
+    int candy1(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> candies(n,1);
         for(int i=0;i<n-1;++i) {
@@ -15,4 +16,51 @@ public:
         count = accumulate(candies.begin(),candies.end(),0);
         return count;
     }
+    // old :
+    int candy(vector<int>& ratings) {
+        int result = 1;
+        int start = 0;
+        int n = ratings.size();
+        bool up = true;
+        int peak = 0;
+        int count=1;
+        for(int i=1;i<n;++i)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                if(!up) {
+                    result -= min(peak,count) -1 ;
+                    count=1;
+                    up = true;
+                    peak = 0;
+                }
+                ++count;
+                result +=count;
+            }
+            else if(ratings[i]<ratings[i-1]) {
+                if(up) {
+                    peak = count;
+                    count = 1;
+                    up = false;
+                }
+                ++count;
+                result +=count;
+            }
+            else
+            {
+                if(!up)
+                    result -= min(peak,count) -1;
+        
+                count =1;
+                result +=count;
+                up = true;
+                peak = 0;
+            }
+        }
+        if(!up)
+            result -= min(peak,count) -1;
+
+        return result;
+    }
+
 };
