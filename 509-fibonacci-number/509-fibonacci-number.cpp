90d09df81b2df3a2983ffe9a667784fa983e87da
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-06 23:54:19 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,13 +2,24 @@ class Solution {
     vector<int> mem;
 public:
     Solution():mem(31,0) {
-        
     }
-    int fib(int n) {
+    // recursion
+    int fib1(int n) {
         if(n<2)
             return n;
         if(mem[n]!=0)
             return mem[n];
         return mem[n] = fib(n-1)+fib(n-2);
+    }
+    // iteration
+    int fib(int n) {
+        std::vector<int> dp(n+2,-1);
+        dp[0]=0;
+        dp[1]=1;
+        int i=1;
+        while(++i<=n) {
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n];
     }
 };
