e9909b3bffcbf5f352368910711154ce467e548f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-07 00:00:42 +0530

    Time: 4 ms (52.45%), Space: 6.1 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -12,7 +12,7 @@ public:
         return mem[n] = fib(n-1)+fib(n-2);
     }
     // iteration
-    int fib(int n) {
+    int fib2(int n) {
         std::vector<int> dp(n+2,-1);
         dp[0]=0;
         dp[1]=1;
@@ -22,4 +22,22 @@ public:
         }
         return dp[n];
     }
+    // fast doubling
+    
+    pair<int, int> fib_helper (int n) {
+        if (n == 0)
+            return {0, 1};
+        auto p = fib_helper(n >> 1);
+        int c = p.first * (2 * p.second - p.first);
+        int d = p.first * p.first + p.second * p.second;
+        if (n & 1)
+            return {d, c + d};
+        else
+            return {c, d};
+    }
+    int fib(int n) {
+        auto res = fib_helper(n);
+        return res.first;
+    }
+
 };
