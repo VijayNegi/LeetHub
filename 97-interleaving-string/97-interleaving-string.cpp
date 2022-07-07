22edff68f642be9920fefef87ca33fe389531136
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-08 00:09:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.63%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -23,8 +23,8 @@ public:
         };
         return dfs(0,0,0);
     }
-    // 55 ms
-    bool isInterleave(string s1, string s2, string s3) {
+    // actually we dont need 3d mem because id3 is just sum of id1 and id2, 7 ms
+    bool isInterleave2(string s1, string s2, string s3) {
         int n1 = s1.size(),n2=s2.size(),n3= s3.size();
         if(n3 != n1+n2)
             return false;
@@ -46,4 +46,26 @@ public:
         };
         return dfs(0,0,0);
     }
+    // official O(n) space solution
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size(),n2=s2.size(),n3= s3.size();
+        if(n3 != n1+n2)
+            return false;
+        vector<bool> dp(n2+1,false);
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j-1] && s2[j-1] == s3[i+j-1];
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1[i-1] == s3[i+j-1];
+                } else {
+                    dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) || (dp[j-1] && s2[j-1] == s3[i+j-1]);
+                }
+            }
+        }
+        return dp[n2];
+    }
+    
 };
