83ab040a2711dd5a48a8e420bb87124ec8c77c43
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-07 23:41:02 +0530

    Time: 55 ms (22.69%), Space: 79.6 MB (4.85%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size(),n2=s2.size(),n3= s3.size();
+        if(n3 != n1+n2)
+            return false;
+        vector<vector<vector<int>>> mem(n1+1,vector(n2+1,vector(n3+1,-1)));
+        function<bool(int,int,int)> dfs = 
+            [&](int id1,int id2,int id3) -> bool {
+                if(id1==n1 && id2==n2 && id3==n3)
+                    return true;
+                if(mem[id1][id2][id3]>=0)
+                    return mem[id1][id2][id3];
+                if(id1!=n1 && id2!=n2 && s1[id1] == s3[id3] && s2[id2]== s3[id3])
+                    return mem[id1][id2][id3] = dfs(id1+1,id2,id3+1) || dfs(id1,id2+1,id3+1);
+                if(id1!=n1 && s1[id1] == s3[id3])
+                    return mem[id1][id2][id3] = dfs(id1+1,id2,id3+1);
+                else if(id2!=n2 && s2[id2]== s3[id3])
+                    return mem[id1][id2][id3] = dfs(id1,id2+1,id3+1);
+                else 
+                    return mem[id1][id2][id3] = false;
+        };
+        return dfs(0,0,0);
+    }
+};
