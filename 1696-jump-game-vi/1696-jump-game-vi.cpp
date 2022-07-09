e6eac96acd2bdeff0c307b20e191d3cf4ba22e4c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-09 14:41:03 +0530

    Time: 487 ms (16.11%), Space: 90.4 MB (50.72%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -3,20 +3,12 @@ public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
         priority_queue<pair<int,int>> pq;
-        int sum = 0;
-        for(int i=0;i<n;++i) {
-            while(pq.size() && pq.top().second < i-k)
+        pq.push({nums[0],0});
+        for(int i=1;i<n;++i) {
+            while(pq.top().second < i-k) // no need to check size
                 pq.pop();
-            if(pq.size())  {
-                //cout<<pq.top().first<<" "<<pq.top().second<<"  ";
-                int s = pq.top().first + nums[i];
-                
-                pq.push({s,i});
-            }
-            else {
-                //cout<<"w"<<endl;
-                pq.push({nums[i],i});
-            }
+            int s = pq.top().first + nums[i];
+            pq.push({s,i}); 
         }
         while(pq.top().second != n-1)
             pq.pop();
