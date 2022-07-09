e2946d09a1f641cf3100c1d4b26e9934729e7729
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-09 14:43:31 +0530

    Time: 224 ms (81.60%), Space: 81.9 MB (61.68%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int maxResult(vector<int>& nums, int k) {
+    // priority solution : 273 ms
+    int maxResult1(vector<int>& nums, int k) {
         int n = nums.size();
         priority_queue<pair<int,int>> pq;
         pq.push({nums[0],0});
@@ -13,5 +14,23 @@ public:
         while(pq.top().second != n-1)
             pq.pop();
         return pq.top().first;
+    }
+    // monotonic queue solution
+    int maxResult(vector<int>& nums, int k) {
+        vector<int> dp(nums.size());
+        deque<int> dq;
+        dp[0] = nums[0];
+        dq.push_back(0);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(dq.front()+k < i)
+                dq.pop_front();
+            dp[i] = (dp[dq.front()] + nums[i]);
+            
+            while(!dq.empty() && dp[dq.back()] < dp[i])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        return dp[nums.size()-1];
     }
 };
