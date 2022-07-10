7fed9a03aad955e6e3af1d98c804a6b6e5783f63
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-11 00:11:52 +0530

    Time: 753 ms (34.86%), Space: 151.5 MB (61.97%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,5 +1,5 @@
 /*
-// standared Segment tree implimentation
+// standared Segment tree implimentation :945 ms
 class NumArray {
     vector<int> nums;
     int n;
@@ -57,7 +57,8 @@ private:
     }
 };
 */
-// Segment tree impl from cp-algorithms
+/*
+// Segment tree impl from cp-algorithms : 865 ms
 class NumArray {
     vector<int> nums;
     int n;
@@ -111,6 +112,64 @@ private:
                + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
     }
 };
+*/
+// Iterative implimentation : 
+class NumArray {
+    vector<int> nums;
+    int n;
+    vector<int> tree;      // tree
+    
+public:
+    NumArray(vector<int>& _nums):nums(_nums),n(nums.size()),tree(2*n,0) {
+        build();         // Note the root index
+    }
+    
+    void update(int pos, int val) {
+        pos += n;
+        tree[pos] = val;
+        while (pos > 0) {
+            int left = pos;
+            int right = pos;
+            if (pos % 2 == 0) {
+                right = pos + 1;
+            } else {
+                left = pos - 1;
+            }
+            // parent is updated after child is updated
+            tree[pos / 2] = tree[left] + tree[right];
+            pos /= 2;
+        }
+    }
+    
+    int sumRange(int l, int r) {
+        // get leaf with value 'l'
+        l += n;
+        // get leaf with value 'r'
+        r += n;
+        int sum = 0;
+        while (l <= r) {
+            if ((l % 2) == 1) {
+               sum += tree[l];
+               l++;
+            }
+            if ((r % 2) == 0) {
+               sum += tree[r];
+               r--;
+            }
+            l /= 2;
+            r /= 2;
+        }
+        return sum;
+    }
+private:
+    void build() {
+        for (int i = n, j = 0;  i < 2 * n; i++,  j++)
+            tree[i] = nums[j];
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+    }
+};
+
 
 /**
  * Your NumArray object will be instantiated and called as such:
