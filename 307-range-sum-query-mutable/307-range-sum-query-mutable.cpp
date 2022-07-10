724d081e19883aba6c360696ef8a67d1e5fe457f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-10 23:48:47 +0530

    Time: 865 ms (16.33%), Space: 153.1 MB (25.51%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,3 +1,5 @@
+/*
+// standared Segment tree implimentation
 class NumArray {
     vector<int> nums;
     int n;
@@ -19,7 +21,6 @@ public:
     }
 private:
     void build(int idx,int sl,int sr) {
-        //cout<<"build -"<<idx<<" "<<sl<<" "<<sr<<endl;
         if(sl==sr) {
             seg[idx] = nums[sl];
             return;
@@ -30,7 +31,6 @@ private:
         seg[idx] = seg[2*idx+1]+seg[2*idx+2];
     }
     void update(int segIdx, int sl,int sr,int idx,int diff) {
-        //cout<<"update";
         if(idx < sl || idx > sr)
             return;
         seg[segIdx] += diff;
@@ -43,7 +43,6 @@ private:
         }
     }
     int sumRange(int idx,int sl,int sr,int left, int right) {
-        //cout<<"sumRange";
         if(right < sl || left > sr)
             return 0;
         if(left <= sl && sr <= right)           // Note the range comparison
@@ -55,6 +54,61 @@ private:
                     sumRange(2*idx+2,mid+1,sr,left,right);
         }
         return 0;
+    }
+};
+*/
+// Segment tree impl from cp-algorithms
+class NumArray {
+    vector<int> nums;
+    int n;
+    vector<int> t;      // tree
+    
+public:
+    NumArray(vector<int>& _nums):nums(_nums),n(nums.size()),t(4*n,0) {
+        build(1,0,n-1);         // Note the root index
+    }
+    
+    void update(int index, int val) {
+        //int diff = val - nums[index];
+        nums[index] = val;
+        update(1,0,n-1,index,val);      // Note the root index
+    }
+    
+    int sumRange(int left, int right) {
+        return sum(1,0,n-1,left,right);
+    }
+private:
+    void build(int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = nums[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(v*2, tl, tm);
+            build(v*2+1, tm+1, tr);
+            t[v] = t[v*2] + t[v*2+1];
+        }
+    }
+    void update(int v, int tl, int tr, int pos, int new_val) {
+        if (tl == tr) {
+            t[v] = new_val;
+        } else {
+            int tm = (tl + tr) / 2;
+            if (pos <= tm)
+                update(v*2, tl, tm, pos, new_val);
+            else
+                update(v*2+1, tm+1, tr, pos, new_val);
+            t[v] = t[v*2] + t[v*2+1];
+        }
+    }
+    int sum(int v, int tl, int tr, int l, int r) {
+        if (l > r) 
+            return 0;
+        if (l == tl && r == tr) {
+            return t[v];
+        }
+        int tm = (tl + tr) / 2;
+        return sum(v*2, tl, tm, l, min(r, tm))
+               + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
     }
 };
 
