265598be3fd9ee43daba8c883935bb6cd8c8d54f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-13 00:53:07 +0530

    Time: 391 ms (49.21%), Space: 68.4 MB (7.14%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -106,7 +106,7 @@ public:
         return false;
     }
     //https://leetcode.com/problems/matchsticks-to-square/discuss/95744/cpp-6ms-solution-with-DFS
-    bool makesquare(vector<int>& nums) {
+    bool makesquare3(vector<int>& nums) {
         if (nums.size() < 4) return false;
         int sum = accumulate(begin(nums),end(nums),0);
         if (sum % 4 != 0) return false;
@@ -115,6 +115,29 @@ public:
             return false;
         vector<int> sidesLength(4, 0);
         return dfs(sidesLength, nums, 0, sum / 4);
+    }
+    // another neat solution
+    //https://leetcode.com/problems/partition-to-k-equal-sum-subsets/discuss/480707/C%2B%2B-DP-bit-manipulation-in-20-lines
+    // int dp[(1<<16)+2];
+     bool makesquare(vector<int>& nums) {
+        int k=4;
+        int n = nums.size(), sum = 0;
+        vector<int> dp((1<<16)+2,-1);
+        //fill(dp, dp+(1<<16)+2, -1);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) sum += nums[i];
+        if (sum % k) return false;
+        int tar = sum/k;
+        
+        for (int mask = 0; mask < (1<<n); mask++) {
+            if (dp[mask] == -1) continue;  // if current state is illegal, simply ignore it
+            for (int i = 0; i < n; i++) {
+                if (!(mask&(1<<i)) && dp[mask]+nums[i] <= tar) {  // if nums[i] is unchosen && choose nums[i] would not cross the target
+                    dp[mask|(1<<i)] = (dp[mask]+nums[i]) % tar;
+                }
+            }
+        }
+        return dp[(1<<n)-1] == 0;
     }
     
 };
