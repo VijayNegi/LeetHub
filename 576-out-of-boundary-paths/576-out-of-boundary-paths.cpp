905039550d7dad1b37b8c778697921c35b63c31a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-16 18:49:28 +0530

    Time: 39 ms (18.84%), Space: 6.4 MB (90.53%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -27,44 +27,25 @@ public:
             fill(begin(temp),end(temp),0);
             for(int r=0;r<m;++r)
                 for(int c=0;c<n;++c) {
-                    if(c==0) {
-                        result += curr[r*n+c];
-                        result %= mod;
-                    }
-                    if(r==0) {
-                        result += curr[r*n+c];
-                        result %= mod;
-                    }
-                    if( r==m-1) {
-                        result += curr[r*n+c];
-                        result %= mod;
-                    }
-                    if(c==n-1) {
-                        result += curr[r*n+c];
-                        result %= mod;
-                    }
-                    // if( (c==0 && r==0) || (r == m-1 && c== n-1) 
-                    //    || (r==0 && c==n-1) || (r==m-1 && c==0)) {
-                    //     result += curr[r*n+c];
-                    //     result %= mod;
-                    // }
-                    if(valid(r+1,c)) {
+                    long ways=0;
+                    if(c==0) ++ways;
+                    if(r==0) ++ways;
+                    if(r==m-1) ++ways;
+                    if(c==n-1) ++ways;
+                    result += ways*curr[r*n+c];
+                    result %= mod;
+                    
+                    if(valid(r+1,c))
                         temp[(r+1)*n+c] = ((long)curr[r*n+c]+temp[(r+1)*n+c])%mod ;
-                        //temp[(r+1)*n+c] %= mod;
-                    }
-                    if(valid(r-1,c)) {
+                    
+                    if(valid(r-1,c)) 
                         temp[(r-1)*n+c] = ((long)curr[r*n+c]+temp[(r-1)*n+c])%mod ;
-                        //temp[(r-1)*n+c] %= mod;
-                    }
-                    if(valid(r,c+1)) {
+                     
+                    if(valid(r,c+1))
                         temp[r*n+c+1] = ((long)curr[r*n+c]+temp[r*n+c+1])%mod ;
-                        //temp[r*n+c+1] %= mod;
-                    }
-                    if(valid(r,c-1)) {
-                        temp[r*n+c-1] = ((long)curr[r*n+c]+temp[r*n+c-1])%mod ;
-                        //temp[r*n+c-1] %= mod;
-                    }
                         
+                    if(valid(r,c-1)) 
+                        temp[r*n+c-1] = ((long)curr[r*n+c]+temp[r*n+c-1])%mod ;
                 }
             swap(curr,temp);
         }
