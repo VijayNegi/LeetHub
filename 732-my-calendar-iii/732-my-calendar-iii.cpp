6af27c95028b64c863ac8a6186ac6a721d0e048a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-16 20:54:49 +0530

    Time: 80 ms (97.08%), Space: 26.9 MB (13.35%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,11 +1,13 @@
 
 class MyCalendarThree {
     map<int,int> timeline;
+    int res{0};
 public:
     MyCalendarThree() {
+        timeline = {{-1, 0}};
     }
     
-    int book(int start, int end) {
+    int book1(int start, int end) {
         timeline[start]++;
         timeline[end]--;
         int ongoing=0,result=0;
@@ -13,6 +15,15 @@ public:
             result = max(result, ongoing += m.second);
         }
         return result;
+    }
+    //https://leetcode.com/problems/my-calendar-iii/discuss/176950/C++-Map-Solution-Fastest
+    int book(int s, int e) {
+        //map<int, int> count = {{-1, 0}};
+        auto start = timeline.emplace(s, (--timeline.upper_bound(s))->second);
+        auto end = timeline.emplace(e, (--timeline.upper_bound(e))->second);
+        for (auto i = start.first; i != end.first; ++i)
+            res = max(res, ++(i->second));
+        return res;
     }
 };
 
