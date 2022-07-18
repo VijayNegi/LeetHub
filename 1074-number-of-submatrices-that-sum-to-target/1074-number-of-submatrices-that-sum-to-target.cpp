bf28b95cfcc20a9c4642409bc27656b5dcae5a30
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-19 00:15:02 +0530

    Time: 725 ms (92.16%), Space: 165.6 MB (41.31%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -9,13 +9,13 @@ public:
         int result=0;
         for(int c1 =0;c1<cols;++c1)
             for(int c2=0;c2<=c1;++c2) {
-                vector<int> rowsum(rows,0);
+                //vector<int> rowsum(rows,0);
                 long sum=0;
                 multiset<long> sumSeen;
                 sumSeen.insert(sum);
                 for(int r=0;r<rows;++r){
-                    rowsum[r] = matrix[r][c1] - ((c2-1)>=0? matrix[r][c2-1]:0); 
-                    sum += rowsum[r];
+                    sum += matrix[r][c1] - ((c2-1)>=0? matrix[r][c2-1]:0); 
+                    //sum += rowsum[r];
                     result+=sumSeen.count(sum-target);
                     sumSeen.insert(sum);
                 } 
