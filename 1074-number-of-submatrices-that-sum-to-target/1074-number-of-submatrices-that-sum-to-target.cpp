8c25832e35ae393d8b6de82314a44996c0c01b7e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-19 00:21:14 +0530

    Time: 939 ms (73.99%), Space: 173.8 MB (33.20%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+    // 2d window sum : 725 ms
+    int numSubmatrixSumTarget1(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size();
         int cols = matrix[0].size();
         for(int r=0;r<rows;++r)
@@ -18,6 +19,45 @@ public:
                     sumSeen.insert(colSum);
                 } 
             }
+        return result;
+    }
+    
+    int Sum(vector<vector<int>>& matrix, int i,int ii,int j,int jj )
+    {
+        return matrix[ii][jj] - matrix[ii][j-1]- matrix[i-1][jj] + matrix[i-1][j-1];
+    }
+    // old solution
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        
+        int rows = matrix.size();
+        int cols=0;
+        if(rows)
+            cols = matrix[0].size();
+        vector<vector<int>> pre(rows+1, vector<int>(cols+1,0));
+        int result=0;
+        int rowsum=0;
+        for(int i=1; i<rows+1 ; i++)
+        {
+            rowsum=0;
+            for(int j=1; j<cols+1; j++) {
+                rowsum+=matrix[i-1][j-1];
+                pre[i][j]+= rowsum;
+                pre[i][j]+= pre[i-1][j];
+            }
+        }
+        map<int,int> count;
+        for(int i=1;i<rows+1;i++) {
+            for(int ii=i; ii<rows+1; ii++) {
+                count.clear();
+                count[0]++;
+                for(int j=1; j<cols+1; j++) {
+                        int sum = Sum(pre,i,ii,1,j);
+                        if(count.find(sum-target)!=count.end())
+                           result+=count[sum-target]; 
+                        count[sum]++;
+                }
+            }
+        }
         return result;
     }
 };
