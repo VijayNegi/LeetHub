3c672a1bd8d4fd2696b0cfc302a10ea506bc742d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-26 00:52:12 +0530

    Time: 3 ms (99.12%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // using std binary search
+    // using std binary search : 19 ms
     vector<int> searchRange1(vector<int>& nums, int target) {
         auto it = upper_bound(nums.begin(),nums.end(),target-1);
         if(it== nums.end() || (*it!=target))
@@ -10,7 +10,8 @@ public:
         int end = distance(nums.begin(),it1);
         return {start,end-1};
     }
-    vector<int> searchRange(vector<int>& nums, int target) {
+    // binary search self; 23 ms
+    vector<int> searchRange2(vector<int>& nums, int target) {
         int n = nums.size();
         if(n==0)
             return {-1,-1};
@@ -35,6 +36,35 @@ public:
         }
         --l;
         return {start,l};
+    }
+    // one pass
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result(2,-1);
+        int len = nums.size();
+        if(len==0)
+            return result;
+        int begin = 0;
+        int end = len-1;
         
+        while(nums[begin]< nums[end])
+        {
+            int mid = begin + ((end-begin)/2);
+            
+            if(nums[mid]> target)
+                end = mid-1;
+            else if(nums[mid]< target)
+                begin = mid+1;
+            else
+            {
+                if(nums[begin]<target)
+                    ++begin;
+                if(nums[end]>target)
+                    --end;
+            }
+            
+        }
+        if(nums[begin]==target)
+            result[0]=begin,result[1]=end;
+        return result;
     }
 };
