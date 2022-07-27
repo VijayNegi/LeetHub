447c178f0ba712c21604ad6c907a8aa3f30c02d2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-28 00:03:47 +0530

    Time: 62 ms (55.88%), Space: 12.8 MB (61.11%) - LeetHub

--- 699-falling-squares/699-falling-squares.cpp
+++ 699-falling-squares/699-falling-squares.cpp
@@ -55,19 +55,41 @@ struct stree {
 class Solution {
     
 public:
-    vector<int> fallingSquares(vector<vector<int>>& positions) {
+    // self segment tree solution
+    vector<int> fallingSquares1(vector<vector<int>>& positions) {
         stree tree(1,1e8,0);
         int maxheight = 0;
         vector<int> result;
         for(auto b:positions){
-            //cout<<b[1]<<" ";
             int base = tree.get(b[0],b[0]+b[1]-1);
-            //cout<<" get "<<base<<endl;
             tree.update(b[0],b[0]+b[1]-1,base+b[1]);
             maxheight = max(maxheight,base+b[1]);
             result.push_back(maxheight);
-            //cout<< "end "<<b[1]<<endl;;
         }
         return result;
+    }
+    // map solution without coordinate-compression
+    // same as line-sweep
+    //https://leetcode.com/problems/falling-squares/discuss/108783/c%2B%2B-map-based-short-solution
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
+        map<int,int> mp = {{0,0}};
+        int curr=0;
+        vector<int> res;
+        for(auto b:positions){
+            int l = b[0],r = b[0]+b[1],h = b[1];
+            auto it_i = mp.upper_bound(l);  // upper as we have a min sentry entry
+            auto it_j = mp.lower_bound(r);  // match or upper 
+            int tmp = it_j->first == r? it_j->second : prev(it_j)->second; // height after this box end
+            int maxh=0;
+            for(auto i=prev(it_i);i!=it_j;++i) {
+                maxh = max(maxh,i->second);
+            }
+            mp.erase(it_i,it_j);      //erase range
+            mp[l] = h+maxh;           // new left bound
+            mp[r] = tmp;              // new right bound
+            curr = max(curr, mp[l]);
+            res.push_back(curr);
+        }
+        return res;
     }
 };
