1c5d99a97c506654b8f75c58f081871178b437eb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-28 01:03:00 +0530

    Time: 213 ms (94.47%), Space: 62.7 MB (84.42%) - LeetHub

--- 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.cpp
+++ 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int closestToTarget(vector<int>& arr, int target) {
+    // from : https://leetcode.com/problems/find-a-value-of-a-mysterious-function-closest-to-target/discuss/743741/Detailed-General-Ideasolution-for-such-problems-(-AND-OR-GCD-)-in-O(N-*-log(-max(arri)-)-) 
+    int closestToTarget1(vector<int>& arr, int target) {
         set<int> s;
         int n = arr.size();
         int res=INT_MAX;
@@ -16,5 +17,18 @@ public:
             s = temp;
         }
         return res;
+    }
+    //
+    int closestToTarget(vector<int>& arr, int target) {
+        vector<int> q;
+        q.reserve(32);
+        int best = 1'000'000'000;
+        for (size_t i = 0; i < arr.size(); i++) {
+            for (auto& x : q) x &= arr[i];
+            q.push_back(arr[i]);
+            q.erase(unique(begin(q), end(q)), end(q));
+            for (auto x : q) best = min(best, abs(target-x));
+        }
+        return best;
     }
 };
