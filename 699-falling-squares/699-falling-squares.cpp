c98d54fb9469b5f9c79cc5750a416f218d6bebca
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-28 00:09:15 +0530

    Time: 43 ms (80.07%), Space: 12.4 MB (65.69%) - LeetHub

--- 699-falling-squares/699-falling-squares.cpp
+++ 699-falling-squares/699-falling-squares.cpp
@@ -56,6 +56,7 @@ class Solution {
     
 public:
     // self segment tree solution
+    // 291 ms
     vector<int> fallingSquares1(vector<vector<int>>& positions) {
         stree tree(1,1e8,0);
         int maxheight = 0;
@@ -68,10 +69,11 @@ public:
         }
         return result;
     }
+    // 62 ms
     // map solution without coordinate-compression
     // same as line-sweep
     //https://leetcode.com/problems/falling-squares/discuss/108783/c%2B%2B-map-based-short-solution
-    vector<int> fallingSquares(vector<vector<int>>& positions) {
+    vector<int> fallingSquares2(vector<vector<int>>& positions) {
         map<int,int> mp = {{0,0}};
         int curr=0;
         vector<int> res;
@@ -81,7 +83,7 @@ public:
             auto it_j = mp.lower_bound(r);  // match or upper 
             int tmp = it_j->first == r? it_j->second : prev(it_j)->second; // height after this box end
             int maxh=0;
-            for(auto i=prev(it_i);i!=it_j;++i) {
+            for(auto i=prev(it_i);i!=it_j;++i) {  // start from boxes that have already started.
                 maxh = max(maxh,i->second);
             }
             mp.erase(it_i,it_j);      //erase range
@@ -91,5 +93,23 @@ public:
             res.push_back(curr);
         }
         return res;
+    }
+    // line sweep with coordinate compression
+    // https://leetcode.com/problems/falling-squares/discuss/1441736/C%2B%2B-coordinate-compression
+    vector<int> fallingSquares(vector<vector<int>>& ps) {
+        vector<int> x;
+        for (auto &p : ps) x.push_back(p[0]),x.push_back(p[0] + p[1]);
+        sort(x.begin(),x.end());
+        vector<int> hs(x.size()), ans; // hs to store height
+        unordered_map<int, int> idx;
+        for (int i = 0; auto t : x) idx[t] = i++; // provide index for accessing hs
+        for (int mh = 0; auto &p : ps) {
+            int l = idx[p[0]], r = idx[p[0] + p[1]];
+            int mx = *max_element(hs.begin() + l, hs.begin() + r);
+            for (int i = l; i < r; i++) hs[i] = p[1] + mx;
+            mh = max(mh, hs[l]);
+            ans.emplace_back(mh);
+        }
+        return ans;
     }
 };
