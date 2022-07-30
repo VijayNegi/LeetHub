094b899a7f619cf53f6b0e0264b94e62d852647d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-30 23:52:26 +0530

    Time: 338 ms (46.97%), Space: 108.7 MB (29.44%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        int n = words1.size();
+        int m = words2.size();
+        vector<vector<int>> hist(n,vector(26,0));
+        vector<vector<int>> hist2(m,vector(26,0));
+        //vector<vector<int>> hist2(26,0);
+        for(int i=0;i<n;++i) {
+            for(auto& c:words1[i]) 
+                hist[i][c-'a']++;
+        }
+        for(int i=0;i<m;++i) {
+            for(auto& c:words2[i]) 
+                hist2[i][c-'a']++;
+        }
+        vector<int> k(26,0);
+        
+        for(int i=0;i<m;++i) {
+            for(int j=0;j<26;++j)
+                k[j] = max(k[j],hist2[i][j]);
+        }
+        
+        function<bool(vector<int>& ,vector<int>&)> comp = [](vector<int>& v1,vector<int>& v2) {
+            for(int i=0;i<26;++i)
+                if(v1[i]<v2[i]) return false;
+            return true;
+        };
+        vector<string> result;
+        for(int i=0;i<n;++i) 
+            if(comp(hist[i],k)) 
+                result.push_back(words1[i]);
+        
+        return result;
+    }
+};
