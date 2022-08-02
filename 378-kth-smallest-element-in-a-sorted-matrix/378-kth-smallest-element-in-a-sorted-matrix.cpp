edb871f389b080adc72b82071e28b234faa6c8bc
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-03 01:10:05 +0530

    Time: 47 ms (65.37%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/861912/Sharing-My-Both-C%2B%2B-Sol.-Using-Binary-Search-Heap
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
+    int kthSmallest1(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
         int left = matrix[0][0];
         int right = matrix[n-1][n-1];
@@ -31,5 +31,25 @@ public:
         
         
         return left;
+    }
+    //https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1321834/C%2B%2B-Simple-Clean-and-Short-Binary-Search-Solution
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int lo = matrix[0][0], hi = matrix[n-1][n-1] + 1, mid, count, tmp;
+        
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2, tmp = n - 1, count = 0;
+            
+			// For each row, we count the elements that are smaller then mid
+            for (int i = 0; i < n; i++) {
+                while (tmp >= 0 && matrix[i][tmp] > mid) tmp--;
+                count += tmp + 1;
+            }
+            
+            if (count < k) lo = mid + 1;
+            else hi = mid;
+        }
+        
+        return lo;
     }
 };
