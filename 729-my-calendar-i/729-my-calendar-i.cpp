5485e23beee2f6061b2f16b9d6482780f82388f6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-04 00:10:49 +0530

    Time: 150 ms (61.55%), Space: 38.8 MB (60.92%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,26 +1,17 @@
 class MyCalendar {
-    set<pair<int,int>> s;
-    map<int,int>events;
+    map<int,int> cal;
 public:
     MyCalendar() {
         
     }
-    // set solution
-    bool book1(int start, int end) {
-        auto it = s.lower_bound({start,end});
-        if(it != s.end() && it->first < end)
+    
+    bool book(int start, int end) {
+        auto it = cal.upper_bound(start);
+        if(it != cal.end()) {
+            if(it->second < end)
                 return false;
-        if(it!=s.begin() && (--it)->second > start)
-            return false;
-        s.insert({start,end});
-        return true;
-    }
-    // map solution
-    bool book(int start, int end) {  
-        // Another nice solution
-        auto next = events.upper_bound(start);
-        if(next != events.end() &&   (*next).second < end)return false;
-        events.insert({end,start});
+        }
+        cal[end] = start;
         return true;
     }
 };
