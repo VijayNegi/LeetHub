b5f4af1ee9ace8fe5fdc66e68703dc2322eed64d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-05 00:40:52 +0530

    Time: 6 ms (6.61%), Space: 5.9 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -7,7 +7,7 @@ public:
         return gcd(b,a%b);
     }
     
-    int mirrorReflection(int p, int q) {
+    int mirrorReflection1(int p, int q) {
         int lcm = (p*q)/gcd(p, q); // calculating lcm using gcd
         int m = lcm/p;
         int n = lcm/q;
@@ -15,5 +15,14 @@ public:
         if(m%2==0) return 0;
         return 1;
 
+    }
+    // https://leetcode.com/problems/mirror-reflection/discuss/2376191/C%2B%2B-Java-Python-or-Faster-then-100-or-Full-explanations-or
+    // only odd even matters
+    int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0){ 
+            p/=2;
+            q/=2;
+        }
+        return 1 - p % 2 + q % 2;
     }
 };
