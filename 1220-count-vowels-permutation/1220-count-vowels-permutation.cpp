1e2d955f3b9818b263e08da851699b656ffa2907
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-07 19:54:14 +0530

    Time: 97 ms (45.64%), Space: 18.1 MB (49.84%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,15 +1,15 @@
 const int mod = 1e9+7;
 class Solution {
 public:
-    int countVowelPermutation(int n) {
+    // 327 ms
+    int countVowelPermutation1(int n) {
         map<char,vector<char>> pre;
         pre['a'] = {'e','i','u'};
         pre['e'] = {'a','i'};
         pre['i'] = {'e','o'};
         pre['o'] = {'i'};
         pre['u'] = {'i','o'};
-        
-        //vector<vector<int>> dp(2,vector(256,0));
+
         vector<int> dp(256,0);
         for(auto& c: pre) {
             dp[c.first] = 1;
@@ -31,5 +31,26 @@ public:
             result %= mod;
         }
         return result;
+    }
+    // 111 ms
+    int countVowelPermutation(int n) {
+        const int mod = 1e9+7;
+        vector<int> curr(5,1) ;
+        
+        for(int i=2;i<=n;++i)
+        {
+            vector<int> next(5,0);
+            next[0] = ((curr[1] + curr[2])%mod + curr[4])%mod;
+            next[1] = (curr[0] + curr[2])%mod;
+            next[2] = (curr[1] + curr[3])%mod;
+            next[3] = (curr[2])%mod;
+            next[4] = (curr[2] + curr[3])%mod;
+            curr = next;
+        }
+        int sum=0;
+        for(int k=0;k<5;++k)
+            sum  = (sum + curr[k])% mod;
+        
+        return sum;
     }
 };
