57e21b71c19cb3b813af2f82187c92e7463d6926
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-07 19:44:46 +0530

    Time: 327 ms (13.86%), Space: 285.4 MB (5.29%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -9,25 +9,25 @@ public:
         pre['o'] = {'i'};
         pre['u'] = {'i','o'};
         
-        vector<vector<int>> dp(n+1,vector(256,0));
+        //vector<vector<int>> dp(2,vector(256,0));
+        vector<int> dp(256,0);
         for(auto& c: pre) {
-            dp[1][c.first] = 1;
+            dp[c.first] = 1;
         }
-        //dp[1]['a'] = dp[1]['a'] = dp[1]['a'] = dp[1]['a'] = dp[1]['a']
         
         for(int i=1;i<n;++i) {
+            vector<int> temp(256,0);
             for(auto& c: pre) {
                 int ch = c.first;
                 for(auto& k: c.second) {
-                    dp[i+1][k] =  ((long)dp[i+1][k] + dp[i][ch])%mod;
-                    
+                    temp[k] =  ((long)temp[k] + dp[ch])%mod;
                 }
-                
             }
+            swap(dp,temp);
         }
         long result = 0;
         for(auto& c: pre) {
-            result += dp[n][c.first];
+            result += dp[c.first];
             result %= mod;
         }
         return result;
