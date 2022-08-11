96455225bdfa21c387e66d350fc62d116cfff5b6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-11 08:07:27 +0530

    Time: 9 ms (94.12%), Space: 21.6 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
+    bool isValidBST1(TreeNode* root) {
         
         function<bool(TreeNode*,long,long)> isValid = [&](TreeNode* node,long low,long high)
         {
@@ -25,5 +25,22 @@ public:
         };
         
         return isValid(root,LONG_MIN,LONG_MAX);
+    }
+    // inorder traversal without LONG_MAX
+    bool isValidBST(TreeNode* root) {
+       if (root == nullptr) return true;
+       stack<TreeNode*> stack;
+       TreeNode* pre = nullptr;
+       while (root != nullptr || !stack.empty()) {
+          while (root != nullptr) {
+             stack.push(root);
+             root = root->left;
+          }
+          root = stack.top(); stack.pop();
+          if(pre != nullptr && root->val <= pre->val) return false;
+          pre = root;
+          root = root->right;
+       }
+       return true;
     }
 };
