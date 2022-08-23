8d204c55512ef9e9ecf9d8131ba53f03f7e87835
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-23 09:12:29 +0530

    Time: 253 ms (91.34%), Space: 114.1 MB (74.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
+    bool isPalindrome1(ListNode* head) {
         int n = 0;
         vector<int> v;
         ListNode* curr = head;
@@ -23,6 +23,20 @@ public:
             if(v[l++]!=v[r--])
                 return false;
         return true;
-        
+    }
+    // O(n) and O(1) space
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head, *prev, *temp;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        prev = slow, slow = slow->next, prev->next = NULL;
+        while (slow)
+            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
+        fast = head, slow = prev;
+        while (slow) {
+            if (fast->val != slow->val) return false;
+            fast = fast->next, slow = slow->next;
+        }
+        return true;
     }
 };
