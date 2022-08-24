14f4b26640da1aef9740489ca599ee7a4c3975c5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-24 09:12:56 +0530

    Time: 31 ms (61.16%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -21,12 +21,6 @@ public:
         return n==1;
     }
     bool isPowerOfThree(int n) {
-        if(n<1)
-            return false;
-        
-        while(n%3==0) {
-           n/=3;
-        }
-        return n==1;
+        return n > 0 && 1162261467 % n == 0;
     }
 };
