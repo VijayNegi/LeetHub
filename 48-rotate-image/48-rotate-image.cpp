75a05c0fe9d27da7a5067d722113d420e22e1b5c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-30 09:46:44 +0530

    Time: 6 ms (23.35%), Space: 7.2 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate1(vector<vector<int>>& matrix) {
         int s = matrix.size();
         //int cols = matrix.size();
         
@@ -8,14 +8,21 @@ public:
             for(int c = r; c<(s-r-1);++c) {
                 int val = matrix[r][c];
                 matrix[r][c] = matrix[s-1-c][r];
-                matrix[s-1-c][r] = matrix[s-r-1][s-1-c];
-                matrix[s-r-1][s-1-c] = matrix[c][s-r-1];
+                matrix[s-1-c][r] = matrix[s-1-r][s-1-c];
+                matrix[s-1-r][s-1-c] = matrix[c][s-r-1];
                 matrix[c][s-r-1] = val;
             }
         }
     }
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+        for(int row = 0; row < n/2; row++)
+            for(int col = row; col < n - row - 1; col++)
+            {                
+                swap(matrix[row][col], matrix[col][n - 1 - row]);
+                swap(matrix[row][col], matrix[n - 1 - row][n - 1 - col]);
+                swap(matrix[row][col], matrix[n - 1 - col][row]);
+            }
+    }
 };
-
-//            r,c
-// (s-1)-c,r       c,s-r-1
-//         s-r-1,s-1-c
