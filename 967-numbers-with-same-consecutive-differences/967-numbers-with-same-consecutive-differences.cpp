bb54e657258353031de8d18b11400557ae9c0dc8
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-03 13:58:17 +0530

    Time: 3 ms (74.35%), Space: 7.1 MB (96.71%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<int> numsSameConsecDiff(int n, int k) {
+    // self : 9ms
+    vector<int> numsSameConsecDiff1(int n, int k) {
         vector<int> result;
-        
         function<void(string)> dfs = [&](string s) {
             if(s.size()==n){
                 if(s[0]=='0')
@@ -13,11 +13,9 @@ public:
                 
             int last = s[0]-'0';
             if(last+k < 10) {
-                //string k = '0'+ last+k;
                 dfs(to_string(last+k)+s);
             }
             if((last-k)>=0 && k!=0) {
-                //string k = '0'+ last-k;
                 dfs(to_string(last-k)+s);
             }   
         };
@@ -25,5 +23,20 @@ public:
             dfs(to_string(i));
         }
         return result;
+    }
+    vector<int> numsSameConsecDiff(int N, int K) {
+        vector<int> cur = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 2; i <= N; ++i) {
+            vector<int> cur2;
+            for (int x : cur) {
+                int y = x % 10;
+                if (y + K < 10)
+                    cur2.push_back(x * 10 + y + K);
+                if (K > 0 && y - K >= 0)
+                    cur2.push_back(x * 10 + y - K);
+            }
+            cur = cur2;
+        }
+        return cur;
     }
 };
