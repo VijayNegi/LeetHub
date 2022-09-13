9bbba6207612e9738f216ed6b7aba9fb9ede5cd0
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-13 10:18:45 +0530

    Time: 27 ms (28.68%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool validUtf8(vector<int>& data) {
+    //self
+    bool validUtf8_1(vector<int>& data) {
         int n = data.size();
         int dbytes = 1<<7;
         int mask = 1<<7 | 1<<6;
@@ -12,17 +13,30 @@ public:
                 if(p & (1<<j) )
                     ++k;
                 else break;
-            //cout<<"k=="<<k<<endl;
             if(k>4 || k==1) return false;
             if((i+k-2)>=n && k>0)
                 return false;
             for(int j=1;j<k;++j) {
                 int m = data[i++] & mask;
-                //cout<<m<<endl;
                 if(m != dbytes)
                     return false;
             }
         }
         return true;
+    }
+    bool validUtf8(vector<int>& data) {
+        int count = 0;
+        for (auto c : data) {
+            if (count == 0) {
+                if ((c >> 5) == 0b110) count = 1;
+                else if ((c >> 4) == 0b1110) count = 2;
+                else if ((c >> 3) == 0b11110) count = 3;
+                else if ((c >> 7)) return false;
+            } else {
+                if ((c >> 6) != 0b10) return false;
+                count--;
+            }
+        }
+        return count == 0;
     }
 };
