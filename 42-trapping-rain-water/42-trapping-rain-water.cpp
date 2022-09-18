e796549258e585df23f511dc9d860deb04dbcf8a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-18 16:09:59 +0530

    Time: 36 ms (22.19%), Space: 20 MB (52.85%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> stk;
+        int h=0;
+        int result=0;
+        for(int i=0;i<n;++i) {
+            int h=0;
+            //cout<<i<<"-"<<endl;
+            while(stk.size() && (height[stk.back()] <= height[i])) {
+                int minh = min(height[stk.back()], height[i]);
+                int height1 =  minh-h;
+                int width = i- stk.back() -1;
+                int area = width * (height1);
+                result += area;
+                //cout<<area<< " "<<width<<" "<<height1<<" -- "<<minh<<" "<<h<<endl;
+                h = max(h, minh);
+                stk.pop_back();
+            
+            }
+            if(stk.empty())
+                h=0;
+            else {
+                int height1 =  height[i]-h;
+                int width = i- stk.back() -1;
+                int area = width * (height1);
+                result += area;
+                //cout<<area<< " "<<width<<" "<<height1<<" -- "<<height[i]<<" * "<<h<<endl;
+                h = max(h, height[i]);
+            }
+            
+            if(height[i]>0)
+                stk.push_back(i);
+        }
+        return result;
+    }
+};
