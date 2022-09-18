33c8e072c4dd23aa01eebcfa5043bd04a0ccd407
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-18 16:11:11 +0530

    Time: 40 ms (14.07%), Space: 19.9 MB (52.85%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n;++i) {
             int h=0;
             //cout<<i<<"-"<<endl;
-            while(stk.size() && (height[stk.back()] <= height[i])) {
+            while(stk.size() ) {
                 int minh = min(height[stk.back()], height[i]);
                 int height1 =  minh-h;
                 int width = i- stk.back() -1;
@@ -16,19 +16,21 @@ public:
                 result += area;
                 //cout<<area<< " "<<width<<" "<<height1<<" -- "<<minh<<" "<<h<<endl;
                 h = max(h, minh);
-                stk.pop_back();
+                if((height[stk.back()] <= height[i]))
+                    stk.pop_back();
+                else break;
             
             }
-            if(stk.empty())
-                h=0;
-            else {
-                int height1 =  height[i]-h;
-                int width = i- stk.back() -1;
-                int area = width * (height1);
-                result += area;
-                //cout<<area<< " "<<width<<" "<<height1<<" -- "<<height[i]<<" * "<<h<<endl;
-                h = max(h, height[i]);
-            }
+            // if(stk.empty())
+            //     h=0;
+            // else {
+            //     int height1 =  height[i]-h;
+            //     int width = i- stk.back() -1;
+            //     int area = width * (height1);
+            //     result += area;
+            //     //cout<<area<< " "<<width<<" "<<height1<<" -- "<<height[i]<<" * "<<h<<endl;
+            //     h = max(h, height[i]);
+            // }
             
             if(height[i]>0)
                 stk.push_back(i);
