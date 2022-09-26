17425e85ba50213df838b54adeab2b32eec556b5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-26 10:49:36 +0530

    Time: 11 ms (53.23%), Space: 11.4 MB (70.01%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -24,7 +24,7 @@ struct DSU
 class Solution {
 public:
     // DSU
-    bool equationsPossible(vector<string>& equations) {
+    bool equationsPossible1(vector<string>& equations) {
         DSU ds(26);
         for(auto e:equations) {
             if(e[1] == '=') 
@@ -36,5 +36,22 @@ public:
                     return false;
         }
         return true;
+    }
+    // other simple impl
+    int uf[26];
+    bool equationsPossible(vector<string>& equations) {
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (string e : equations)
+            if (e[1] == '=')
+                uf[find(e[0] - 'a')] = find(e[3] - 'a');
+        for (string e : equations)
+            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a'))
+                return false;
+        return true;
+    }
+
+    int find(int x) {
+        if (x != uf[x]) uf[x] = find(uf[x]);
+        return uf[x];
     }
 };
