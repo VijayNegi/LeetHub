9d67472353858ed10555ba4d0d9e91793f450e3e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-29 09:49:16 +0530

    Time: 82 ms (67.86%), Space: 30.9 MB (98.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    vector<int> findClosestElements0(vector<int>& arr, int k, int x) {
         auto cmp = [&](int l,int r) {
             int al = abs(l-x);
             int ar = abs(r-x);
@@ -21,5 +21,37 @@ public:
         }
         sort(begin(result),end(result));
         return result;
+    }
+        vector<int> findClosestElements1(vector<int>& arr, int k, int x) {
+        
+        int l=0,r=k-1;
+        int maxdis = max (abs(arr[l]-x), abs(arr[r]-x));
+        for(int i=k;i<arr.size();++i)
+        {
+            if(arr[i]-x>=maxdis)
+                break;
+            
+            if(abs(arr[i]-x) < maxdis)
+            {
+                r=i;
+                l=i-k+1;
+                maxdis = max(abs(arr[l]-x), abs(arr[r]-x));
+            }
+           
+        }
+        vector<int> result(begin(arr)+l,begin(arr)+r+1);
+        return result;
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left=0,right=arr.size()-k;
+        while(left<right)
+        {
+            int mid = (left+right)/2;
+            if(x-arr[mid] > arr[mid+k]-x)
+                left=mid+1;
+            else
+                right = mid;
+        }
+        return vector<int>(arr.begin()+left,arr.begin()+left+k);
     }
 };
