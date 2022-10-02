1794835b470a9f106d392fced5c7043566a4921b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-02 18:26:32 +0530

    Time: 8 ms (48.89%), Space: 10 MB (26.20%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    // self
+    int mincostTickets1(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int> mincost(n+1,-1);
         function<int(int)> dfs = [&](int day) {
@@ -19,5 +20,24 @@ public:
             return cost;
         };
         return dfs(0);
+    }
+    // bottom up 
+    int mincostTickets(vector<int>& DAYS, vector<int>& cost) {
+        
+        unordered_set<int> days(DAYS.begin(),DAYS.end());
+        vector<int> dp(367);
+        
+        for(int i=1 ; i<366 ; i++)
+        {
+            dp[i] = dp[i-1];
+            if(days.find(i) != days.end())
+            {
+			// cost of ith day , decision of ith day will be
+                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
+                             dp[max(0, i - 7)] + cost[1],  // 2nd case
+                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
+            }
+        }
+        return dp[365];
     }
 };
