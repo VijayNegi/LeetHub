14f44123c4a9d767d8ab4e10d9a81f2e6a3b8b02
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-10 12:18:52 +0530

    Time: 8 ms (10.44%), Space: 6.3 MB (5.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string breakPalindrome(string p) {
+    string breakPalindrome1(string p) {
         int n = p.size();
         if(n<=1)
             return "";
@@ -17,5 +17,16 @@ public:
         if(i==n)
             p.back()++;
         return p;
+    }
+    string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
+            }
+        }
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
     }
 };
