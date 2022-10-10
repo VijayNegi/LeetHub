a6da87dde70e3e362efd6c5466acc1cacbeb4b75
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-10 12:13:27 +0530

    Time: 6 ms (16.14%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string breakPalindrome(string p) {
+        int n = p.size();
+        if(n<=1)
+            return "";
+        bool odd = n%2;
+        int i=0;
+        for(i=0;i<n;++i) {
+            if(odd && i==n/2)
+                continue;
+            if(p[i]!='a'){
+                p[i]='a';
+                break;
+            }  
+        }
+        if(i==n)
+            p.back()++;
+        return p;
+    }
+};
