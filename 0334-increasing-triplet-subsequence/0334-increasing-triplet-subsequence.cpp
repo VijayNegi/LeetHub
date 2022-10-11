826cea7b806320e2f65684aa6e38a8a5026901ed
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-11 12:37:05 +0530

    Time: 88 ms (78.99%), Space: 61.6 MB (67.34%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
+    bool increasingTriplet1(vector<int>& nums) {
         //priority_queue<int> pq;
         vector<int> v;
         for(auto& i:nums) {
@@ -14,4 +14,18 @@ public:
         }
         return false;
     }
+    // simple LIS
+    bool increasingTriplet(vector<int>& nums) {
+        int one = INT_MAX, two = INT_MAX;
+        for (auto num : nums) {
+            if (num <= one)
+                one = num;
+            else if (num <= two)
+                two = num;
+            else 
+                return true;
+        }
+        return false;
+    }
+       
 };
