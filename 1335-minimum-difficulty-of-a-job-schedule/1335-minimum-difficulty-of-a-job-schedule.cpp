b893b6e758318189c41feda2595801b05e443b3b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-16 17:15:30 +0530

    Time: 9 ms (98.94%), Space: 7.5 MB (74.63%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -31,8 +31,8 @@ public:
         return dp[d][0];
     }
     // https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/490316/JavaC%2B%2BPython3-DP-O(nd)-Solution
-    // 1d DP
-    int minDifficulty(vector<int>& A, int D) {
+    // 1d DP : 46 ms
+    int minDifficulty2(vector<int>& A, int D) {
         int n = A.size(), inf = 1e9, maxd;
         if (n < D) return -1;
         vector<int> dp(n + 1, 1e9);
@@ -47,5 +47,28 @@ public:
             }
         }
         return dp[0];
+    }
+    // https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/490316/JavaC%2B%2BPython3-DP-O(nd)-Solution
+    // stack based soution
+    int minDifficulty(vector<int>& A, int D) {
+        int n = A.size();
+        if (n < D) return -1;
+        vector<int> dp(n, 1000), dp2(n), stack;
+        for (int d = 0; d < D; ++d) {
+            stack.clear();
+            for (int i = d; i < n; i++) {
+                dp2[i] = i ? dp[i - 1] + A[i] : A[i];
+                while (stack.size() && A[stack.back()] <= A[i]) {
+                    int j = stack.back(); stack.pop_back();
+                    dp2[i] = min(dp2[i], dp2[j] - A[j] + A[i]);
+                }
+                if (stack.size()) {
+                    dp2[i] = min(dp2[i], dp2[stack.back()]);
+                }
+                stack.push_back(i);
+            }
+            swap(dp, dp2);
+        }
+        return dp[n - 1];
     }
 };
