5b7fc4a454992544472ca59c1bcb058e16e70df7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-16 17:14:26 +0530

    Time: 46 ms (82.48%), Space: 7.4 MB (79.30%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int minDifficulty(vector<int>& jd, int d) {
+    // self: bottom-up : 97 ms
+    int minDifficulty1(vector<int>& jd, int d) {
         int n = jd.size();
         if(n<d) return -1;
         if(n==d) 
@@ -28,5 +29,23 @@ public:
             }
         }
         return dp[d][0];
+    }
+    // https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/490316/JavaC%2B%2BPython3-DP-O(nd)-Solution
+    // 1d DP
+    int minDifficulty(vector<int>& A, int D) {
+        int n = A.size(), inf = 1e9, maxd;
+        if (n < D) return -1;
+        vector<int> dp(n + 1, 1e9);
+        dp[n] = 0;
+        for (int d = 1; d <= D; ++d) {
+            for (int i = 0; i <= n - d; ++i) {
+                maxd = 0, dp[i] = inf;
+                for (int j = i; j <= n - d; ++j) {
+                    maxd = max(maxd, A[j]);
+                    dp[i] = min(dp[i], maxd + dp[j + 1]);
+                }
+            }
+        }
+        return dp[0];
     }
 };
