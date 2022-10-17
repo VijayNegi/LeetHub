07b0ea3d1d7ec7bba2bec4ec77c58ddbdec5243c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-17 08:32:57 +0530

    Time: 7 ms (38.35%), Space: 7.1 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
+    bool checkIfPangram1(string sentence) {
         bool map[26] = {false};
         for(auto& c: sentence)
             map[c-'a'] = true;
         for(auto& b:map)
             if(b==false) return false;
         return true;
+    }
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> seen(sentence.begin(), sentence.end());
+        return seen.size() == 26;
     }
 };
