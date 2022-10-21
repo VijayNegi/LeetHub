0ca1608fa57418886df2918949d610f2c7b06d3c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-21 16:36:58 +0530

    Time: 195 ms (90.88%), Space: 72.3 MB (91.47%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    // map solution
+    bool containsNearbyDuplicate1(vector<int>& nums, int k) {
         unordered_map<int,int> seenIdx;
         int n = nums.size();
         for(int i=0;i<n;++i) {
@@ -9,6 +10,19 @@ public:
                     return true;
             }
             seenIdx[nums[i]]=i;
+        }
+        return false;
+    }
+    // set solution
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> st;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) {
+                st.erase(nums[i - k - 1]);
+            }
+            if (!st.insert(nums[i]).second) {
+                return true;
+            }
         }
         return false;
     }
