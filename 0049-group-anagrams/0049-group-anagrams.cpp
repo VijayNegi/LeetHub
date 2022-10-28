bafcf67d677b4ae8373eba9c2c500c33a8d2f72b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-28 15:33:36 +0530

    Time: 89 ms (32.21%), Space: 20.6 MB (56.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    // new : 31 ms
+    vector<vector<string>> groupAnagrams1(vector<string>& strs) {
         vector<vector<string>> result;
         vector<pair<string,int>> sorted;
         for(int i=0;i<strs.size();++i){
@@ -18,5 +19,33 @@ public:
             pre = s.first;
         }
         return result;    
+    }
+    
+    // old solution
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mAna;
+        
+        for(auto n: strs)
+        {
+            mAna[strSort(n)].push_back(n);
+        }
+        
+        vector<vector<string>> result;
+        for(auto vec:mAna)
+        {
+            result.push_back(vec.second);
+        }
+        return result;
+    }
+    string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t.append(counter[c], c + 'a');
+        }
+        return t;
     }
 };
