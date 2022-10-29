b7d7af5a9b2f26ffc70874768cbe12c4d02af302
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-29 22:41:43 +0530

    Time: 163 ms (62.50%), Space: 58.9 MB (25.00%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int destroyTargets(vector<int>& nums, int space) {
+    int destroyTargets1(vector<int>& nums, int space) {
         
         unordered_map<int,int> mod;
         unordered_map<int,int> idx;
@@ -24,5 +24,19 @@ public:
             result = max(result,m.second);
         }
         return id;
+    }
+    int destroyTargets(vector<int>& nums, int space) {
+        int ans = INT_MAX;
+        unordered_map<int, int> mp;
+        int mx = INT_MIN;
+        for(auto n: nums){
+            int r = n % space;                  //evaluate reminder
+            mp[r]++;                               // add reminder to map
+            if(mx < mp[r])  mx = mp[r];   //keep track of the max count, with same reminder
+        }
+        for(auto n: nums){  //scan smalest element with same riminder as of mx
+            if(mx == mp[n%space])  ans = min(ans, n);
+        }
+        return ans;
     }
 };
