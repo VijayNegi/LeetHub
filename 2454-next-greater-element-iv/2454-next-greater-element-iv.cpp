34d0e33621dd2abae4240c7d2e67484f691748a7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-29 23:12:04 +0530

    Time: 352 ms (12.50%), Space: 71 MB (12.50%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> secondGreaterElement(vector<int>& nums) {
+    //self: 295ms
+    vector<int> secondGreaterElement1(vector<int>& nums) {
         int n =nums.size();
         vector<int> nextGreater(n,n);
         vector<int> stk;
@@ -26,7 +27,30 @@ public:
                     result.push_back(-1); 
             }
         }
-        
         return result;
+    }
+    // just more cleaner
+    vector<int> secondGreaterElement(vector<int>& nums) {
+        int n = nums.size();
+        int nextBig[n+4];
+        nextBig[n]=nextBig[n-1]=n;
+        for(int i=n-1; i>=0; i--){
+            int pos=i+1;
+            while(pos<n && nums[pos]<= nums[i] )
+                pos = nextBig[pos];
+            nextBig[i]=pos;
+        }
+        vector<int>ans(n);
+        int pos;
+        for(int i=0; i<n; i++){
+            pos = nextBig[i];
+            pos++;
+            while(pos<n && nums[pos]<=nums[i]){
+                pos = nextBig[pos];
+            }
+            if(pos<n) ans[i]=nums[pos];
+            else ans[i]=-1;
+        }
+        return ans;
     }
 };
