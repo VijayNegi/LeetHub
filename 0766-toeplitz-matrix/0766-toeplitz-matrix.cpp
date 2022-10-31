2a75eef256941820881308b1cc8aa185f16cb090
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-31 23:31:04 +0530

    Time: 31 ms (53.41%), Space: 17.4 MB (85.40%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+    // self: 50 ms
+    bool isToeplitzMatrix1(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
         
@@ -17,6 +18,16 @@ public:
                         return false;
                 }
             }
+        return true;
+    }
+    // solution
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for(int r=1;r<rows;++r)
+            for(int c=1;c<cols;++c)
+                if(matrix[r][c] != matrix[r-1][c-1])
+                    return false;
         return true;
     }
 };
