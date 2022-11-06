6a8fe090a75633baf7b6a3d0cbc1342cc9207ab9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-06 12:11:25 +0530

    Time: 14 ms (21.26%), Space: 10.8 MB (32.18%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    string orderlyQueue(string s, int k) {
+    // self: 4 ms
+    string orderlyQueue1(string s, int k) {
         int n = s.size();
         string res = s;
         if(k==1){
@@ -23,6 +24,16 @@ public:
         else{
             sort(res.begin(),res.end());
         }
+        return res;
+    }
+      string orderlyQueue(string S, int K) {
+        if (K > 1) {
+            sort(S.begin(), S.end());
+            return S;
+        }
+        string res = S;
+        for (int i = 1; i < S.length(); i++)
+            res = min(res, S.substr(i) + S.substr(0, i));
         return res;
     }
 };
