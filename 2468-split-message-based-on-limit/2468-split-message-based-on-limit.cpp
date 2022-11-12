9b3368d0a82874472183fcc51312f55f103ab74d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-13 00:32:21 +0530

    Time: 175 ms (15.79%), Space: 38 MB (10.53%) - LeetHub

--- 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
+++ 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
@@ -5,29 +5,29 @@ public:
         int n = message.size();
         //cout<<"n="<<n<<endl;
         int suffixLen = 3; // without digits
-        int maxSegment = 0;
-        int extraDigits = 0;
+        int maxSegment = 0; // maxSegments for curr no of digits
+        int extraDigits = 0; // Note: previous segments had one less digits and while counting available digits we need to add these extra digits
         int digit = 1;
         while(digit < 5){
             suffixLen += 2; // add new digits
             if(limit <= suffixLen)
                 return result;
             maxSegment = maxSegment*10+9;
-            //cout<<digit<<" "<<suffixLen<<" "<<maxSegment<<endl;
             // check if string can be fit in given digit segment length
-            if(limit*maxSegment - (suffixLen*maxSegment - extraDigits) >= n) 
+            if(limit*maxSegment + extraDigits - suffixLen*maxSegment >= n) 
             {
                 int pos = 0;
                 int seg = 1;
+                string suffix = "/"+ to_string(maxSegment)+ ">";
                 while(pos<n) {
-                    string temp = "<"+to_string(seg)+"/"+ to_string(maxSegment)+ ">";
+                    string temp = "<"+to_string(seg)+suffix;
                     int r = limit - temp.size();
                     temp = message.substr(pos,r)+ temp;
                     result.push_back(temp);
                     pos +=r;
                     ++seg;
                 }
-               
+                // replace total segment with exact numbers
                 string s = to_string(--seg);
                 for(auto& r:result)
                     r.replace(r.size()-(digit+1),digit,s);
