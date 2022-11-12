86966a80d29b3f0b8497a508e5502e8641d07919
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-13 00:28:10 +0530

    Time: 313 ms (15.79%), Space: 38 MB (10.53%) - LeetHub

--- 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
+++ 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
@@ -4,98 +4,38 @@ public:
         vector<string> result;
         int n = message.size();
         //cout<<"n="<<n<<endl;
-        int suffixLen = 5;
-        if(limit <= suffixLen)
-            return result;
-        // check if string can be fit in 1-9 segment range i.e. single digit segment length
-        // max chars - suffix
-        if(limit*9 - suffixLen*9 >= n) 
-        {
-            int pos = 0;
-            int seg = 1;
-            while(pos<n) {
-                string temp = "<"+to_string(seg)+"/"+"9>";
-                int r = limit - temp.size();
-                temp = message.substr(pos,r)+ temp;
-                result.push_back(temp);
-                pos +=r;
-                ++seg;
-            }
-            string s = to_string(--seg);
-            int start = limit -2;
-            for(auto& r:result)
-                r.replace(r.size()-2,1,s);
+        int suffixLen = 3; // without digits
+        int maxSegment = 0;
+        int extraDigits = 0;
+        int digit = 1;
+        while(digit < 5){
+            suffixLen += 2; // add new digits
+            if(limit <= suffixLen)
+                return result;
+            maxSegment = maxSegment*10+9;
+            //cout<<digit<<" "<<suffixLen<<" "<<maxSegment<<endl;
+            // check if string can be fit in given digit segment length
+            if(limit*maxSegment - (suffixLen*maxSegment - extraDigits) >= n) 
+            {
+                int pos = 0;
+                int seg = 1;
+                while(pos<n) {
+                    string temp = "<"+to_string(seg)+"/"+ to_string(maxSegment)+ ">";
+                    int r = limit - temp.size();
+                    temp = message.substr(pos,r)+ temp;
+                    result.push_back(temp);
+                    pos +=r;
+                    ++seg;
+                }
+               
+                string s = to_string(--seg);
+                for(auto& r:result)
+                    r.replace(r.size()-(digit+1),digit,s);
 
-            return result;
-        }
-        // check if string can be fit in 1-99 segment range i.e. two digit segment length
-        suffixLen = 7;
-        if(limit <= suffixLen)
-            return result;
-        
-        if(limit*99 - (suffixLen*99 - 9) >= n) 
-        {
-            int pos = 0;
-            int seg = 1;
-            while(pos<n) {
-                string temp = "<"+to_string(seg)+"/"+"99>";
-                int r = limit - temp.size();
-                temp = message.substr(pos,r)+ temp;
-                result.push_back(temp);
-                pos +=r;
-                ++seg;
+                return result;
             }
-            string s = to_string(--seg);
-            int start = limit - 3;
-            for(auto& r:result)
-                r.replace(r.size()-3,2,s);
-            return result;
-        }
-        // check if string can be fit in 1-999 segment range i.e. three digit segment length
-        suffixLen = 9;
-        if(limit <= suffixLen)
-            return result;
-        
-        if(limit*999 - (suffixLen*999 - 9 - 99) >= n) 
-        {
-            int pos = 0;
-            int seg = 1;
-            while(pos<n) {
-                string temp = "<"+to_string(seg)+"/"+"999>";
-                int r = limit - temp.size();
-                temp = message.substr(pos,r)+temp;
-                result.push_back(temp);
-                pos +=r;
-                ++seg;
-            }
-            string s = to_string(--seg);
-            int start = limit - 4;
-            for(auto& r:result)
-                r.replace(r.size()-4,3,s);
-            return result;
-        }
-        
-        // check if string can be fit in 1-9999 segment range i.e. four digit segment length
-        suffixLen = 11;
-        if(limit <= suffixLen)
-            return result;
-        
-        if(limit*9999 - (suffixLen*9999 - 9 - 99 - 999) >= n) 
-        {
-            int pos = 0;
-            int seg = 1;
-            while(pos<n) {
-                string temp = "<"+to_string(seg)+"/"+"9999>";
-                int r = limit - temp.size();
-                temp = message.substr(pos,r)+temp;
-                result.push_back(temp);
-                pos +=r;
-                ++seg;
-            }
-            string s = to_string(--seg);
-            for(auto& r:result)
-                r.replace(r.size()-5,4,s);
-            return result;
+            extraDigits += maxSegment;
+            ++digit;
         }
         return result;
     }
