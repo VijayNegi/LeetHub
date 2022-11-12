ec096e6a0534f96f11aaa50dc8e3ec47dcc72d6c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-13 00:11:21 +0530

    Time: 261 ms (15.79%), Space: 38.1 MB (10.53%) - LeetHub

--- 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
+++ 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cpp
@@ -3,20 +3,22 @@ public:
     vector<string> splitMessage(string message, int limit) {
         vector<string> result;
         int n = message.size();
-        cout<<"n="<<n<<endl;
-        int d = 5;
-        if(limit<6)
+        //cout<<"n="<<n<<endl;
+        int suffixLen = 5;
+        if(limit <= suffixLen)
             return result;
-        if(limit*9 - d*9 >= n) 
+        // check if string can be fit in 1-9 segment range i.e. single digit segment length
+        // max chars - suffix
+        if(limit*9 - suffixLen*9 >= n) 
         {
-            int l = 0;
+            int pos = 0;
             int seg = 1;
-            while(l<n) {
+            while(pos<n) {
                 string temp = "<"+to_string(seg)+"/"+"9>";
                 int r = limit - temp.size();
-                temp = message.substr(l,r)+ temp;
+                temp = message.substr(pos,r)+ temp;
                 result.push_back(temp);
-                l +=r;
+                pos +=r;
                 ++seg;
             }
             string s = to_string(--seg);
@@ -26,19 +28,21 @@ public:
 
             return result;
         }
-        d=6;
-        if(limit<7)
+        // check if string can be fit in 1-99 segment range i.e. two digit segment length
+        suffixLen = 7;
+        if(limit <= suffixLen)
             return result;
-        if(limit*99 - (d*99 - 9) >= n) 
+        
+        if(limit*99 - (suffixLen*99 - 9) >= n) 
         {
-            int l = 0;
+            int pos = 0;
             int seg = 1;
-            while(l<n) {
+            while(pos<n) {
                 string temp = "<"+to_string(seg)+"/"+"99>";
                 int r = limit - temp.size();
-                temp = message.substr(l,r)+ temp;
+                temp = message.substr(pos,r)+ temp;
                 result.push_back(temp);
-                l +=r;
+                pos +=r;
                 ++seg;
             }
             string s = to_string(--seg);
@@ -47,19 +51,21 @@ public:
                 r.replace(r.size()-3,2,s);
             return result;
         }
-        d=7;
-        if(limit<10)
+        // check if string can be fit in 1-999 segment range i.e. three digit segment length
+        suffixLen = 9;
+        if(limit <= suffixLen)
             return result;
-        if(limit*999 - (d*999 - 9 - 99) >= n) 
+        
+        if(limit*999 - (suffixLen*999 - 9 - 99) >= n) 
         {
-            int l = 0;
+            int pos = 0;
             int seg = 1;
-            while(l<n) {
+            while(pos<n) {
                 string temp = "<"+to_string(seg)+"/"+"999>";
                 int r = limit - temp.size();
-                temp = message.substr(l,r)+temp;
+                temp = message.substr(pos,r)+temp;
                 result.push_back(temp);
-                l +=r;
+                pos +=r;
                 ++seg;
             }
             string s = to_string(--seg);
@@ -68,29 +74,25 @@ public:
                 r.replace(r.size()-4,3,s);
             return result;
         }
-        d=8;
-        if(limit<12)
+        
+        // check if string can be fit in 1-9999 segment range i.e. four digit segment length
+        suffixLen = 11;
+        if(limit <= suffixLen)
             return result;
-        cout<<"check";
-        //return result;
-        if(limit*9999 - (d*9999 - 9 - 99 - 999) >= n) 
+        
+        if(limit*9999 - (suffixLen*9999 - 9 - 99 - 999) >= n) 
         {
-            int l = 0;
+            int pos = 0;
             int seg = 1;
-            while(l<n) {
+            while(pos<n) {
                 string temp = "<"+to_string(seg)+"/"+"9999>";
                 int r = limit - temp.size();
-                if(l>=n-1000) {
-                    cout<<l<<"," <<r<<"\n";
-                }
-                temp = message.substr(l,r)+temp;
+                temp = message.substr(pos,r)+temp;
                 result.push_back(temp);
-                l +=r;
+                pos +=r;
                 ++seg;
-                
             }
             string s = to_string(--seg);
-            //int start = limit - 4;
             for(auto& r:result)
                 r.replace(r.size()-5,4,s);
             return result;
