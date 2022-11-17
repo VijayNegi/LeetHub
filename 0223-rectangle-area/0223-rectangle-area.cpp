209a72e245f65f4771083c16fa5af1879d40b412
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-18 00:20:04 +0530

    Time: 19 ms (38.56%), Space: 5.9 MB (75.43%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -3,17 +3,15 @@ public:
     // line sweep or total - common
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         int area = abs((ax2-ax1)*(ay2-ay1)) + abs((bx2-bx1)*(by2-by1));
-        //cout<<area<<endl;
-        int common = 0;
         int xtotal = abs(ax2-ax1) + abs(bx2-bx1);
         int ytotal = abs(ay2-ay1) + abs(by2-by1);
         bool xoverlap = abs(bx2-ax1) < xtotal && abs(ax2-bx1) < xtotal;
         bool yoverlap = abs(ay2-by1) < ytotal && abs(by2-ay1) < ytotal;
-        //cout<<xoverlap<<yoverlap<<endl;
-        if(xoverlap && yoverlap)
-            common = (max(ax1,bx1) - min(ax2,bx2))*(max(ay1,by1) - min(ay2,by2));
-        if(common)
+
+        if(xoverlap && yoverlap){
+            int common = (max(ax1,bx1) - min(ax2,bx2))*(max(ay1,by1) - min(ay2,by2));
             area -= common;
+        }
         return area;
     }
 };
