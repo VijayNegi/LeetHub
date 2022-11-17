01e2d950b83bdac51f62d73a6ec79fc9d428b252
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-18 00:28:55 +0530

    Time: 3 ms (99.03%), Space: 5.9 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // line sweep or total - common
-    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+    int computeArea1(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         int area = abs((ax2-ax1)*(ay2-ay1)) + abs((bx2-bx1)*(by2-by1));
         int xtotal = abs(ax2-ax1) + abs(bx2-bx1);
         int ytotal = abs(ay2-ay1) + abs(by2-by1);
@@ -13,6 +13,19 @@ public:
             area -= common;
         }
         return area;
+    }
+    int computeArea2(int A, int B, int C, int D, int E, int F, int G, int H)
+    {
+        int x = min(G, C) > max(E, A) ? (min(G, C) - max(E, A)) : 0;
+        int y = min(D, H) > max(B, F) ? (min(D, H) - max(B, F)) : 0;
+        return (D - B) * (C - A) + (G - E) * (H - F) - x * y;
+    }
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) 
+    {
+        // sum of areas of two rectangles minus the area of their intersection
+        return (ax2 - ax1) * (ay2 - ay1) + 
+             (bx2 - bx1) * (by2 - by1) - 
+             max(min(ax2,bx2) - max(ax1,bx1),0) * max(min(ay2,by2) - max(ay1,by1),0);
     }
 };
 
