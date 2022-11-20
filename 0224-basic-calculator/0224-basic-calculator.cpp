9dd95298fc4067359e635b6388fc25786a2d5144
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-20 12:19:13 +0530

    Time: 21 ms (58.65%), Space: 8.1 MB (72.15%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int calculate(string s) {
+    int calculate1(string s) {
         int n = s.size();
         function<int(string&,int&)> dfs = [&](string& s, int& pos){
             int result=0;
@@ -31,6 +31,36 @@ public:
         };
         int pos = 0;
         return dfs(s,pos);
+    }
+    // stack based
+    int calculate(string s) {
+        stack <int> nums, ops;
+        int num = 0;
+        int rst = 0;
+        int sign = 1;
+        for (char c : s) { 
+            if (isdigit(c)) {
+                num = num * 10 + ( c - '0');
+            }
+            else {
+                rst += sign * num;
+                num = 0;
+                if (c == '+') sign = 1;
+                if (c == '-') sign = -1;
+                if (c == '(') {
+                    nums.push(rst);
+                    ops.push(sign);
+                    rst = 0;
+                    sign = 1;
+                }
+                if (c == ')' && ops.size()) {
+                    rst = ops.top() * rst + nums.top();
+                    ops.pop(); nums.pop();
+                }
+            }
+        }
+        rst += sign * num;
+        return rst;
     }
 
 };
