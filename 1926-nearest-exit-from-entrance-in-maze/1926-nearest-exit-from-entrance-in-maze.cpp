37822d1af0bde60811db7783fe9b193a794b9055
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-21 09:47:57 +0530

    Time: 205 ms (78.02%), Space: 46.4 MB (10.54%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int rows = maze.size();
+        int cols = maze[0].size();
+        
+        function<bool(int,int)> isValid = [&](int r,int c){
+            if(r<0 || c<0 || r>=rows || c>=cols || maze[r][c]=='+' )
+                return false;
+            return true;
+        };
+        function<bool(int,int)> isBorder = [&](int r,int c){
+            if(r == 0 || c==0 || r==(rows-1) || c==(cols-1 ))
+                return true;
+            return false;
+        };
+        vector<int> dir{1,0,-1,0,1};
+        vector<vector<bool>> visited(rows,vector(cols,false));
+        int level=-1;
+        vector<vector<int>> queue{entrance};
+        visited[entrance[0]][entrance[1]] = true;
+        while(queue.size()){
+            vector<vector<int>> temp;
+            for(auto& p:queue){
+                if(level>=0 && isBorder(p[0],p[1]))
+                   return level+1;
+                for(int i=0;i<4;++i){
+                    if(isValid(p[0]+dir[i],p[1]+dir[i+1]) && !visited[p[0]+dir[i]][p[1]+dir[i+1]] ){
+                        temp.push_back({p[0]+dir[i],p[1]+dir[i+1]});
+                        visited[p[0]+dir[i]][p[1]+dir[i+1]] = true;
+                    }
+                }
+            }
+            queue = temp;
+            ++level;
+        }
+        return -1;
+    }
+};
