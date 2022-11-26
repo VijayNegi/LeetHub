0cd980544523278ed9edc0c16cb33b6e0b59338d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-26 23:38:08 +0530

    Time: 1270 ms (8.33%), Space: 71.2 MB (16.67%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
@@ -1,4 +1,5 @@
 const int mod = 1e9+7;
+#define ll long long
 class Solution {
 public:
     // self : TLE
@@ -27,7 +28,8 @@ public:
         }
         return result;
     }
-    int countPalindromes(string s) {
+    // o(1) space, from one contestent
+    int countPalindromes2(string s) {
 			int n = s.size();
 			long ans = 0;
 			int ff[10] = {0};
@@ -44,5 +46,45 @@ public:
 				ff[s[i] - '0']++;
 			}
 			return (int)(ans % mod);
-		}
+	}
+    ll dp[10001][11][11][6];
+    ll dfs(int ind, int first, int second, int i, string &s) {
+        if(i == 5) return 1; //found a subsequence
+        if(ind == s.length()) return 0;
+        if(dp[ind][first][second][i] != -1) return dp[ind][first][second][i];
+        
+        //option of not choosing current digit
+        long long res = dfs(ind + 1, first, second, i, s);
+
+        if(i == 0) {
+            //option of choosing the first digit of the subsequence
+            res += dfs(ind + 1, s[ind] - '0', second, i + 1, s);
+            res %= mod;
+        } else if(i == 1) {
+            //option of choosing the second digit of the subsequence
+            res += dfs(ind + 1, first, s[ind] - '0', i + 1, s);
+            res %= mod;
+        } else if(i == 2) {
+            //option of choosing the third digit of the subsequence
+            res += dfs(ind + 1, first, second, i + 1, s);
+            res %= mod;
+        } else if(i == 3) {
+            //option of choosing the fourth digit of the subsequence if it matches with the second digit
+            if(s[ind] - '0' == second) {
+                res += dfs(ind + 1, first, second, i + 1, s);
+                res %= mod;
+            }
+        } else if(i == 4) {
+            //option of choosing the fifth digit of the subsequence if it matches with the first digit
+            if(s[ind] - '0' == first) {
+                res += dfs(ind + 1, first, second, i + 1, s);
+                res %= mod;
+            }
+        }
+        return dp[ind][first][second][i] = res;
+    }
+    int countPalindromes(string s) {
+        memset(dp, -1, sizeof dp); 
+        return dfs(0, 10, 10, 0, s);
+    }
 };
