d890525b109e611e0f351f9b341555ef364b2218
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-26 13:24:36 +0530

    Time: 276 ms (67.64%), Space: 74.1 MB (11.63%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -34,7 +34,8 @@ public:
         return dfs(0,0);
     }
     // https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/409009/JavaC%2B%2BPython-DP-Solution
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    // Bottom up : 254 ms
+    int jobScheduling2(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
         vector<vector<int>> jobs;
         for (int i = 0; i < n; ++i) {
@@ -48,5 +49,26 @@ public:
                 dp[job[0]] = cur;
         }
         return dp.rbegin()->second;
+    }
+    //https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/409188/C++-with-picture/418565
+    // Bottom up : 
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        map<int, int> dp;
+        vector<vector<int>> jobs;
+        for (int i = 0; i < startTime.size(); i++) {
+            jobs.push_back(vector<int>{startTime[i], endTime[i], profit[i]});
+        }
+        sort(jobs.begin(), jobs.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] > b[0];
+        });
+        int maxT = 0;
+        for (auto job : jobs) {
+            auto it = dp.lower_bound(job[1]);
+            int pre = (it == dp.end()) ? 0 : it->second;
+            maxT = max(maxT, job[2] + pre);
+            dp[job[0]] = maxT;
+        }
+        
+        return maxT;
     }
 };
