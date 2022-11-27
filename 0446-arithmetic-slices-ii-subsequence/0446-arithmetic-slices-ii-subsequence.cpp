01514c9520b06a7ab922972038695f41193c718e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-27 19:03:31 +0530

    Time: 736 ms (89.06%), Space: 154.8 MB (51.65%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,7 +1,7 @@
 #define LL long long
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
+    int numberOfArithmeticSlices1(vector<int>& A) {
         int n = A.size();
         LL ans = 0;
         vector<map<LL, int>> cnt(n);
@@ -18,5 +18,19 @@ public:
         }
 
         return (int)ans;
+    }
+    int numberOfArithmeticSlices(vector<int> &nums) {
+        int n = nums.size();
+        int ans = 0;
+        vector<unordered_map<long long, int>> dp(n); // dp[i][d]
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long long diff = (long long) nums[i] - nums[j];
+                int cnt = dp[j].count(diff) ? dp[j][diff] : 0;
+                dp[i][diff] += cnt + 1;
+                ans += cnt;
+            }
+        }
+        return ans;
     }
 };
