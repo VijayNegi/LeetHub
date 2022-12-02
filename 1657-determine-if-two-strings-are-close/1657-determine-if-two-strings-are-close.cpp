b95da4f62bd3af4abcf4ebdd4ea3b228d5a26fd3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-02 19:01:10 +0530

    Time: 71 ms (97.31%), Space: 20.5 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -3,7 +3,7 @@ public:
     bool closeStrings(string word1, string word2) {
         if(word1.size() != word2.size())
             return false;
-        int hist1[26]= {0},hist2[26]={0};
+        vector<int> hist1(26,0),hist2(26,0);
         for(auto& c:word1)
             hist1[c-'a']++;
         for(auto& c:word2)
@@ -12,20 +12,15 @@ public:
         for(int i=0;i<26;++i){
             if(hist1[i] != hist2[i])
                 flg = false;
-            if((hist1[i] && !hist2[i]) || (!hist1[i] && hist2[i]))
+            if((hist1[i] && !hist2[i]) || (!hist1[i] && hist2[i])) // we can swap but cannot get new charactor
                 return false;
         }
         if(flg)
             return true;
-        vector<int> v1,v2;
-        for(auto& h:hist1)
-            if(h) v1.push_back(h);
-        for(auto& h:hist2)
-            if(h) v2.push_back(h);
         
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        if(v1 == v2)
+        sort(hist1.begin(),hist1.end());
+        sort(hist2.begin(),hist2.end());
+        if(hist1 == hist2)
             return true;
         return false;
         
