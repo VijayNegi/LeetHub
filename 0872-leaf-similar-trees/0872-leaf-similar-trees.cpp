2f50edbef160b67fc063790dd5cec42e192856ef
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-09 00:53:49 +0530

    Time: 4 ms (73.78%), Space: 12.7 MB (85.59%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+    bool leafSimilar1(TreeNode* root1, TreeNode* root2) {
         vector<TreeNode*> stk1,stk2;
         function<void(vector<TreeNode*>&, TreeNode*)> pushLeft = [](vector<TreeNode*>& stk, TreeNode* node) {
             while(node){
@@ -58,6 +58,22 @@ public:
         }
             
         return true;
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1;
+        vector<int> leaves2;
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+
+        return leaves1 == leaves2;
+    }
+
+    void dfs(TreeNode* node, vector<int>& leaves) {
+        if (node == NULL) return;
+        if (node->left == NULL && node->right == NULL)
+            leaves.push_back(node->val);
+        dfs(node->left, leaves);
+        dfs(node->right, leaves);
     }
     
 };
