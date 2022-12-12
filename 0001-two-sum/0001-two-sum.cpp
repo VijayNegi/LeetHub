b62d27aa820dca5d5ae8d491e6845aa7565a18df
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-12 20:34:09 +0530

    Time: 24 ms (73.24%), Space: 10.8 MB (49.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum1(vector<int>& nums, int target) {
         unordered_map<int,int> seen;
         for(int i=0;i<nums.size();++i){
             if(seen[target - nums[i]])
@@ -8,5 +8,16 @@ public:
             seen[nums[i]] = i+1;
         }
         return {0,0};
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hashmap;
+        for(int i=0;i<nums.size();++i)
+        {
+            auto it = hashmap.find(target - nums[i]);
+            if(it!=hashmap.end())
+                return {it->second,i};
+            hashmap[nums[i]] = i;
+        }
+        return {};
     }
 };
