37a288fed5d9e5dd4abc9883219c90b94453190c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-12 20:54:21 +0530

    Time: 59 ms (55.17%), Space: 13.8 MB (70.03%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> uniqueMails;
+        for(auto& mail:emails){
+            auto pos = mail.find('@');
+            string name = getName(mail.substr(0,pos));
+            uniqueMails.insert(name + mail.substr(pos,mail.size()-pos-1));
+        }
+        return uniqueMails.size();
+    }
+    string getName(string local){
+        int l=0,r=0,n=local.size();
+        while(r<n){
+            if(local[r]=='+') break;
+            if(local[r]!= '.')
+                local[l++] = local[r];
+            ++r;
+        }
+        local.resize(l);
+        return local;
+    }
+    
+};
