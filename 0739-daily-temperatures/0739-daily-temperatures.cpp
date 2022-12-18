d443ecb0c3a7da226cf1a65b4c12f8aa0a725fbf
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-18 22:14:00 +0530

    Time: 222 ms (87.07%), Space: 98.7 MB (74.20%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temp) {
-        int n = temp.size();
-        vector<int> res(n,0),stk;
-        for(int i=n-1;i>=0;--i){
-            while(stk.size() && temp[stk.back()] <= temp[i])
-                stk.pop_back();
-            if(stk.empty()){
-                res[i]=0;
-                stk.push_back(i);
+    // backward monotonic this is more efficient then forward monotonic - 144 ms
+    vector<int> dailyTemperatures(vector<int>& temps) {
+        int n = temps.size();
+        stack<int> stk;
+        vector<int> res(n,0);
+        for(int i= n-1;i>=0;--i)
+        {
+            while(!stk.empty() && temps[stk.top()]<= temps[i])
+                stk.pop();
+            if(!stk.empty())
+                res[i] = stk.top() - i;
+            
+            stk.push(i);
+        }
+        return res;
+    }
+    //forward monotonic - 231 ms
+    vector<int> dailyTemperatures2(vector<int>& temps) {
+        int n = temps.size();
+        stack<int> stk;
+        vector<int> res(n,0);
+        for(int i= 0;i<n ;++i)
+        {
+            while(!stk.empty() && temps[stk.top()]< temps[i])
+            {
+                res[stk.top()] = i - stk.top();
+                stk.pop();
             }
-            else {
-                res[i] = stk.back()-i;
-                stk.push_back(i);
+            stk.push(i);
+        }
+        return res;
+    }
+    // No stack ~ 151 ms
+    vector<int> dailyTemperatures3(vector<int>& temps) {
+        int n = temps.size();
+        stack<int> stk;
+        vector<int> res(n,0);
+        int hottest = 0;
+        for(int i= n-1;i>=0;--i)
+        {
+            if(temps[i]>= hottest)
+            {
+                hottest = temps[i];
+                continue;
             }
+            int days = 1;
+            while( temps[i + days]<= temps[i])
+            {
+                days+= res[i+days];
+            }
+            res[i] = days;
         }
         return res;
     }
