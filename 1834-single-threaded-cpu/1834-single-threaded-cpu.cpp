aa26025ba1a7c964e100d3b8646b31ac70f5a0de
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-30 00:30:06 +0530

    Update 1834-single-threaded-cpu.cpp

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,5 +1,43 @@
+using vi = vector<int>;
 class Solution {
 public:
+    // self 1151 ms
+    vector<int> getOrder1(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+        for(int i=0;i<n;++i){
+            tasks[i].push_back(i);
+        }
+        auto comp = [](vi& l,vi& r){
+            if(l[1] == r[1])
+                return l[2] > r[2];
+            return l[1] > r[1];
+        };
+        sort(tasks.begin(),tasks.end());
+        priority_queue<vector<int>,vector<vector<int>>, decltype(comp)> pq(comp);
+        
+        long t = 0;
+        pq.push(tasks[0]);
+        t = tasks[0][0];
+        int i=1;
+        vector<int> result;
+        while(pq.size()){
+            auto tsk = pq.top();
+            pq.pop();
+            t += tsk[1];
+            result.push_back(tsk[2]);
+            while(i<n && t >= tasks[i][0]){
+                pq.push(tasks[i]);
+                ++i;
+            }
+            if(pq.empty() && i<n){
+                pq.push(tasks[i]);
+                t = tasks[i][0];
+                ++i;
+            }
+        }
+        return result;
+    }
+    // older solution 841 ms
     vector<int> getOrder(vector<vector<int>>& tasks) {
         
         auto cmp = [&](int left,int right) { return (tasks[left][0] > tasks[right][0]);};
