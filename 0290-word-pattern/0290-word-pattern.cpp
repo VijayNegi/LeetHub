9d0ece3b4f59cacededb035d7afd7e43ecbc11e4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-01 10:46:59 +0530

    Update 0290-word-pattern.cpp

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,68 +1,29 @@
 class Solution {
 public:
     bool wordPattern1(string pattern, string s) {
-        int n = s.size();
-        int pn = pattern.size();
-        unordered_map<char,string> ptoword;
-        unordered_map<string,char> wordtop;
-        int space = 0;
-        int pIdx = 0;
-        for(int i=0;i<n;++i)
-        {
-            if(isspace(s[i]))
-            {
-                if(pIdx==pn)
+        unordered_set<string> seen;
+        unordered_map<char,string> match;
+        string token;
+        stringstream ss(s);
+        int i=0;
+        while(getline(ss,token,' ')){
+            if(i==pattern.size())
+                return false;
+            if(match.count(pattern[i])){
+                if(match[pattern[i]] != token)
                     return false;
-                string sub = s.substr(space,i-space);
-                char c = pattern[pIdx];
-                if(ptoword.count(c))
-                {
-                    if(ptoword[c] != sub)
-                        return false;
-                }
-                else if (wordtop.count(sub))
-                {
-                     if(wordtop[sub] != c)
-                        return false;
-                }
-                else
-                {
-                    ptoword[c] = sub;
-                    wordtop[sub] = c;
-                }
-                
-                space = i+1;
-                ++pIdx;
             }
-        }
-        // check last word
-        string sub = s.substr(space,n-space);
-        char c = pattern[pIdx];
-        if(ptoword.count(c))
-        {
-            if(ptoword[c] != sub)
+            else if(seen.count(token))
                 return false;
+            match[pattern[i]] = token;
+            seen.insert(token);
+            ++i;
         }
-        else if (wordtop.count(sub))
-        {
-             if(wordtop[sub] != c)
+        if(i!=pattern.size())
                 return false;
-        }
-        else
-        {
-            ptoword[c] = sub;
-            wordtop[sub] = c;
-        }
-
-        ++pIdx;
-        
-        if(pIdx != pn)
-            return false;
-        
         return true;
     }
     // just cleaner
-    
     bool wordPattern(string pattern, string str) {
         map<char, int> p2i;
         map<string, int> w2i;
