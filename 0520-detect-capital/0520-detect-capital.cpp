7920c158cd4e2e8ca369a7bebd6539eedd421819
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-02 15:23:35 +0530

    Time: 5 ms (24.37%), Space: 6.1 MB (70.23%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     bool detectCapitalUse(string word) {
         int n = word.size();
         int count=0;
@@ -9,5 +10,43 @@ public:
         if(count==0 || count==n || (count==1 && toupper(word[0]) == word[0]))
             return true;
         return false;
+    }
+    bool detectCapitalUse1(string word) {
+        int n = word.size();
+        int caps = 0;
+        for(int i=0;i<n;++i)
+        {
+            if(isupper(word[i]))
+                ++caps;
+            
+            if(caps != i+1 && isupper(word[i]))
+                return false;
+            if(caps > 1 && islower(word[i]))
+                return false;
+        }
+        
+        return true;
+    }
+    //another way
+    bool detectCapitalUse2(string word) {
+        int cnt=0;
+        int loc;
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                cnt++;
+                loc = i;
+            }
+        }
+        if(cnt == word.length() || cnt == 0 || (cnt==1 && loc==0))
+            return true;
+        return false;
+    }
+    //another
+    bool detectCapitalUse3(string word) {
+        for(int i = 1; i < word.size(); i++){
+            if(isupper(word[1]) != isupper(word[i]) || islower(word[0]) && isupper(word[i])) 
+                return false;
+        }        
+        return true;
     }
 };
