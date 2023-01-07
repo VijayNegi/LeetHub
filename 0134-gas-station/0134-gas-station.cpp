8da15b6712d6ba2454390dfe42a12b01939b5764
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-07 18:14:26 +0530

    Update 0134-gas-station.cpp

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int tcost = std::accumulate(cost.begin(), cost.end(), 0);
-        int tgas = std::accumulate(gas.begin(), gas.end(), 0);
-        if(tgas<tcost)
-            return -1;
-        int n = gas.size();
-        int index = 0;
-        int sum = 0;
-        int minSum = INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            int mi=i%gas.size();
-            int temp = gas[mi]-cost[mi];
-            sum +=temp;
-            if(sum<=minSum)
-            {
-                minSum = sum;
-                index = mi;
+        int station=0,mingas=0,currgas=0;
+        for(int i=0;i<gas.size();++i){
+            if(currgas<mingas){
+                station=i;
+                mingas=currgas;
             }
+            currgas += gas[i]-cost[i];
         }
-        return (index+1)%n;
+        if(currgas<0)
+            return -1;
+        return station;
     }
 };
