f269ca4f1c1e1d4a56d07e943cabc5b781d1655e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-08 11:12:24 +0530

    Time: 81 ms (100.00%), Space: 39.9 MB (40.00%) - LeetHub

--- 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.cpp
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    int xorBeauty(vector<int>& nums) {
+    int xorBeauty1(vector<int>& nums) {
         int result = nums[0];
         for(int i=1;i<nums.size();++i)
             result = result ^ nums[i];
         return result;
+    }
+    int xorBeauty(const vector<int>& nums) {
+        return accumulate(begin(nums), end(nums), 0, bit_xor{});
     }
 };
