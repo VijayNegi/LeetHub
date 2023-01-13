2a84a1aaf595af16912c88219e65276b7d7450ca
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-13 13:47:00 +0530

    Time: 385 ms (94.38%), Space: 181.3 MB (65.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int longestPath(vector<int>& parent, string s) {
+    int longestPath1(vector<int>& parent, string s) {
         int n = s.size();
         vector<vector<int>> child(n+1);
         for(int i=1;i<n;++i){
@@ -25,5 +25,26 @@ public:
         };
         dfs(0);
         return result;
+    }
+    
+    int longestPath(vector<int>& parent, string s) {
+        int n = s.size(), res = 0;
+        vector<vector<int>> children(n, vector<int>());
+        for (int i = 1; i < n; ++i)
+            children[parent[i]].push_back(i);
+        dfs(children, s, res, 0);
+        return res;
+    }
+
+    int dfs(vector<vector<int>>& children, string& s, int& res, int i) {
+        int big1 = 0, big2 = 0;
+        for (int& j : children[i]) {
+            int cur = dfs(children, s, res, j);
+            if (s[i] == s[j]) continue;
+            if (cur > big2) big2 = cur;
+            if (big2 > big1) swap(big1, big2);
+        }
+        res = max(res, big1 + big2 + 1);
+        return big1 + 1;
     }
 };
