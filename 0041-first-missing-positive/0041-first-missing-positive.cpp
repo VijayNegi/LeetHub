f1153dcf6ee4e53e30d423d6b38ea2492d3b0aa5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-13 21:47:20 +0530

    Time: 39 ms (97.92%), Space: 31.4 MB (50.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -17,8 +17,8 @@ public:
         }
         return n+1;
     }
-    //more clean
-    int firstMissingPositive(vector<int>& A)
+    //more clean 52 ms
+    int firstMissingPositive2(vector<int>& A)
     {
         int n = A.size();
         for(int i = 0; i < n; ++ i)
@@ -29,6 +29,13 @@ public:
             if(A[i] != i + 1)
                 return i + 1;
         
+        return n + 1;
+    }
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
+        for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
+        for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
         return n + 1;
     }
 };
