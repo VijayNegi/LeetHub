6eb2e114b1f6285ee34e24a0144393941e227576
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-21 23:24:11 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (70.83%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<string> restoreIpAddresses(string s) {
+    vector<string> restoreIpAddresses1(string s) {
         int dots=0, n = s.size();
         vector<string> result;
         function<void(string,int )> dfs = [&](string c,int p){
@@ -10,8 +10,7 @@ public:
                 return;
             }
             if(p==n || dots==4) return;
-            
-            //if(s[p]=='0') return;
+        
             int i=0,num=0;
             while(i<3 && (i+p)<n){
                 if(i>0 && num==0) break;
@@ -26,5 +25,25 @@ public:
         };
         dfs("",0);
         return result;
+    }
+        vector<string> restoreIpAddresses(string s) {
+        vector<string> ret;
+        string ans;
+        
+        for (int a=1; a<=3; a++)
+        for (int b=1; b<=3; b++)
+        for (int c=1; c<=3; c++)
+        for (int d=1; d<=3; d++)
+            if (a+b+c+d == s.length()) {
+                int A = stoi(s.substr(0, a));
+                int B = stoi(s.substr(a, b));
+                int C = stoi(s.substr(a+b, c));
+                int D = stoi(s.substr(a+b+c, d));
+                if (A<=255 && B<=255 && C<=255 && D<=255)
+                    if ( (ans=to_string(A)+"."+to_string(B)+"."+to_string(C)+"."+to_string(D)).length() == s.length()+3)
+                        ret.push_back(ans);
+            }    
+        
+        return ret;
     }
 };
