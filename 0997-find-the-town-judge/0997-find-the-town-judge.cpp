889255298a9f602fef97b7f770741b16ca651522
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-23 14:01:27 +0530

    Time: 160 ms (98.77%), Space: 60.8 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> judge(n+1,0);
-        for(auto& v:trust){
-            judge[v[0]]--;
-            judge[v[1]]++;
+        vector<int> trustCount(n+1,0);
+        
+        for(auto& t: trust)
+        {
+            trustCount[t[0]]--; // cant be a judge
+            trustCount[t[1]]++;
         }
-        int res = -1;
-        for(int i=1;i<=n;++i){
-            if(judge[i]==n-1 && res>0) return -1;
-            else if(judge[i] == n-1) res = i;
-        }
-        return res;
+        
+        for(int i=1;i<=n;++i)
+            if(trustCount[i]==n-1)
+                return i;
+                
+        return -1;
     }
 };
