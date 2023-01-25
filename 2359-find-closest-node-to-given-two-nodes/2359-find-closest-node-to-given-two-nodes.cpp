23631fbbabd7160f5a0152d765cc305a96bbe18a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-26 00:14:45 +0530

    Time: 233 ms (64.27%), Space: 106.4 MB (48.20%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int result=node1<node2? node1:node2;
+        if(node1==node2)
+            return result;
+        int n1 = node1,n2 = node2;
+        unordered_set<int> s1{node1},s2{node2};
+        while(edges[n1] >= 0 || edges[n2]>=0){
+            if(edges[n1]>=0){
+                s1.insert(edges[n1]);
+                int t = n1;
+                n1 = edges[n1];
+                edges[t] = -1;
+            }
+            if(edges[n2]>=0){
+                s2.insert(edges[n2]);
+                int t = n2;
+                n2 = edges[n2];
+                edges[t] = -1;
+            }
+            if(s1.count(n2) && s2.count(n1)){
+                //cout<<"ddd";
+                return n1<n2? n1:n2;
+            }
+            if(s1.count(n2) ){
+                return n2;
+            }
+            else if(s2.count(n1)){
+                return n1;
+            }
+            else if(n1==n2)
+                return n1;
+            // if(s1.count(n1) && s2.count(n2))
+            //     break;
+        }
+        return -1;
+    }
+};
