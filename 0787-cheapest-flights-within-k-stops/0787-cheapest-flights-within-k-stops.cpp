dcf370f237083ecd356741968687324d4ef9813c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-27 00:40:34 +0530

    Time: 107 ms (13.30%), Space: 12.5 MB (95.82%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -32,8 +32,8 @@ public:
         }
         return result==INT_MAX?-1:result;
     }
-    // official bfs
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    // official bfs: 64 ms
+    int findCheapestPrice2(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int, int>>> adj(n);
         for (auto& e : flights) {
             adj[e[0]].push_back({e[1], e[2]});
@@ -57,6 +57,26 @@ public:
                 }
             }
             stops++;
+        }
+        return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
+    }
+    // official : Bellmen ford
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // Distance from source to all other nodes.
+        vector<int> dist(n, numeric_limits<int>::max());
+        dist[src] = 0;
+
+        // Run only K+1 times since we want shortest distance in K hops.
+        for (int i = 0; i <= k; i++) {
+            // Create a copy of dist vector.
+            vector<int> temp(dist);
+            for (auto& flight : flights) {
+                if (dist[flight[0]] != numeric_limits<int>::max()) {
+                    temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
+                }
+            }
+            // Copy the temp vector into dist.
+            dist = temp;
         }
         return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
     }
