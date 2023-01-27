a81d7472cf08f8e2d7739f76af16616f97965ee7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-27 22:57:54 +0530

    Time: 281 ms (62.01%), Space: 46.9 MB (65.18%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+    // self : 897ms
+    vector<string> findAllConcatenatedWordsInADict1(vector<string>& words) {
         sort(words.begin(),words.end(),[](string& l,string& r){return l.size()<r.size();});
         unordered_map<char,vector<string>> dict;
         vector<string> result;
@@ -22,5 +23,23 @@ public:
             dict[w[0]].push_back(w);
         }
         return result;
+    }
+    // 
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> s(words.begin(), words.end());
+        vector<string> res;
+        for (auto w : words) {
+            int n = w.size();
+            vector<int> dp(n+1);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (dp[i] == 0) continue;
+                for (int j = i+1; j <= n; j++) {
+                    if (j - i < n && s.count(w.substr(i, j-i))) dp[j] = 1;
+                }
+                if (dp[n]) { res.push_back(w); break; }
+            }
+        }
+        return res;
     }
 };
