4390bebf72e6b2c942f1fd2c0b3626a719b62217
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-03 16:53:25 +0530

    Time: 16 ms (54.72%), Space: 8.2 MB (90.43%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1) return s;
+        int n = s.size();
+        string res;
+        for(int i=0;i<numRows;++i){
+            int j=i;
+            while(j<n){
+                if(i==0 || i==(numRows-1)){
+                    res+=s[j];
+                    j+= 2*(numRows-1);
+                }
+                else{
+                    res+=s[j];
+                    cout<<s[j];
+                    j+= 2*(numRows-i-1);
+                    if(j<n)
+                        res+=s[j];
+                    j+= 2*(i);
+                }
+            }
+        }
+        return res;
+    }
+};
