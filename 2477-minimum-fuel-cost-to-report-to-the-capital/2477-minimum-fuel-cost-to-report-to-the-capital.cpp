eb70782afbf39ac5cfb8fe460da98598f5281d0b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-12 13:59:11 +0530

    Time: 624 ms (71.37%), Space: 165.6 MB (97.82%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        int n = roads.size()+1;
+        int n = roads.size()+1; // no of nodes 
         vector<int> inDegree(n,0);
         vector<vector<int>> adj(n);
         for(auto& r:roads){
@@ -12,34 +12,27 @@ public:
         }
         vector<int> seat(n,1);
         queue<int> leafs;
-        for(int i=1;i<n;++i){
+        for(int i=1;i<n;++i){ // ignore capital
             if(inDegree[i]==1)
                 leafs.push(i);
         }
         long long fuel=0;
-        long long full=0;
         while(leafs.size() ){
             int k = leafs.size();
-            //cout<<k<<"-"<<full <<endl;
-            //fuel += full;
             while(k--){
                 auto node = leafs.front();
                 leafs.pop();
-                //if(node==0) continue;
                 ++fuel;
                 fuel += (seat[node]-1)/seats;
-                //full += seat[node]/seats;
-                //seat[node] %=seats;
                 for(auto nei:adj[node]){
                     inDegree[nei]--;
                     seat[nei]+= seat[node];
-                    if(nei !=0 && inDegree[nei]==1){
+                    if(nei !=0 && inDegree[nei]==1){ // ignore capital
                         leafs.push(nei);
                     }  
                 }
             }
         }
-        //cout<<"full"<<full<<endl;
         return fuel;
     }
 };
