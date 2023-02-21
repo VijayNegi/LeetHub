efdc5652cb1a20b0c4dbcec9cebdb04088ba7ec8
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-21 16:46:30 +0530

    Time: 25 ms (82.58%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    //self
+    int singleNonDuplicate2(vector<int>& nums) {
         int l,r,n;
         n = nums.size();
         l=0,r=n-1;
@@ -24,5 +25,30 @@ public:
             }
         }
         return -1;
+    }
+    //xor
+    int singleNonDuplicate1(vector<int>& nums) {
+        int ans(0);
+        
+        for(const auto& n:nums)
+        {
+            ans ^=n;
+        }
+        return ans;
+    }
+    // O(log n)
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        
+        while(l<r)
+        {
+            int mid = l + (r-l)/2;
+            if(nums[mid] == nums[mid ^ 1])
+                l = mid +1;
+            else
+                r = mid;
+        }
+        return nums[l];
     }
 };
