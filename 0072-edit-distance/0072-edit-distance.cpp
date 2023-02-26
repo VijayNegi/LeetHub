df0ba878e9af9ecc8407c2eac9260bf43991b42b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-26 18:53:43 +0530

    Time: 6 ms (93.28%), Space: 7.4 MB (79.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,7 +2,7 @@ class Solution
 {
 public:
     vector<vector<int>> memo;
-    int minDistance(string word1, string word2)
+    int minDistance1(string word1, string word2)
     {
         memo.resize(word1.length() + 1, vector<int>(word2.length() + 1, -1));
         return minDistanceRecur(word1, word2, word1.size(), word2.size());
@@ -36,5 +36,36 @@ public:
         }
         memo[word1Index][word2Index] = minEditDistance;
         return minEditDistance;
+    }
+    // bottom up approach
+    // https://leetcode.com/problems/edit-distance/discuss/662240/C%2B%2B-Easy-DP-Solution-(Well-Commented-)
+    int minDistance(string word1, string word2) {
+// Create a table to store results of subproblems
+        int dp[word1.size()+1][word2.size()+1];
+// If first string is empty, only option is to 
+// insert all characters of second string 
+        for(int k=0; k<=word1.size(); k++)
+            dp[k][0] = k;
+// If second string is empty, only option is to 
+// remove all characters of first string
+        for(int k=0; k<=word2.size(); k++)
+            dp[0][k] = k;
+// Fill dp[][] in bottom up manner 
+        for(int i=1; i<=word1.size(); i++){
+            for(int j=1; j<=word2.size(); j++){
+//if characters at current position in 2 strings are equal
+//there will be no new operation so copy value from previous position
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+// If the last character is different, consider all 
+// possibilities and find the minimum
+                else
+                    dp[i][j] = 1 + min({dp[i][j - 1], // Insert 
+                                   dp[i - 1][j], // Remove 
+                                   dp[i - 1][j - 1]}); // Replace 
+            }
+        }
+        
+        return dp[word1.size()][word2.size()];
     }
 };
