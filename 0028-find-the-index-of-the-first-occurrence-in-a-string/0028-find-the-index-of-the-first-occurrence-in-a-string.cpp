a97e3e0c5eb3e7799c70889339be4fdd72cb7cb6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-03 22:26:52 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,32 +1,99 @@
 class Solution {
 public:
-    //KMP
-    int strStr(string haystack, string needle) {
-        int h = haystack.size(),n = needle.size();
-        if(n>h) return -1;
-        if(n==0) return 0;
-        auto pre = getPrefix(needle);
-        int k=0;
-        for(int i=0;i<h;++i){
-            while(k>0 && needle[k] != haystack[i])
-                k = pre[k-1];
-            if(needle[k]==haystack[i])
-                ++k;
-            if(k==n) return i-n+1;
+    // bruteforce
+    int strStr1(string haystack, string needle) {
+        int h = haystack.size(), n = needle.size();
+        if(n==0)
+            return 0;
+        if(n>h)
+            return -1;
+        
+        for(int i=0;i<h;++i)
+        {
+            int j=0;
+            for(;j<n;++j)
+                if(haystack[i+j]!=needle[j])
+                    break;
+           if(j==n)
+               return i;
         }
         return -1;
     }
-    vector<int> getPrefix(string str){
-        int n = str.size();
-        vector<int> pre(n,0);
-        for(int i=1;i<n;++i){
-            int j = pre[i-1];
-            while(j>0 && str[i] != str[j])
-                j = pre[j-1];
-            if(str[i]==str[j])
+    
+    vector<int> prefix_function(string s)
+    {
+        int n = s.size();
+        vector<int> pi(n,0);
+        for(int i=1;i<n;++i)
+        {
+            int j = pi[i-1];
+            while(j>0 && s[i] != s[j])
+                j = pi[j-1];
+            if(s[i]==s[j])
                 ++j;
-            pre[i]=j;
+            pi[i] = j;
         }
-        return pre;
+        return pi;
+        
     }
+    // KMP
+    int strStr2(string haystack, string needle) {
+        int h = haystack.size(), n = needle.size();
+        if(n==0)
+            return 0;
+        if(n>h)
+            return -1;
+        vector<int> pi = prefix_function(needle);
+        int k=0;
+        for(int i=0;i<h;++i)
+        {
+            while(k>0 && needle[k] != haystack[i])
+                k = pi[k-1];
+            if(needle[k]==haystack[i])
+                ++k;
+            if(k==n)
+            {
+                return i-n+1;
+            }
+        }
+        return -1;
+    }
+    
+    // start with -1
+    vector<int> prefix_function2(string s) {
+        int n = s.size();
+        vector<int> pi(n + 1, 0);
+        pi[0] = -1;
+        int k = -1;
+              
+        for (int i = 0; i < n; i++) {
+            while(k >= 0 && s[k] != s[i]) 
+                k = pi[k];
+            pi[i+1] = ++k;
+        }
+        
+        return pi;
+    }
+    
+    int strStr(string haystack, string needle) {
+        int h = haystack.size(), n = needle.size();
+        if(n==0)
+            return 0;
+        if(n>h)
+            return -1;
+        vector<int> pi = prefix_function2(needle);
+        int k=0;
+        for(int i=0;i<h;++i)
+        {
+            while(k>=0 && needle[k] != haystack[i])
+                k = pi[k];
+            ++k;
+            if(k==n)
+            {
+                return i-n+1;
+            }
+        }
+        return -1;
+    }
+    
 };
