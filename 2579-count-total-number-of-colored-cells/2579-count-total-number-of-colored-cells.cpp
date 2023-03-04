52d11cb52661233771047b5aa2769200c2917c50
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-04 23:31:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (66.67%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -7,6 +7,13 @@ public:
             result += 4*(i-1);
         return result;
     }
+    // self , sum of above series
+    long long coloredCells(int n) {
+        long long result=0;
+        --n;
+        result = 1ll *n*(n+1)/2; // sum of natural numbers
+        return 1+result*4; // multiply by four
+    }
     //https://leetcode.com/problems/count-total-number-of-colored-cells/discuss/3256196/JavaC%2B%2BPython-Cut-and-Combine-O(1)
     long long coloredCells2(int n) {
         return 1l * n * n + 1l * (n - 1) * (n - 1);
@@ -15,7 +22,7 @@ public:
         return 2l * n * (n - 1) + 1;
     }
     // https://leetcode.com/problems/count-total-number-of-colored-cells/discuss/3256418/Sum-of-arithmetic-mean-oror-Very-Simple-and-Easy-to-understand-solution
-    long long coloredCells(int n) {
+    long long coloredCells4(int n) {
         if(n == 1) return 1;
         long long t  = n-1;
         return 1 + (t * (8 + (t-1)*4))/2;
