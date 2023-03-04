12e8d46bfa7ac109cc14784fb5f1a613bd83a17e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-04 22:48:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (66.67%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int splitNum(int num) {
+    // self, contest
+    int splitNum1(int num) {
         vector<int> count(10,0);
         string str= to_string(num);
         int n = str.size();
@@ -26,5 +27,24 @@ public:
             }
         }
         return result;
+    }
+    int splitNum2(int num) 
+    {
+        string a,b,s=to_string(num);
+        sort(s.begin(),s.end());
+        for(int i=0;i<s.size();i++) i&1? b.push_back(s[i]):a.push_back(s[i]);
+        return stoll(a)+stoll(b);
+    }
+    int splitNum(int num) {
+        vector<int> v;
+        while(num > 0){
+            v.push_back(num%10);
+            num = num/10;
+        }
+        sort(v.begin(), v.end());
+        int n1 = 0, n2 = 0;
+        for(int i = 0; i < v.size(); i = i+2){ n1 = n1*10 + v[i]; }
+        for(int i = 1; i < v.size(); i = i+2){ n2 = n2*10 + v[i]; }
+        return n1 + n2;
     }
 };
