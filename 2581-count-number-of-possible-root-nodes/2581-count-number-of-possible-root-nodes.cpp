c5499e9e937fc9942822aeeee5cd34eada7f4a0b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-05 13:51:37 +0530

    Time: 778 ms (100.00%), Space: 276.7 MB (100.00%) - LeetHub

--- 2581-count-number-of-possible-root-nodes/2581-count-number-of-possible-root-nodes.cpp
+++ 2581-count-number-of-possible-root-nodes/2581-count-number-of-possible-root-nodes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int rootCount(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) {
+    // self
+    int rootCount1(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) {
         int n = edges.size()+1;
         vector<vector<int>> adj(n);
         for(auto& e:edges){
@@ -13,13 +14,9 @@ public:
         int result=0;
         int gcount=0;
         map<pair<int,int>,int> dp;
-        //vector<vector<int>> dp(n+1,vector<int>(n,-1));
-        //vector<bool> visited(n,false);
         function<int(int,int)> dfs = [&](int node,int parent){
             if(dp.count({parent,node}))
                 return dp[{parent,node}];
-            //if(dp[parent][node] != -1)
-            //    return dp[parent][node];
             int gcount = 0;
             for(auto& nei:adj[node]){
                 if(nei!= parent){
@@ -28,7 +25,6 @@ public:
                     gcount += dfs(nei,node);
                 }
             }
-            //return dp[parent][node] = gcount;
             return dp[{parent,node}] = gcount;
         };
         for(int i=0;i<n;++i){
@@ -36,7 +32,55 @@ public:
             if(gk>=k)
                 ++result;
         }
-        
         return result;
     }
+    // rerooting 
+     int rootCount(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) {
+        int n = edges.size()+1;
+        vector<vector<int>> adj(n);
+        for(auto& e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        unordered_map<int,unordered_set<int>> gset;
+        for(auto& g:guesses)
+            gset[g[0]].insert(g[1]);
+         vector<int> dp(n,0),res(n,0);
+         function<int(int,int)> dfs = [&](int node,int parent){
+            int gcount = 0;
+            for(auto& nei:adj[node]){
+                if(nei!= parent){
+                    if(gset[node].count(nei))
+                        ++gcount;
+                    gcount += dfs(nei,node);
+                }
+            }
+            return dp[node] = gcount;
+        };
+         function<void(int,int)> moveRoot = [&](int pre,int curr ){
+             if(gset[pre].count(curr))
+                 dp[pre]--;
+             int temp = dp[pre];
+             dp[pre] -= dp[curr];
+             dp[curr] = temp;
+             if(gset[curr].count(pre))
+                 dp[curr]++;
+         };
+         function<void(int,int)> solve = [&](int node,int parent){
+            res[node] = dp[node];
+            for(auto& nei:adj[node]){
+                if(nei!= parent){
+                    moveRoot(node,nei);
+                    solve(nei,node);
+                    moveRoot(nei,node);
+                }
+            }
+         };
+         dfs(0,-1);
+         solve(0,-1);
+         int result=0;
+         for(auto& c:res)
+             if(c>=k) ++result;
+         return result;
+     }
 };
