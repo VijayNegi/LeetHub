6e06dacf5c24421d0eda668ff98be0d9e9d9eacc
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-05 15:50:04 +0530

    Update 1345-jump-game-iv.cpp

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -2,61 +2,41 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        if(n<3)
-            return n-1;
-        
-        unordered_map<int,vector<int>> value_indexes;
-        
-        for(int i=0;i<n;++i)
-            value_indexes[arr[i]].push_back(i);
-        
-        vector<bool> visited(n,false);
-        int dist = 0;
-        list<int> que;
-        que.push_back(0);
-        visited[0] = true;
-        list<int> child;
-        //bfs
-        while(!que.empty())
-        {
-            int idx = que.front();
-            que.pop_front();
-            //visited[idx] = true;
-            
-            if(idx == n-1)
-                return dist;
-            
-            if(idx-1>=0 && !visited[idx-1])
-            {
-                child.push_back(idx-1);
-                visited[idx-1] = true;
-            }
-                
-            if(idx+1<n && !visited[idx+1])
-            {
-                child.push_back(idx+1);
-                visited[idx+1] = true;
-            }
-                
-            
-            for(auto& i:value_indexes[arr[idx]])
-            {
-                if(visited[i])
-                    continue;
-                child.push_back(i);
-                visited[i] = true;
-            }
-            
-            value_indexes[arr[idx]].clear();
-                        
-            if(que.empty())
-            {
-                ++dist;
-                swap(que,child);
-            }
-            
+        map<int,vector<int>> val2id;
+        for(int i=0;i<n;++i){
+            val2id[arr[i]].push_back(i);
         }
-        
-        return dist;
+        queue<int> q;
+        q.push(0);
+        int depth=0;
+        vector<bool> visited(n,false);
+        visited[0] = true;
+        while(q.size()){
+            int k = q.size();
+            while(k--){
+                int next = q.front();
+                q.pop();
+                if(next == n-1)
+                    return depth;
+                if(next>0 && !visited[next-1]){
+                    q.push(next-1);
+                    visited[next-1] = true;
+                }
+                    
+                if(next<n-1 && !visited[next+1]){
+                    q.push(next+1);
+                    visited[next+1] = true;
+                }
+                for(auto& i:val2id[arr[next]]){
+                    if(!visited[i]){
+                        q.push(i);
+                        visited[i] = true;
+                    }
+                }
+                val2id[arr[next]].clear(); // imp optimization
+            }
+            ++depth;
+        }
+        return -1;
     }
 };
