52bcf9ca962f16edfe053c698dde47dc55627de9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-23 14:57:09 +0530

    Time: 165 ms (31.40%), Space: 59.2 MB (12.57%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -19,16 +19,43 @@ struct DSU{
 };
 class Solution {
 public:
-    // Dsu
-    int makeConnected(int n, vector<vector<int>>& connections) {
+    // self : Dsu
+    int makeConnected1(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) {
+            return -1;
+        }
         DSU ds(n);
         int edges = 0;
         for(auto& v: connections){
             if(!ds.merge(v[0],v[1]))
                 ++edges;
         }
-        if(ds.sets <= edges+1)
-            return ds.sets-1;
-        return -1;
+        return ds.sets-1;
+    }
+    // dfs solution
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n - 1)
+            return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<bool> visited(n, false);
+        function<void(int)> dfs = [&](int src) {
+            visited[src] = true;
+            for(int i : adj[src])
+                if(!visited[i])
+                    dfs(i);
+        };
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i]) {
+                dfs(i);
+                components++;
+            }
+        return components - 1;
     }
 };
