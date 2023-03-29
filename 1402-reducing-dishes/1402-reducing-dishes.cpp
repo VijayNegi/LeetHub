7d37bb125a2f74625c9bf5554d22060b7aa299a5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-29 18:05:52 +0530

    Time: 4 ms (88.09%), Space: 8.1 MB (89.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int maxSatisfaction(vector<int>& sat) {
+    // n2 sol
+    int maxSatisfaction1(vector<int>& sat) {
         sort(begin(sat),end(sat));
         int result = 0,n=sat.size();
         for(int i=0;i<n;++i){
@@ -9,6 +10,18 @@ public:
                 curr += sat[j]*(j-i+1);
             }
             result = max(result,curr);
+        }
+        return result;
+    }
+    // nlog(n)
+    int maxSatisfaction(vector<int>& sat) {
+        sort(begin(sat),end(sat));
+        int result = 0,n=sat.size();
+        int pre=0;
+        for(int i=n-1;i>=0;--i){
+            if(i<n-1) sat[i] += sat[i+1];
+            pre += sat[i];
+            result = max(result,pre);
         }
         return result;
     }
