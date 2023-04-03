a2029ca2f0b4d95ce2077d58052c6b25e80c54e7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-03 12:07:29 +0530

    Time: 91 ms (48.71%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -4,14 +4,13 @@ public:
         int result=0;
         int l=0,r=people.size()-1;
         sort(begin(people),end(people));
-        while(l<r){
+        while(l<=r){
             if(people[l]+people[r]<= limit)
                 ++l,--r;
             else
                 --r;
              ++result;
         }
-        if(l==r)++result;
         return result;
     }
 };
