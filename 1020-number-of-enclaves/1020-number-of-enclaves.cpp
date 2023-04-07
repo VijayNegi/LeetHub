4f2316f26d4e4d87a63c9249bf03c2dbd46c9b77
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-07 17:36:39 +0530

    Time: 115 ms (11.39%), Space: 55.4 MB (5.49%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> visited(rows,vector(cols,false));
+        int curr=0;
+        function<bool(int,int)> dfs = [&](int r,int c){
+            if(r<0 || r>=rows || c<0 || c>=cols)
+                return false;
+            if(!grid[r][c] || visited[r][c]) return true;
+            int dir[] = {0,-1,0,1,0};
+            visited[r][c] = true;
+            ++curr;
+            bool res = true;
+            for(int i=0;i<4;++i){
+                if(!dfs(r+dir[i],c+dir[i+1]))
+                    res = false;
+            }
+            return res;
+        };
+        int result=0;
+        for(int i=0;i<rows;++i)
+            for(int j=0;j<cols;++j){
+                curr=0;
+                if(grid[i][j] && !visited[i][j] && dfs(i,j))
+                    result+=curr;
+            }
+        return result;
+    }
+};
