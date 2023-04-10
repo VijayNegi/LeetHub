996fecee5e53e49b7140f406cf2e8aa6738d57a7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-10 14:11:35 +0530

    Update 0020-valid-parentheses.cpp

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> stk;
-        
-        for(const auto& c:s)
-        {
-            if(c =='(' || c=='{' || c=='[')
-            {
-                if(c=='(')
-                    stk.push(')');
-                else if(c=='{')
-                    stk.push('}');
-                else
-                    stk.push(']');
-            }
-            else
-            {
-                if(!stk.empty() && stk.top()==c)
-                {
-                    stk.pop();
-                }
-                else
-                    return false;
-            }
-        }
-        if(stk.empty())
-            return true;
-        return false;
-    }
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto& c:s){
+            if(c=='{')
+                stk.push('}');
+            else if(c=='[')
+                stk.push(']');
+            else if(c=='(')
+                stk.push(')');
+            else
+            {
+                if(stk.empty() || stk.top() != c) return false;
+                stk.pop();
+            }
+        }
+        return stk.size()==0;
+    }
 };
