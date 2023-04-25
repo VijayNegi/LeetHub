0acfea367d6f8df6a3937ed85c1f8de30f763409
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-25 23:24:50 +0530

    Time: 67 ms (96.05%), Space: 35.6 MB (77.45%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,3 +1,4 @@
+/* self
 class SmallestInfiniteSet {
     set<int> exclude;
 public:
@@ -27,6 +28,33 @@ public:
     
     void addBack(int num) {
         exclude.erase(num);
+    }
+};
+*/ 
+class SmallestInfiniteSet {
+    set<int> addedIntegers;
+    int currentInteger;
+public:
+    SmallestInfiniteSet() {
+        currentInteger = 1;
+    }
+    int popSmallest() {
+        int answer;
+        if (!addedIntegers.empty()) {
+            answer = *addedIntegers.begin();
+            addedIntegers.erase(addedIntegers.begin());
+        } 
+        else {
+            answer = currentInteger;
+            currentInteger += 1;
+        }
+        return answer;
+    }
+    void addBack(int num) {
+        if (currentInteger <= num) {
+            return;
+        }
+        addedIntegers.insert(num);
     }
 };
 
