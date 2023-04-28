a0c35142dca03265f71238e8a3401d9de64b46ce
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-28 11:38:34 +0530

    Time: 202 ms (43.49%), Space: 10.2 MB (76.83%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,45 @@
+struct DSU{
+    int s;
+    vector<int> parent;
+    DSU(int n):s(n),parent(n){
+        iota(begin(parent),end(parent),0);
+    }
+    int find(int id){
+        if(parent[id]==id) return id;
+        return parent[id] = find(parent[id]);
+    }
+    void merge(int id1,int id2){
+        int pid1 = find(id1);
+        int pid2 = find(id2);
+        if(pid1 == pid2) return;
+        parent[pid2] = pid1;
+        --s;
+    }
+};
+class Solution {
+public:
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        int k = strs[0].size();
+        if(k<3) return 1;
+        DSU diset(n);
+        // unordered_map<string,int> strId;
+        // for(int i=0;i<n;++i)
+        //     strId[strs[i]] = i;
+        auto f = [&](string& str1,string& str2){
+            int diff=0;
+            for(int i=0;i<k;++i)
+                if(str1[i]!=str2[i]) ++diff;
+            return diff;
+        };
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                int d =  f(strs[i],strs[j]);
+                if(d==0 || d==2)
+                    diset.merge(i,j);
+            }
+            
+        }
+        return diset.s;
+    }
+};
