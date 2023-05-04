56e7f164f2351a3f35d7b07fc3ab8261ab232696
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-04 10:15:56 +0530

    Time: 8 ms (72.14%), Space: 7.1 MB (76.34%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    string predictPartyVictory(string senate) {
+    // self
+    string predictPartyVictory1(string senate) {
         int rd=0,rr=0;
         int s= senate.size();
         while(true){
@@ -29,4 +30,42 @@ public:
         if(senate[0]=='R') return "Radiant";
         return "Dire";
     }
+    // Official solution
+    string predictPartyVictory(string senate) {
+
+        int rCount = 0, dCount = 0;
+        int dFloatingBan = 0, rFloatingBan = 0;
+
+        queue<char> q;
+        for (char c : senate) {
+            q.push(c);
+            if (c == 'R') rCount++;
+            else dCount++;
+        }
+
+        while (rCount && dCount) {
+            char curr = q.front();
+            q.pop();
+
+            if (curr == 'D') {
+                if (dFloatingBan) {
+                    dFloatingBan--;
+                    dCount--;
+                } else {
+                    rFloatingBan++;
+                    q.push('D');
+                }
+            } else {
+                if (rFloatingBan) {
+                    rFloatingBan--;
+                    rCount--;
+                } else {
+                    dFloatingBan++;
+                    q.push('R');
+                }
+            }
+        }
+        return rCount ? "Radiant" : "Dire";
+    }
+
 };
