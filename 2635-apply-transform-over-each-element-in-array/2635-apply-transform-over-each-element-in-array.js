6a5f547da63752dcff8905f0b53127e674a75e9d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-08 11:28:42 +0530

    Time: 57 ms (63.25%), Space: 42.1 MB (37.61%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -6,8 +6,25 @@
 var map1 = function(arr, fn) {
     return arr.map(fn);
 };
-var map = function(arr, fn) {
+// InMemory transform
+var map2 = function(arr, fn) {
     for(let i=0;i<arr.length;++i)
         arr[i] = fn(arr[i],i)
     return arr
+};
+// Initial empty array
+var map3 = function(arr, fn) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+        newArr[i] = fn(arr[i], i);
+    }
+    return newArr;
+};
+// works for sparse array
+var map = function(arr, fn) {
+    const newArr = new Array(arr.length);
+    for (const i in arr) {
+        newArr[i] = fn(arr[i], Number(i));
+    }
+    return newArr;
 };
