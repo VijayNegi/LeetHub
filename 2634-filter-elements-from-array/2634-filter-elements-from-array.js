b35d1c2c713427656142061bf3f3dd22c983b068
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-10 00:08:16 +0530

    Time: 61 ms (39.20%), Space: 42.1 MB (50.33%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -3,11 +3,23 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
+var filter1 = function(arr, fn) {
     const res = [] 
     for (const i in arr) {
         if(fn(arr[i], Number(i)))
             res.push(arr[i])
     }
     return res
+};
+var filter2 = function(arr, fn) {
+    return arr.filter(fn);
+};
+var filter = function(arr, fn) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+        if (fn(arr[i], i)) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
