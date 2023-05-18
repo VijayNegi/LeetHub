c2435a0802e70facc26b3cc8b068dd3824861196
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-18 23:54:46 +0530

    Time: 60 ms (57.98%), Space: 42.2 MB (25.04%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,4 +1,4 @@
-
+// Approach 2: setTimeout + clearTimeout + Function Syntax
 var TimeLimitedCache = function() {
      this.cache = new Map();
 };
@@ -41,3 +41,96 @@ TimeLimitedCache.prototype.count = function() {
  * obj.get(1) // 42
  * obj.count() // 1
  */
+
+// Approach 1: setTimeout + clearTimeout + Class Syntax
+class TimeLimitedCache1 {
+  cache = new Map();
+
+  set(key, value, duration) {
+    const valueInCache = this.cache.get(key);
+    if (valueInCache) {
+      clearTimeout(valueInCache.timeout);
+    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+    return Boolean(valueInCache);
+  }
+
+  get(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+  }
+
+  count() {
+    return this.cache.size;
+  }
+};
+
+// Approach 3: Maintain Expiration Times
+class TimeLimitedCache2 {
+  cache = {};
+
+  set(key, value, duration) {
+    const hasUnexpiredValue = key in this.cache && Date.now() < this.cache[key].expiration;
+    this.cache[key] = { value, expiration: Date.now() + duration };
+    return hasUnexpiredValue;
+  }
+
+  get(key) {
+    if (this.cache[key] === undefined) return -1;
+    if (Date.now() > this.cache[key].expiration) return -1;
+    return this.cache[key].value;
+  }
+
+  count() {
+    let count = 0;
+    for (const entry of Object.values(this.cache)) {
+        if (Date.now() < entry.expiration) {
+            count += 1;
+        }
+    }
+    return count;
+  }
+};
+//  Approach 4: Maintain Expiration Times + Priority Queue
+class TimeLimitedCache3 {
+  cache = {};
+  queue = new MinPriorityQueue();
+  size = 0;
+
+  handleExpiredData() {
+    const now = Date.now();
+    while (this.queue.size() > 0 && this.queue.front().priority < now) {
+      const entry = this.queue.dequeue().element;
+      if (!entry.overwritten) {
+        delete this.cache[entry.key];
+        this.size -= 1;
+      }
+    }
+  };
+
+  set(key, value, duration) {
+    this.handleExpiredData();
+    const hasVal = key in this.cache
+    if (hasVal) {
+      this.cache[key].overwritten = true
+    } else {
+      this.size += 1;
+    }
+    const expiration = Date.now() + duration;
+    const entry = { key, value, expiration, overwritten: false }
+    this.cache[key] = entry
+    this.queue.enqueue(entry, expiration);
+    return hasVal;
+  }
+
+  get(key) {
+    this.handleExpiredData();
+    if (this.cache[key] === undefined) return -1;
+    return this.cache[key].value;
+  }
+
+  count() {
+    this.handleExpiredData();
+    return this.size;
+  }
+};
