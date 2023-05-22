92c5ab1a567555b603d6677c4d69768b59351905
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-22 23:57:06 +0530

    Time: 85 ms (52.45%), Space: 75.8 MB (12.58%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -4,9 +4,9 @@
  */
 
 
-// Approach 1: Using JSON-like String Concatenation
+// Approach 1: Using JSON-like String Concatenation : 89
 
-var jsonStringify = function(object) {
+var jsonStringify1 = function(object) {
   if (object === null) {
     return 'null';
   }
@@ -27,4 +27,29 @@ var jsonStringify = function(object) {
   }
 
   return String(object);
+};
+
+//  Approach 2: Using Switch Case
+
+var jsonStringify = function(object) {
+  switch (typeof object) {
+    case 'object':
+      if (Array.isArray(object)) {
+        const elements = object.map((element) => jsonStringify(element));
+        return `[${elements.join(',')}]`;
+      } else if (object) {
+        const keys = Object.keys(object);
+        const keyValuePairs = keys.map((key) => `"${key}":${jsonStringify(object[key])}`);
+        return `{${keyValuePairs.join(',')}}`;
+      } else {
+        return 'null';
+      }
+    case 'boolean':
+    case 'number':
+      return `${object}`;
+    case 'string':
+      return `"${object}"`;
+    default:
+      return '';
+  }
 };
