1cb532b72ba7cdf1caca4c958b75645f5d95e85d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-22 23:15:46 +0530

    Time: 12 ms (91.14%), Space: 13.7 MB (68.96%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,138 @@
+class Solution {
+public:
+    // nlogn
+    vector<int> topKFrequent1(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        
+        for(auto& n:nums)
+            count[n]++;
+        function<bool(int,int)> cmp = [&](int a,int b){ if(count[a]<count[b])
+                                                  return true;
+                                                 return false; };
+        priority_queue<int,vector<int>, decltype(cmp)> q(cmp);
+        
+        for(auto& [n,c]:count)
+        {
+            q.push(n);
+        }
+        
+        vector<int> result;
+        while(k--)
+        {
+            result.push_back(q.top());
+            q.pop();
+        }
+        
+        return result;
+        
+    }
+    // nlogk: 16ms
+    vector<int> topKFrequent2(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for(auto& n:nums)
+            count[n]++;
+        function<bool(int,int)> cmp = [&](int a,int b){ if(count[a]>count[b])
+                                                  return true;
+                                                 return false; };
+        priority_queue<int,vector<int>, decltype(cmp)> q(cmp);
+        
+        for(auto& [n,c]:count)
+        {
+            q.push(n);
+            if(q.size()>k)
+                q.pop();
+        }
+        
+        vector<int> result;
+        while(!q.empty())
+        {
+            result.push_back(q.top());
+            q.pop();
+        }
+        
+        return result;
+    }
+    // O(n) : 12 ms , note : other solution is quickselect which is also O(n) but little complex to impl
+    vector<int> topKFrequent3(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for(auto& n:nums)
+            count[n]++;
+        
+        unordered_map<int,vector<int>> freq;
+        
+        for(auto& [n,c]:count)
+        {
+            freq[c].push_back(n);
+        }
+        vector<int> result;
+        for(int i=nums.size() ;i>0;--i)
+        {
+            if(freq.count(i))
+            {
+                result.insert(result.end(),begin(freq[i]),end(freq[i]));
+                if(result.size()>=k)
+                    break;
+            }
+        }
+        
+        return result;
+    }
+    
+    //quick-select: 20ms
+    vector<int> topKFrequent4(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(auto n:nums)
+            mp[n]++;
+        vector<int> t;
+        for(auto it:mp)
+            t.push_back(it.first);
+        if(k==t.size())
+            return t;
+        int l=0,r=t.size()-1;
+        while(true)
+        {
+            int p = partition(t,l,r,mp);
+            if(p==k-1)          break;
+            else if(p>k-1)      r = p-1;
+            else                l = p+1;
+        }
+        t.resize(k);
+        return t;
+        
+    }
+    
+    int partition(vector<int>& v,int l, int r,unordered_map<int,int>& mp)
+    {
+        int pivot = l;
+        ++l;
+        while(l<=r)
+        {
+            if(mp[v[l]]< mp[v[pivot]] && mp[v[r]] > mp[v[pivot]])
+                swap(v[l++],v[r--]);
+            else if(mp[v[l]] >= mp[v[pivot]])     ++l;
+            else if(mp[v[r]] <= mp[v[pivot]])     --r;
+        }
+        swap(v[r],v[pivot]);
+        return r;
+    }
+    
+    // quick-select from stl :
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> freq;
+        std::vector<int> ret;
+        ret.reserve(nums.size());
+        for (auto n : nums) {
+          if (1 == ++freq[n]) {
+            // Count the frequency for each int, storing each new int as we go
+            ret.push_back(n);
+          }
+        }
+
+        std::nth_element(ret.begin(), ret.begin() + k - 1, ret.end(), [&freq] (int l, int r) -> bool {
+            return freq[l] > freq[r];
+          });
+        ret.resize(k);
+        return ret;
+      }
+};
+
