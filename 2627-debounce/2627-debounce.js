7e2f3f8214b8a6ae52e5d22dbbfbe7f9e9cef3a1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-22 23:24:06 +0530

    Time: 71 ms (10.40%), Space: 41.6 MB (90.29%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,36 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+// Approach 1: setTimeout + clearTimeout
+var debounce = function(fn, t) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, t);
+  };
+};
+
+// Approach 2: setInterval + clearInterval
+var debounce1 = function(fn, t) {
+  let interval;
+  return function(...args) {
+    const lastCall = Date.now()
+    clearInterval(interval);
+    interval = setInterval(() => {
+      if (Date.now() - lastCall >= t) {
+        fn(...args);
+        clearInterval(interval);
+      }
+    }, 1);
+  }
+};
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
