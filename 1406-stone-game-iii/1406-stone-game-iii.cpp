d40ce23a5865459b6f65adbb88bdb38e2710d710
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-27 15:03:07 +0530

    Time: 290 ms (38.63%), Space: 132.2 MB (81.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& sv) {
-        //int sum = 0;//accumulate(begin(sv),end(sv),0);
-       
+    // self
+    string stoneGameIII1(vector<int>& sv) {
         int n = sv.size();
         vector<int> sum(n+1,0);
         vector<int> dp(n+1,INT_MIN);
         dp[n]=0;
         for(int i=n-1;i>=0;--i){
             sum[i] = sv[i] + sum[i+1];
-            //if(i+3 >= n) dp[i] = sum[i];
-            //else {
                 int ss=0;
                 for(int j=i;j<min(i+3,n) ;++j){
                     ss += sv[j];
-                    //int pre = 
+
                     dp[i] = max(dp[i],ss+ sum[j+1] - dp[j+1]);
-                    //cout<<j<<dp[i]<<" "<<ss<<endl;
                 }
-            //}
-            //cout<<dp[i]<<" ";
+
         }
-        //cout<<"\n"<<sum[0]<<" "<<dp[0]<<endl;
         if(sum[0] == 2*dp[0]) return "Tie";
         return sum[0] > 2*dp[0] ? "Bob":"Alice";
     }
+    // official solution
+    // Note here dp is difference between the score
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n + 1);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = stoneValue[i] - dp[i + 1];
+            if (i + 2 <= n) {
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
+            }
+            if (i + 3 <= n) {
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
+            }
+        }
+        if (dp[0] > 0) {
+            return "Alice";
+        }
+        if (dp[0] < 0) {
+            return "Bob";
+        }
+        return "Tie";
+    }
+
 };
