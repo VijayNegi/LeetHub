52d058a40c377dc143dc306a00626624ef986286
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-27 15:41:28 +0530

    Time: 56 ms (62.85%), Space: 42.1 MB (25.98%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,8 +1,14 @@
-Array.prototype.last = function() {
-    if(this.length == 0) return -1
+Array.prototype.last1 = function() {
+    if(this.length === 0) return -1
     return this[this.length-1]
 };
-
+// Implementation 3: Nullish Coalescing Operator
+Array.prototype.last2 = function() {
+  return this[this.length - 1] ?? -1;
+}
+ Array.prototype.last = function() {
+  return this.at(-1) ?? -1;
+}
 /**
  * const arr = [1, 2, 3];
  * arr.last(); // 3
