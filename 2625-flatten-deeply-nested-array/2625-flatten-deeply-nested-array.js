34a5044c88fbd88c53b4b374f72743f753634458
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-27 13:47:28 +0530

    Time: 212 ms (33.06%), Space: 101.1 MB (19.43%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,39 @@
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat = function (arr, n) {
+    if (n === 0) return arr
+    let res = [];
+    for (const ele of arr) {
+        if (Array.isArray(ele)) {
+            res.push(...flat(ele, n-1))
+        }
+        else {
+            res.push(ele)
+        }
+    }
+    return res
+};
+
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat1 = function (arr, n) {
+    let res = [];
+    const flattening = (nums, l) => {
+      for (const num of nums) {
+        if (Array.isArray(num) && l > 0) {
+          flattening(num, l - 1);
+        } else {
+          res.push(num);
+        }
+      }
+    }
+
+    flattening(arr, n);
+    return res;
+};
