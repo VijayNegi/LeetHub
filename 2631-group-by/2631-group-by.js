eecb8defc261b58c80f0c8b3ba7d6406534938a9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-28 18:13:33 +0530

    Time: 138 ms (83.10%), Space: 61.3 MB (67.73%) - LeetHub

--- 2631-group-by/2631-group-by.js
+++ 2631-group-by/2631-group-by.js
@@ -2,6 +2,7 @@
  * @param {Function} fn
  * @return {Array}
  */
+// self
 Array.prototype.groupBy = function(fn) {
     let arr = {}
     for(const ele of this){
@@ -11,6 +12,19 @@ Array.prototype.groupBy = function(fn) {
         arr[k].push(ele)
     }
     return arr
+};
+
+Array.prototype.groupBy = function(fn) {
+  const returnObject = {};
+  for (const item of this) {
+    const key = fn(item);
+    if (key in returnObject) {
+      returnObject[key].push(item);
+    } else {
+      returnObject[key] = [item];
+    }
+  }
+  return returnObject;
 };
 
 /**
