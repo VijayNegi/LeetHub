49698e58b73d6bf9a22846ff79346325e72c67e1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-02 10:31:27 +0530

    Time: 62 ms (31.29%), Space: 42.4 MB (20.57%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -13,6 +13,27 @@ var fibGenerator = function*() {
     }
 };
 
+// Destructuring assignment
+var fibGenerator = function*() {
+    let a = 0;
+    let b = 1;
+
+    while (true){
+        yield a;
+        [a,b] = [b, a+b]; 
+    }
+};
+// recursive generator
+var fibGenerator = function*(a = 0, b=1) {
+    // yield the first number
+    yield a;
+
+    // recursively call fibGenerator and yield the entire generator function
+    yield* fibGenerator(b, a+b);
+};
+
+
+
 /**
  * const gen = fibGenerator();
  * gen.next().value; // 0
