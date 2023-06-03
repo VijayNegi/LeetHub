57ce2825efe7f0df7221f9285bd4cd0705c4349f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-03 18:37:17 +0530

    Time: 173 ms (77.42%), Space: 74.4 MB (48.27%) - LeetHub

--- 2649-nested-array-generator/2649-nested-array-generator.js
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -2,13 +2,29 @@
  * @param {Array} arr
  * @return {Generator}
  */
+// self
 var inorderTraversal = function*(arr) {
     let arr1 = arr.flat(99999999);
     for(ele of arr1)
         yield ele
     return
 };
+// flat method
+var inorderTraversal = function*(arr) {
+  yield* arr.flat(Infinity);
+};
 
+// Recursive Solution
+var inorderTraversal = function* (arr) {
+    if(!Array.isArray(arr)) {
+        yield arr
+        return
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        yield* inorderTraversal(arr[i])
+    }
+};
 /**
  * const gen = inorderTraversal([1, [2, 3]]);
  * gen.next().value; // 1
