db6ee701a00d9606c55a9e2944251e3a52d1e580
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-06 23:24:52 +0530

    Time: 4 ms (74.54%), Space: 8.9 MB (92.44%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,10 +1,48 @@
 class Solution {
 public:
-    bool canMakeArithmeticProgression(vector<int>& arr) {
+    bool canMakeArithmeticProgression1(vector<int>& arr) {
         sort(begin(arr),end(arr));
         int d = arr[1]-arr[0];
-        for(int i=1;i<arr.size();++i)
+        for(int i=2;i<arr.size();++i)
             if(arr[i]-arr[i-1]!=d) return false;
+        return true;
+    }
+    // O(n) solution
+    bool canMakeArithmeticProgression(std::vector<int>& arr) {
+        int minValue = *std::min_element(arr.begin(), arr.end());
+        int maxValue = *std::max_element(arr.begin(), arr.end());
+        int n = arr.size();
+        
+        if ((maxValue - minValue) % (n - 1) != 0) {
+            return false;
+        }
+        
+        int diff = (maxValue - minValue) / (n - 1);
+        int i = 0;
+        
+        while (i < n) {
+            // If arr[i] is at the correct index, move on.
+            if (arr[i] == minValue + i * diff) {
+                i += 1;
+                
+            // If arr[i] doesn't belong to this arithmetic sequence, return false.
+            } else if ((arr[i] - minValue) % diff != 0) {
+                return false;
+                
+            // Otherwise, find the index j to which arr[i] belongs, swap arr[j] with arr[i].
+            } else {
+                int j = (arr[i] - minValue) / diff;
+                
+                // If we find duplicated elements, return False.
+                if (arr[i] == arr[j]) {
+                    return false;
+                }
+                
+                // Swap arr[i] with arr[j].
+                swap(arr[i], arr[j]);
+            }
+        }
+        
         return true;
     }
 };
