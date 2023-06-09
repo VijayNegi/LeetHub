f1b56fd2b98c806f785764752fa5973803ca25d7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-09 13:09:45 +0530

    Time: 26 ms (5.25%), Space: 15.9 MB (32.10%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        auto it = upper_bound(letters.begin(),letters.end(),target);
-        if(it != letters.end())
-            return *it;
-        return letters[0];
+        auto it = upper_bound(begin(letters),end(letters),target);
+        if(it == letters.end())
+            return letters[0];
+        return *it;
     }
 };
