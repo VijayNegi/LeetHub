a9846cd4a9238e8d6411ea2f6a361f8d513cc2e0
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-18 17:41:42 +0530

    Time: 264 ms (93.52%), Space: 123.7 MB (89.38%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -64,7 +64,7 @@ public:
             d2[l] = depth[n->val];
         return depth[n->val];
     };
-    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+    vector<int> treeQueries2(TreeNode* root, vector<int>& queries) {
         traverse(root, 0);
         transform(begin(queries), end(queries), begin(queries), [&](int q){
             int l = level[q];
@@ -72,4 +72,32 @@ public:
         });
         return queries;
     }
+    // Lee Solution - preOrder and postOrder
+    // https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/discuss/2759353/C%2B%2BPython-Preoder-and-Postorder-DFS
+    int preh[100001], posth[100001], maxh;
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        maxh = 0; pre(root, 0);
+        maxh = 0; post(root, 0);
+        vector<int> res;
+        for (int q : queries)
+            res.push_back(max(preh[q], posth[q]));
+        return res;
+    }
+
+    void pre(TreeNode* root, int h) {
+        if (!root) return;
+        preh[root->val] = maxh;
+        maxh = max(maxh, h);
+        pre(root->left, h + 1);
+        pre(root->right, h + 1);
+    }
+
+    void post(TreeNode* root, int h) {
+        if (!root) return;
+        posth[root->val] = maxh;
+        maxh = max(maxh, h);
+        post(root->right, h + 1);
+        post(root->left, h + 1);
+    }
+
 };
