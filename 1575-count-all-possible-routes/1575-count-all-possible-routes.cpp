e98b05b2eb582e7da2795d6be004a8d18415dde1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-25 20:53:08 +0530

    Time: 734 ms (9.95%), Space: 11.9 MB (27.62%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,7 +1,8 @@
 const int mod = 1e9+7;
 class Solution {
 public:
-    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+    // top down
+    int countRoutes1(vector<int>& locations, int start, int finish, int fuel) {
         int n = locations.size();
         vector<vector<int>> dp(n,vector(fuel+1,-1));
         function<int (int,int)> dfs = [&](int k,int f){
@@ -21,5 +22,28 @@ public:
             return dp[k][f] = res;
         };
         return dfs(start,fuel);
+    }
+    // iterative
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        int n = locations.size();
+        vector<vector<int>> dp(n, vector<int>(fuel + 1));
+        for(int i = 0; i<=fuel; i++) {
+            dp[finish][i] = 1;
+        }
+
+        for (int j = 0; j <= fuel; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int k = 0; k < n; k++) {
+                    if (k == i) {
+                        continue;
+                    }
+                    if (abs(locations[i] - locations[k]) <= j) {
+                        dp[i][j] = (dp[i][j] + dp[k][j - abs(locations[i] - locations[k])]) %mod;
+                    }
+                }
+            }
+        }
+
+        return dp[start][fuel];
     }
 };
