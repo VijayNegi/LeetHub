3eead06d11ad0d696e77a440be2834b7f0029c45
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-06-25 23:09:17 +0530

    Time: 40 ms (61.95%), Space: 28.4 MB (69.32%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.cpp
+++ 1214-two-sum-bsts/1214-two-sum-bsts.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    bool twoSumBSTs(TreeNode* root1, TreeNode* root2, int target) {
+    // binary search
+    bool twoSumBSTs1(TreeNode* root1, TreeNode* root2, int target) {
         if(!root1 || !root2) return false;
         function<bool(TreeNode*,int)> find = [&](TreeNode* node,int t){
             if(!node) return false;
@@ -22,5 +23,37 @@ public:
         if(find(root2,target - root1->val))
             return true;
         return twoSumBSTs(root1->left,root2,target) or twoSumBSTs(root1->right,root2,target);
+    }
+    // stack
+    bool twoSumBSTs(TreeNode* root1, TreeNode* root2, int target) {
+        if (!root1 || !root2) {
+            return false;
+        }
+        stack<TreeNode*> s1, s2;
+        s1.push(root1);
+        s2.push(root2);
+        root1 = root1->left;
+        root2 = root2->right;
+        while (!s1.empty() && !s2.empty() || root1 || root2) {
+            while (root1) {
+                s1.push(root1);
+                root1 = root1->left;
+            }
+            while (root2) {
+                s2.push(root2);
+                root2 = root2->right;
+            }          
+            int sum = s1.top()->val + s2.top()->val;
+            if (sum == target) {
+                return true;
+            } else if (sum > target) {
+                root2 = s2.top()->left;
+                s2.pop();
+            } else {  // sum < target
+                root1 = s1.top()->right;
+                s1.pop();
+            }
+        }
+        return false;
     }
 };
