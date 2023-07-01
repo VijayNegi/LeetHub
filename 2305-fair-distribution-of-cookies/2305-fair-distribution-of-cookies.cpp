2d599417bccb9da26888cb1542d34f5f390bd68d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-01 16:23:04 +0530

    Time: 909 ms (66.67%), Space: 6.8 MB (62.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // optimization
+    // https://leetcode.com/problems/fair-distribution-of-cookies/discuss/2140918/Easy-C++-or-Backtracking/1434765
     int distributeCookies(vector<int>& cookies, int k) {
         vector<int> child(k,0);
         int n = cookies.size();
@@ -11,11 +13,11 @@ public:
                 return;
             }
             for(int j=0;j<k;++j){
-                if(child[j] + cookies[i] > result) continue;
+                if(child[j] + cookies[i] > result) continue; // if it is already worse then skip
                 child[j] += cookies[i];
                 dfs(i+1);
                 child[j] -= cookies[i];
-                if(child[j]==0) break; // optimization : all zero cookie cases will be considered only once. (5,0) or (0,5) are same
+                //if(child[j]==0) break; // optimization : all zero cookie cases will be considered only once. (5,0) or (0,5) are same
             }
         };
         dfs(0);
