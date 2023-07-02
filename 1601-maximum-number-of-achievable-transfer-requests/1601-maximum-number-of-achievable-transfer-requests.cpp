97f84b3fca465a7a0f2350e957041ec05c5f64db
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-02 11:44:06 +0530

    Time: 144 ms (51.96%), Space: 8.6 MB (100.00%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int maximumRequests(int n, vector<vector<int>>& requests) {
+    // https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/discuss/866456/Python-Check-All-Combinations/712116
+    // 1331 ms
+    int maximumRequests1(int n, vector<vector<int>>& requests) {
         int nr = requests.size();
         int ans = 0;
         for (int mask = 0; mask < (1 << nr); mask++){
@@ -18,5 +20,17 @@ public:
             }
         }
         return ans;
+    }
+    // https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/discuss/868403/C%2B%2BPython-knapsack-01
+    int bal[21] = {};
+    int maximumRequests(int n, vector<vector<int>>& requests, int i = 0) {
+        if (i == requests.size())
+            return count(begin(bal), begin(bal) + n, 0) == n ? 0 : INT_MIN;
+        --bal[requests[i][0]];
+        ++bal[requests[i][1]];
+        auto take = 1 + maximumRequests(n, requests, i + 1);
+        ++bal[requests[i][0]];
+        --bal[requests[i][1]];
+        return max(take, maximumRequests(n, requests, i + 1));
     }
 };
