c6c987c66f9ae4e3e1177102f1ca9be33dfdf4c1
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-07 23:34:11 +0530

    Time: 76 ms (20.67%), Space: 10 MB (59.24%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string answerKey, int k) {
+    int maxConsecutiveAnswers1(string answerKey, int k) {
         int start=0;
         int n = answerKey.size();
         int fill=0;
@@ -27,5 +27,23 @@ public:
             result = max(result,i-start+1);
         }
         return result;
+    }
+    // single loop
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int maxSize = 0;
+        unordered_map<char, int> count;
+        
+        for (int right = 0; right < answerKey.length(); right++) {
+            count[answerKey[right]]++;
+            int minor = min(count['T'], count['F']);
+            
+            if (minor <= k) {
+                maxSize++;
+            } else {
+                count[answerKey[right - maxSize]]--;
+            }
+        }
+
+        return maxSize;
     }
 };
