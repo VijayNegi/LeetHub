524c3fec6903d5b0b1d8625acc71cee67fcb186e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-08 00:12:21 +0530

    Time: 2610 ms (5.07%), Space: 150 MB (8.48%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     // self
-    int numBusesToDestination1(vector<vector<int>>& routes, int source, int target) {
+    // 1194 ms
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         if(source == target)
             return 0;
         int n = routes.size();
+        unordered_map<int,unordered_set<int>> bus2bus;
         unordered_map<int,unordered_set<int>> stop2bus;
+        
         
         for(int i=0;i<n;++i){
             for(auto& s:routes[i])
                 stop2bus[s].insert(i);
+        }
+        for(auto& it:stop2bus){
+            auto& connected = it.second;
+            for(auto& b:connected){
+                bus2bus[b].insert(connected.begin(),connected.end());
+                bus2bus[b].erase(b);
+            }
         }
         vector<bool> visited_bus(n,false);
         unordered_set<int> starting(stop2bus[source]);
@@ -22,19 +32,20 @@ public:
             for(auto& b:starting){
                 if(destination.count(b))
                     return path;
-                for(auto& stop: routes[b])
-                    for(auto& bus: stop2bus[stop]){
-                        if( visited_bus[bus] ) continue;
-                        temp.insert(bus);
-                        visited_bus[bus] = true;
-                    }
+                for(auto& bus: bus2bus[b]){
+                    if( visited_bus[bus] ) continue;
+                    temp.insert(bus);
+                    visited_bus[bus] = true;
+                }
             }
             swap(temp,starting);
             ++path;
         }
         return -1;
     }
-    int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
+    // 263 ms
+    //https://leetcode.com/problems/bus-routes/discuss/122771/C%2B%2BJavaPython-BFS-Solution
+    int numBusesToDestination2(vector<vector<int>>& routes, int S, int T) {
         unordered_map<int, vector<int>> to_routes;
         for (int i = 0; i < routes.size(); ++i)
             for (int j : routes[i])
