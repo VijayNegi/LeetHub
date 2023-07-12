6fdd33d66dd33e531e084f77c6814090332d742c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-13 00:14:17 +0530

    Time: 223 ms (43.76%), Space: 62.3 MB (9.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    // self : 399 ms
+    vector<int> eventualSafeNodes1(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<vector<int>> incoming(n);
         for(int i=0;i<n;++i){
@@ -13,7 +14,6 @@ public:
             outgoing[i].insert(begin(graph[i]),end(graph[i]));
             //outgoing[i].erase(i);
         }
-        //vector<bool> safe(n,false);
         vector<int> safe;
         for(int i=0;i<n;++i){
             if(outgoing[i].empty())
@@ -24,7 +24,6 @@ public:
             result.insert(safe.begin(),safe.end());
             vector<int> temp;
             for(auto& i:safe){
-                //cout<<i<<endl;
                 for(auto& d:incoming[i]){
                     outgoing[d].erase(i);
                     if(outgoing[d].empty())
@@ -36,5 +35,30 @@ public:
         }
         
         return {result.begin(),result.end()};
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& G) {
+        int N = G.size();
+        vector<vector<int>> R(N);
+        vector<int> outdegree(N), safe(N), ans;
+        queue<int> q;
+        for (int i = 0; i < N; ++i) {
+            for (int v : G[i]) {
+                R[v].push_back(i);
+            }
+            outdegree[i] = G[i].size();
+            if (outdegree[i] == 0) q.push(i);
+        }
+        while (q.size()) {
+            int u = q.front();
+            q.pop();
+            safe[u] = 1;
+            for (int v : R[u]) {
+                if (--outdegree[v] == 0) q.push(v);
+            }
+        }
+        for (int i = 0; i < N; ++i) {
+            if (safe[i]) ans.push_back(i);
+        }
+        return ans;
     }
 };
