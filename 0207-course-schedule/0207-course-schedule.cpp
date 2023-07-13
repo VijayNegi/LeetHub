03bb0664818a9ebc60e6adced4dd3c4d49f48b8f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-13 21:38:51 +0530

    Update 0207-course-schedule.cpp

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    // self 23ms
+    bool canFinish2(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
+        vector<int> inDegree(n,0);
+        for(auto& p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            inDegree[p[0]]++;
+        }
+        vector<int> done;
+        int result=0;
+        for(int i=0;i<n;++i){
+            if(inDegree[i]==0){
+                done.push_back(i);
+            }
+        }
+        while(done.size()){
+            vector<int> next;
+            for(auto& course:done){
+                for(auto& nextcourse:adj[course]){
+                    --inDegree[nextcourse];
+                    if(inDegree[nextcourse]==0)
+                        next.push_back(nextcourse);
+                }
+                adj[course].clear();
+            }
+            result += done.size();
+            swap(next,done);
+        }
+        return result == n;
+     }
     // topological sort : 30 ms
     bool canFinish1(int n, vector<vector<int>>& prereq) {
 
