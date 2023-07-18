cba7b89449a5fa53f48e3f337bcb362212f76618
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-18 10:19:12 +0530

    Time: 564 ms (13.21%), Space: 177.3 MB (38.73%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,3 +1,54 @@
+#if 0 
+class LRUCache {
+    int size;
+    unordered_map<int,list<pair<int,int>>::iterator> cache;
+    list<pair<int,int>> used;
+public:
+    LRUCache(int capacity) {
+        size = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.count(key))
+        {
+            auto it = cache[key];
+            int value = (*it).second;
+            used.erase(it);
+            used.push_front({key,value});
+            cache[key] = used.begin();
+            return value;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        // find key in map, yes, update it
+        // if not already in map, check size, 
+        // less then size put in cache
+        // size more then remove least recently use put.
+        if(cache.count(key))
+        {
+            used.erase(cache[key]);
+            used.push_front({key,value});
+            cache[key] = used.begin();
+            
+        }
+        else
+        {
+            if(used.size()==size)
+            {
+                auto k = used.back();
+                used.pop_back();
+                cache.erase(k.first);
+            }
+            
+            used.push_front({key,value});
+            cache[key] = used.begin();
+        }
+    }
+};
+
+#endif
 class LRUCache {
     map<int,int> time2key;
     unordered_map<int,pair<int,int>> key2timeval;
