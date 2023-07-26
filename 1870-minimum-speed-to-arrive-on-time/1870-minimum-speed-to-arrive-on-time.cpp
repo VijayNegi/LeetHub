fccb0063f4805bdf0ef10a830307c34354cc1447
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-27 00:50:49 +0530

    Time: 263 ms (96.11%), Space: 101.5 MB (26.33%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -6,14 +6,11 @@ public:
         if(hour <= n-1) return -1;
         int l=1,r =1e7;
         function<bool(int)> canReach = [&](int s){
-            //cout<<endl;
             double time=0;
             for(auto& k:dist){
                 time = ceil(time);
                 time += (double)k/s;
-                //cout<<time<<" ";
             }
-            //cout<<endl<<s<<" "<<time;
             return time <= hour;
         };
         
