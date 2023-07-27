5d770058b5c98322e95870a0c0a647b7b4762556
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-28 00:04:44 +0530

    Time: 106 ms (100.00%), Space: 55.8 MB (71.58%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // https://leetcode.com/problems/maximum-running-time-of-n-computers/discuss/1693347/Heap-vs.-Binary-Search
     // https://leetcode.com/problems/maximum-running-time-of-n-computers/discuss/1692939/JavaC%2B%2BPython-Sort-Solution-with-Explanation-O(mlogm)
-    long long maxRunTime(int n, vector<int>& bat) {
+    long long maxRunTime1(int n, vector<int>& bat) {
         long long sum = accumulate(begin(bat), end(bat), 0LL);
         priority_queue<int> pq(begin(bat), end(bat));
         while (pq.top() > sum / n) {
@@ -10,6 +10,20 @@ public:
             --n;
         }
         return sum / n;
+    }
+    // binary search
+    long long maxRunTime(int n, vector<int>& bat) {
+        long long l = 0, r = accumulate(begin(bat), end(bat), 0LL) / n;
+        while (l < r) {
+            long long m = (l + r + 1) / 2, minutes = 0;
+            for (int b : bat)
+                minutes += min((long long)b, m);
+            if (minutes >= n * m)
+                l = m;
+            else
+                r = m - 1;
+        }
+        return l;
     }
 };
 
