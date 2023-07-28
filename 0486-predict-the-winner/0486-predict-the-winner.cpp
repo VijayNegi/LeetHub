95f61970716f9430c01ed845780cc534959228d5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-28 23:32:47 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.26%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        int n = nums.size()-1;
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector(n,-1));
         function<int(int,int)> canWin = [&](int l,int r){
             if(l==r){
                 return nums[l];
             }
-            int diff = 0;
+            if(dp[l][r] != -1) return dp[l][r];
             int diff1 = nums[r] - canWin(l,r-1);
             int diff2 = nums[l] - canWin(l+1,r);
-            diff = max(diff1,diff2);
-            return diff;
+            return dp[l][r] = max(diff1,diff2);;
         };
-        return canWin(0,n)>=0;
+        return canWin(0,n-1)>=0;
     }
 };
