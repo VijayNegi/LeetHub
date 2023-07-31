87e68d4dd03d5b0fa861bd1a037c17938a5f7dd4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-31 16:20:20 +0530

    Time: 180 ms (5.11%), Space: 15.6 MB (5.04%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        int result = numeric_limits<int>::max();
+        vector<vector<int>> dp(n1+1,vector(n2+1,-1));
+        function<int(int,int)> dfs = [&](int i,int j){
+            if(i==n1 && j==n2)
+                return 0;
+            if(dp[i][j] != -1) return dp[i][j];
+            dp[i][j] = numeric_limits<int>::max();
+            if(i<n1)
+                dp[i][j] = dfs(i+1,j) + s1[i];
+             if(j<n2)
+                dp[i][j] = min(dp[i][j], dfs(i,j+1) + s2[j]);
+            if(i<n1 && j<n2){
+                if(s1[i] == s2[j])
+                    dp[i][j] = min(dp[i][j],dfs(i+1,j+1));
+                else
+                    dp[i][j] = min(dp[i][j],dfs(i+1,j+1) + s1[i] + s2[j]);
+            }
+            
+            return dp[i][j];
+        };
+        return dfs(0,0);
+    }
+};
