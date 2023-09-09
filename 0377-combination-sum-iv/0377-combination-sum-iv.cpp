f7fcbbd367129442fc7310012bb78c295261fa73
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-09-09 22:55:42 +0530

    Time: 3 ms (50.37%), Space: 6.6 MB (85.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<uint32_t> dp(target+1,0); // note the unsigned int
+        dp[0]=1;
+        for(int i=1;i<=target;++i)
+            for(auto& k:nums) {
+                if(k<=i)
+                    dp[i] += dp[i-k];
+            }
+        return dp[target];
+    }
+};
