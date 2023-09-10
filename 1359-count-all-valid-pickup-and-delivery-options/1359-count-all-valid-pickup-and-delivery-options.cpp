c4d4c63abfe676a3e83ef3fe073f24095bf9b5b3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-09-11 00:44:59 +0530

    Update 1359-count-all-valid-pickup-and-delivery-options.cpp

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,6 +1,26 @@
 const int mod = 1e9+7;
 class Solution {
 public:
+    // self
+    int countOrders0(int n) {
+        vector<vector<int>> dp(n+1,vector(n+1,-1));
+        function<int(int,int)> ways = [&](int p, int d){
+            if(p==0 && d==0)
+                return 1;
+            if(dp[p][d] != -1) return dp[p][d];
+            long val = 0;
+            if(p>0){
+                val = p*1l * ways(p-1,d+1);
+                val %=mod;
+            }
+            if(d >0){
+                val += d *1l* ways(p,d-1);
+                val %=mod;
+            }
+            return dp[p][d] = val;
+        };
+        return ways(n,0);
+    }
     int countOrders1(int n) {
         long result=1;
         
@@ -19,6 +39,16 @@ public:
         return result;
     }
     // https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/516951/C%2B%2BJavaPython-Simple-Math-Formula-with-Explanation-Clean-and-Concise
+    int countOrders2(int n) {
+        long long MODULO = 1e9 + 7, ans = 1;
+        for (int i = 2; i <= n; i++) {
+            int spaceNum = (i-1)*2 + 1;
+            ans *= spaceNum * (spaceNum + 1) / 2;
+            ans %= MODULO;
+        }
+        return (int) ans;
+    }
+    // same post
     int countOrders(int n) {
         long long MODULO = 1e9 + 7, ans = 1;
         for (int i = 2; i <= n; i++) {
@@ -28,5 +58,4 @@ public:
         }
         return (int) ans;
     }
-    
 };
