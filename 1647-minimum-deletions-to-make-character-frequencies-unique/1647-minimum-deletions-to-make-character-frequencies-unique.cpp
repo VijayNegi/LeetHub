397dd12172ecad8a66f578285f57e3702f8fd391
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-09-12 23:21:44 +0530

    Update 1647-minimum-deletions-to-make-character-frequencies-unique.cpp

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    // self
-    int minDeletions1(string s) {
+    int minDeletions(string s) {
+        unordered_map<int,int> seen;
         vector<int> freq(26,0);
-        for(auto& c:s) {
+        for(auto& c:s){
             freq[c-'a']++;
         }
-        sort(freq.begin(),freq.end());
-        unordered_set<int> ss;
         int result = 0;
-        for(int i=0;i<26;++i) {
-            if(freq[i]==0)
-                continue;
-            if(ss.count(freq[i])==0) {
-                ss.insert(freq[i]);
-            }
-            else {
-                int org = freq[i];
-                while(org>0 && ss.count(org)==1)
-                    org--;
-                ss.insert(org);
-                result += freq[i]-org;
+        for(auto& f : freq){
+            if(f){
+                while(seen.count(f)){
+                    --f;
+                    ++result;
+                }
+                if(f>0) seen[f] = 1;
             }
         }
         return result;
-    }
-    // cleaner
-    int minDeletions(string s) {
-        vector<int> freq(26, 0);
-        for (char c : s) {
-            freq[c - 'a']++;
-        }
-        int count = 0;
-        unordered_set<int> seenFrequencies;
-        for (int i = 0; i < 26; i++) {
-            while (freq[i] && seenFrequencies.find(freq[i]) != seenFrequencies.end()) {
-                freq[i]--;
-                count++;
-            }
-            seenFrequencies.insert(freq[i]);
-        }
-        return count;
     }
 };
