bd11df12d1964a347467977f97c439bba40e5dcf
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-09-17 22:57:21 +0530

    Time: 47 ms (57.64%), Space: 17.3 MB (51.19%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,48 @@
+using pi = pair<int,int>;
+class Solution {
+public:
+    // basically smart bfs : what smart ?? visit state i.e what nodes are visited prior to visiting this node(to avoid loop).
+    int shortestPathLength(vector<vector<int>>& graph) {
+        unordered_set<int> visited;    // node, state
+        queue<pi> que;
+        int n = graph.size();
+        for(int i=0;i<n;++i)
+        {
+            pi p{i,1<<i};
+            que.push(p);
+            int hash = (1<<(i+12)) | p.second ; 
+            visited.insert(hash);
+        }
+        int cost = 0;
+        int endState = (1<< n) - 1;
+        int count = que.size();
+        while(que.size())
+        {
+            if(count == 0 )
+            {
+                count = que.size();
+                ++cost;
+            }
+            
+            pi curr = que.front();
+            que.pop();
+            --count;
+            if(curr.second == endState)
+                return cost;
+            
+            for(auto nei: graph[curr.first])
+            {
+                int state = curr.second | (1 << nei);
+                int hash = (1<<(nei+12)) | state; 
+                pi p{nei, state };
+               
+                if(visited.insert(hash).second)
+                {
+                    que.push(p);
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
