6a25c2a75614adef1afedaa6c36c7e5054593236
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-07 00:54:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int integerBreak(int n) {
+    int integerBreak1(int n) {
+        if (n <= 3) 
+            return n - 1;
         int result=0;
         vector<int> dp(60,-1);
         function<int(int)> dfs = [&](int num){
-            if(num<=2) return 1;
-            //if(num==3) return 2;
+            if(num<=3) return num;
             if(dp[num]!= -1) return dp[num];
-            for(int i=2; i<=(num+1)/2;++i){
+            for(int i=2; i<=num/2;++i){
                 dp[num] = max(i*max(num-i,dfs(num-i)),dp[num]);
             }
-            //dp[num] = max(dp[num],num);
             return dp[num];
         };
         return dfs(n);
+    }
+    // maths solution: its optimal to break in 2's and 3's
+    int integerBreak(int n) {
+        if (n <= 3) 
+            return n - 1;
+        
+        int ans = 1;
+        while (n > 4) {
+            ans *= 3;
+            n -= 3;
+        }
+        
+        return ans * n;
     }
 };
