f5e4dcb275506d5c3ad36758325f2f3871625b21
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-14 18:38:31 +0530

    Time: 267 ms (19.35%), Space: 116.1 MB (24.65%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    
-    int n;
-    
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
         vector<vector<int>> memo(n, vector(n + 1, -1));
         function<int(int, int)> dp = [&](int i,int remain)-> int{
-            if (remain <= 0) {
-                return 0;
-            }
-
-            if (i == n) {
-                return 1e9;
-            }
-
-            if (memo[i][remain] != -1) {
+            if (remain <= 0) return 0;
+            if (i == n) return 1e9;
+        
+            if (memo[i][remain] != -1) 
                 return memo[i][remain];
-            }
-
-
+            
             int paint = cost[i] + dp(i + 1, remain - 1 - time[i]);
             int dontPaint = dp(i + 1, remain);
             memo[i][remain] = min(paint, dontPaint);
@@ -27,6 +17,4 @@ public:
         };
         return dp(0, n);
     }
-    
-   
 };
