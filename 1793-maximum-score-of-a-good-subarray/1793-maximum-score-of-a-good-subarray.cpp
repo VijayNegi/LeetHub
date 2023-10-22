9035908122a104380d66592030e27ba80b041f32
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-22 21:07:40 +0530

    Time: 115 ms (96.05%), Space: 89.7 MB (80.92%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int maximumScore(vector<int>& nums, int k) {
+    // self
+    int maximumScore1(vector<int>& nums, int k) {
         int score = nums[k];
         int n = nums.size();
         int l=k;
@@ -40,5 +41,27 @@ public:
             else break;
         }
         return score;
+    }
+    // same but concise
+        int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = k;
+        int right = k;
+        int ans = nums[k];
+        int currMin = nums[k];
+        
+        while (left > 0 || right < n - 1) {
+            if ((left > 0 ? nums[left - 1]: 0) < (right < n - 1 ? nums[right + 1] : 0)) {
+                right++;
+                currMin = min(currMin, nums[right]);
+            } else {
+                left--;
+                currMin = min(currMin, nums[left]);
+            }
+            
+            ans = max(ans, currMin * (right - left + 1));
+        }
+        
+        return ans;
     }
 };
