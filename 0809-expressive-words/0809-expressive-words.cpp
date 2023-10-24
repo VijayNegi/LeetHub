7f197bc30a4bab2f3402aac60dd87e8b189ecaad
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-24 16:08:22 +0530

    Time: 7 ms (32.66%), Space: 8.3 MB (46.23%) - LeetHub

--- 0809-expressive-words/0809-expressive-words.cpp
+++ 0809-expressive-words/0809-expressive-words.cpp
@@ -6,7 +6,7 @@ public:
         return res;
     }
 
-    bool check(string S, string W) {
+    bool check1(string S, string W) {
         int n = S.size(), m = W.size(), j = 0;
         for (int i = 0; i < n; i++)
             if (j < m && S[i] == W[j]) j++;
@@ -14,5 +14,15 @@ public:
             else if (0 < i && i < n - 1 && S[i - 1] == S[i] && S[i] == S[i + 1]);
             else return false;
         return j == m;
+    }
+    bool check(string S, string W) {
+        int n = S.size(), m = W.size(), i = 0, j = 0;
+        for (int i2 = 0, j2 = 0; i < n && j < m; i = i2, j = j2) {
+            if (S[i] != W[j]) return false;
+            while (i2 < n && S[i2] == S[i]) i2++;
+            while (j2 < m && W[j2] == W[j]) j2++;
+            if (i2 - i != j2 - j && i2 - i < max(3, j2 - j)) return false;
+        }
+        return i == n && j == m;
     }
 };
