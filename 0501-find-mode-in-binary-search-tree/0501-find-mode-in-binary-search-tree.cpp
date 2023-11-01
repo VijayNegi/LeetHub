3fdb4b8a22cf07e528501c6f6f6fb79359c48a8e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-01 22:46:36 +0530

    Time: 11 ms (90.70%), Space: 24.4 MB (82.92%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    vector<int> findMode(TreeNode* root) {
+    vector<int> findMode1(TreeNode* root) {
         unordered_map<int,int> freq;
         int max_count=0;
         vector<int> result;
@@ -31,5 +31,43 @@ public:
         };
         dfs(root);
         return result;
+    }
+    // without hash table, inorder traversal
+    vector<int> ans;
+    int maxStreak = 0;
+    int currStreak = 0;
+    int currNum = 0;
+    
+    vector<int> findMode(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    
+    void dfs(TreeNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        dfs(node->left);
+        
+        int num = node->val;
+        if (num == currNum) {
+            currStreak++;
+        } else {
+            currStreak = 1;
+            currNum = num;
+        }
+
+        if (currStreak > maxStreak) {
+            ans = {};
+            maxStreak = currStreak;
+        }
+
+        if (currStreak == maxStreak) {
+            ans.push_back(num);
+        }
+
+        
+        dfs(node->right);
     }
 };
