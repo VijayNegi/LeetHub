df89de2f137b2b12090f9810b829a81b10ef4447
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-10 10:26:19 +0530

    Time: 570 ms (23.03%), Space: 151.1 MB (34.85%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjP) {
+        if(adjP.size()==1)
+            return adjP[0];
+        map<int,unordered_set<int>> links;
+        int k = 1;
+        for(const auto& v:adjP){
+            links[v[0]].insert(v[1]);
+            links[v[1]].insert(v[0]);
+        }
+        for(auto& [val,adjset]:links)
+            if(adjset.size()==1){
+                k = val;
+                break;
+            }
+        vector<int> result{k};
+        while(links[k].size()){
+            int kn = *links[k].begin();
+            links[kn].erase(k);
+            k = kn;
+            result.push_back(k);
+        }
+        return result;
+    }
+};
