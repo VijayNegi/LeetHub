2b6038a0b658295061fc3a84d3abd6849013b54d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-20 23:23:37 +0530

    Time: 154 ms (91.25%), Space: 102.4 MB (91.88%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+    int garbageCollection1(vector<string>& garbage, vector<int>& travel) {
         int n = travel.size();
         function<int(char)> collectionTime = [&](char c){
             int currentCost=0;
@@ -20,5 +20,21 @@ public:
         result += collectionTime('P');
         result += collectionTime('G');
         return result;
+    }
+    // other solution from commetns
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int len = garbage.size();
+        int res = 0;
+        unordered_set<char> s;
+        for (int i = len - 1; i >= 0 ; i--) {
+            for (char ch : garbage[i]) 
+                if (!s.count(ch)) 
+                    s.insert(ch);
+
+            res += garbage[i].length();
+            res += i > 0 ? s.size() * travel[i - 1] : 0;
+        }
+
+        return res;
     }
 };
