af79b3901f24e22935ef37ebab641edb0f52a9fc
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-20 23:27:23 +0530

    Time: 198 ms (13.35%), Space: 102.6 MB (19.78%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -22,7 +22,7 @@ public:
         return result;
     }
     // other solution from commetns
-    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+    int garbageCollection2(vector<string>& garbage, vector<int>& travel) {
         int len = garbage.size();
         int res = 0;
         unordered_set<char> s;
@@ -36,5 +36,30 @@ public:
         }
 
         return res;
+    }
+    // offical solution
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        // Store the prefix sum in travel itself.
+        for (int i = 1; i < travel.size(); i++) {
+            travel[i] = travel[i - 1] + travel[i];
+        }
+        
+        // Map to store garbage type to the last house index.
+        unordered_map<char, int> garbageLastPos;
+        int ans = 0;
+        for (int i = 0; i < garbage.size(); i++) {
+            for (char c : garbage[i]) {
+                garbageLastPos[c] = i;
+            }
+            ans +=  garbage[i].size();
+        }
+        
+        string garbageTypes = "MPG";
+        for (char c : garbageTypes) {
+            // No travel time is required if the last house is at index 0.
+            ans += (garbageLastPos[c] == 0 ? 0 : travel[garbageLastPos[c] - 1]);
+        }
+        
+        return ans;
     }
 };
