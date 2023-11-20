474757bce1640c77c9ed336c86f18623ea1ab7a6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-20 23:14:36 +0530

    Time: 185 ms (21.47%), Space: 102.7 MB (13.57%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int n = travel.size();
+        function<int(char)> collectionTime = [&](char c){
+            int currentCost=0;
+            int finalCost = 0;
+            for(int i=0;i<=n;++i){
+                int num_items = std::count(garbage[i].begin(), garbage[i].end(), c);
+                if(num_items){
+                    currentCost+=num_items;
+                    finalCost = currentCost;
+                }
+                if(i<n)
+                    currentCost += travel[i];
+            }
+            return finalCost;
+        };
+        int result = collectionTime('M');
+        result += collectionTime('P');
+        result += collectionTime('G');
+        return result;
+    }
+};
