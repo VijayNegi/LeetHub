94d9e4c7882daa5fbab85e95278a04444346c03e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-23 10:48:53 +0530

    Time: 217 ms (5.19%), Space: 80.9 MB (5.03%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -12,12 +12,51 @@ public:
         
         return true;
     }
+    // sorting
+    vector<bool> checkArithmeticSubarrays1(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> ans;
+        for (int i = 0; i < l.size(); i++) {
+            vector<int> arr(begin(nums) + l[i], begin(nums) + r[i] + 1);
+            ans.push_back(check(arr));
+        }
+        
+        return ans;
+    }
+    // hash
+    bool check_hash(vector<int>& arr) {
+        int minElement = INT_MAX;
+        int maxElement = INT_MIN;
+        unordered_set<int> arrSet;
+        
+        for (int num : arr) {
+            minElement = min(minElement, num);
+            maxElement = max(maxElement, num);
+            arrSet.insert(num);
+        }
+        
+        if ((maxElement - minElement) % (arr.size() - 1) != 0) {
+            return false;
+        }
+        
+        int diff = (maxElement - minElement) / (arr.size() - 1);
+        int curr = minElement + diff;
+        
+        while (curr < maxElement) {
+            if (arrSet.find(curr) == arrSet.end()) {
+                return false;
+            }
+            
+            curr += diff;
+        }
+        
+        return true;
+    }
     
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
         vector<bool> ans;
         for (int i = 0; i < l.size(); i++) {
             vector<int> arr(begin(nums) + l[i], begin(nums) + r[i] + 1);
-            ans.push_back(check(arr));
+            ans.push_back(check_hash(arr));
         }
         
         return ans;
