895495b6673b5e716dc64daa3d415214163277b9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-24 10:43:34 +0530

    Time: 95 ms (46.99%), Space: 57 MB (5.22%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // simulate
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        deque<int> queue;
+        for (int num : piles) {
+            queue.push_back(num);
+        }
+        
+        int ans = 0;
+        while (!queue.empty()) {
+            queue.pop_back(); // alice
+            ans += queue.back(); // us
+            queue.pop_back();
+            queue.pop_front(); // bob
+        }
+        
+        return ans;
+    }
+    // faster
+    
+};
