5145e06cfaa5fd4f50c6d8555008d154ac7b6a80
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-12-06 08:03:24 +0530

    Time: 3 ms (13.68%), Space: 6.2 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int totalMoney(int n) {
+    int totalMoney1(int n) {
         int res=0;
         int week=4;
         while(n){
@@ -16,5 +16,20 @@ public:
             }
         }
         return res;
+    }
+    //O(1)
+    int totalMoney(int n) {
+        int k = n / 7;
+        int F = 28;
+        int L = 28 + (k - 1) * 7;
+        int arithmeticSum = k * (F + L) / 2;
+        
+        int monday = 1 + k;
+        int finalWeek = 0;
+        for (int day = 0; day < n % 7; day++) {
+            finalWeek += monday + day;
+        }
+        
+        return arithmeticSum + finalWeek;
     }
 };
