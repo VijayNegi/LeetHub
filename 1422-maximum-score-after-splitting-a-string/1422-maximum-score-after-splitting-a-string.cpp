4a5277447032191c3e351d55db1ad663d4f2e683
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-12-22 22:19:26 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (87.50%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int maxScore(string s) {
+    int maxScore1(string s) {
         int n = s.size();
         auto sum = [](int ss,char c){
             return ss + c - '0';
         };
         int one = accumulate(begin(s),end(s), 0,sum);
+        // int one = count(s.begin(), s.end(), '1');  // or this works
         int result = 0;
         int zero=0;
         for(int i=0;i<n-1;++i){
@@ -16,5 +17,27 @@ public:
             result = max(result,zero + one);
         }
         return result;
+    }
+    // single pass
+    int maxScore(string s) {
+        int ones = 0;
+        int zeros = 0;
+        int best = INT_MIN;
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+            
+            best = max(best, zeros - ones);
+        }
+        
+        if (s[s.size() - 1] == '1') {
+            ones++;
+        }
+        
+        return best + ones;
     }
 };
