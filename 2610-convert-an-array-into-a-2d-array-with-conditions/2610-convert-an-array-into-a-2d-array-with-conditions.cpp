14376a2c10950ada7e135821507aefdc39d4c8d2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-01-02 20:32:21 +0530

    Time: 42 ms (5.35%), Space: 29.8 MB (92.71%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> findMatrix(vector<int>& nums) {
+    // self
+    vector<vector<int>> findMatrix1(vector<int>& nums) {
         vector<vector<int>> result;
         unordered_map<int,int> count;
         for(auto& n:nums)
@@ -19,5 +20,21 @@ public:
             result.push_back(temp);
         }
         return result;
+    }
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
+        
+        vector<vector<int>> ans;
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.push_back({});
+            }
+            
+            // Store the integer in the list corresponding to its current frequency.
+            ans[freq[c]].push_back(c);
+            freq[c]++;
+        }
+        
+        return ans;
     }
 };
