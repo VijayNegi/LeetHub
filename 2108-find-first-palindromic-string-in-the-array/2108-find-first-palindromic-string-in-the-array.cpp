86b93bb05877c48ac0a910850f05a9712309bec0
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-02-13 22:28:12 +0530

    Time: 48 ms (59.59%), Space: 26.4 MB (44.08%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
+    bool isPalindrome(string& s) {
+        int start = 0;
+        int end = s.size() - 1;
+        
+        while (start <= end) {
+            // Return false if the characters are not the same.
+            if (s[start] != s[end]) {
+                return false;
+            }
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+    
     string firstPalindrome(vector<string>& words) {
         for (string s : words) {
-            string reversed = s;
-            reverse(reversed.begin(), reversed.end());
-            
-            if (s == reversed) {
+            if (isPalindrome(s)) {
                 return s;
             }
         }
+        
         return "";
     }
 };
