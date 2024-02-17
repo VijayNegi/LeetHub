321c46e34360b2eb8da5bd7fda213dfd70b84c9e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-02-17 23:58:23 +0530

    Time: 8 ms (49.57%), Space: 10.9 MB (23.38%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.cpp
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -1,3 +1,4 @@
+/* self
 class H2O {
     std::mutex mu;
     int hCount{0};
@@ -35,5 +36,37 @@ public:
         reset();
         locker.unlock();
         cond1.notify_one();
+    }
+};
+*/
+
+// https://leetcode.com/problems/building-h2o/discuss/1184961/C%2B%2B-solution-with-posix-semaphores-and-barriers
+#include<semaphore.h>
+class H2O {
+public:
+    pthread_barrier_t b;
+    sem_t h;
+    sem_t o;
+    H2O() {
+        pthread_barrier_init(&b, 0, 3); //Need 3 threads to pass the barrier
+        sem_init(&h, 0, 2); // Two molecules of hydrogen
+        sem_init(&o, 0, 1); // One molecule of oxygen
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        
+        sem_wait(&h); // Two hydrogen threads can arrive at a time
+        pthread_barrier_wait(&b); // but wait for one more thread , which must be oxygen
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+        sem_post(&h);
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        sem_wait(&o);  // only one oxygen can arrive
+        pthread_barrier_wait(&b); // but wait for 3 threads at this barrier, 2 of twhich must be hydrogen
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+        sem_post(&o);
     }
 };
