096b20d2408bb4c766dae7e5c0daf02bb75dda83
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-03-13 23:55:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int pivotInteger(int n) {
+    int pivotInteger1(int n) {
         int sum = n*(n+1)/2;
         int prev = 0;
         for(int i=1;i<=n;++i){
@@ -9,5 +9,32 @@ public:
             prev += i;
         }
         return -1;
+    }
+    // math
+    int pivotInteger(int n) {
+        const int sum = (n * (n + 1) / 2);
+        const int pivot = sqrt(sum);
+        // If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+        return pivot * pivot == sum ? pivot : -1;
+  }
+    // binary search
+    int pivotInteger2(int n) {
+    
+        int left = 1, right = n;
+        int totalSum = n * (n + 1) / 2;
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (mid * mid - totalSum < 0) {
+                left = mid + 1; 
+            } else {
+                right = mid;
+            }
+        }
+        if (left * left - totalSum == 0) {
+            return left;
+        } else {
+            return -1;
+        }
     }
 };
