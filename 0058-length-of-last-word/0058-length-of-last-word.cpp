13894379a1cde2e6df3f9427f8d060ec17440155
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-04-01 09:15:40 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (87.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
+    int lengthOfLastWord1(string s) {
         stringstream ss(s);
         string last,w;
         while(getline(ss,w,' ')){
@@ -8,5 +8,20 @@ public:
                 last = w;
         }
         return last.size();
+    }
+    int lengthOfLastWord(string s) { 
+        int len = 0, tail = s.length() - 1;
+        while (tail >= 0 && s[tail] == ' ') tail--;
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+    int lengthOfLastWord2(string s) {  
+        stringstream ss(s);  
+        string temp;  
+        while(ss>>temp){}  
+        return temp.size();  
     }
 };
