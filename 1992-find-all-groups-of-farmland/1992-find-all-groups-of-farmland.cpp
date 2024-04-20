bf9a18d8267523792a0772cc4ff4671747478774
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-04-21 00:48:51 +0530

    Time: 91 ms (89.09%), Space: 59.2 MB (91.64%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    // self
+    vector<vector<int>> findFarmland1(vector<vector<int>>& land) {
         vector<vector<int>> result;
         int rows = land.size();
         int cols = land[0].size();
@@ -20,4 +21,23 @@ public:
         }
         return result;
     }
+    // https://leetcode.com/problems/find-all-groups-of-farmland/discuss/1451820/Mark-Left-Side
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    int m = land.size(), n = land[0].size();
+    vector<vector<int>> res;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j) {
+            if (land[i][j] == 1) {
+                int x = i, y = j;
+                while (y < n && land[i][y])
+                    ++y;                
+                for (; x < m && land[x][j]; ++x)
+                    land[x][j] = y - j + 1;
+                res.push_back({i, j, x - 1, y - 1});
+            }
+            if (land[i][j] > 1)
+                j += land[i][j] - 1;
+        }
+    return res;
+}  
 };
