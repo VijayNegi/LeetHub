97deb665187c9051c7320cbac6ad450857a27fbd
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-04-26 18:22:01 +0530

    Time: 16 ms (99.03%), Space: 16.8 MB (95.32%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& arr) {
-    int fm = 0, sm = 0, pos = -1;
+    int fmin = 0, smin = 0, pos = -1;
     for (auto i = 0; i < arr.size(); ++i) {
-        auto fm2 = INT_MAX, sm2 = INT_MAX, pos2 = -1;
+        auto fmin2 = INT_MAX, smin2 = INT_MAX, pos2 = -1;
         for (auto j = 0; j < arr[i].size(); ++j) {
-            auto mn = j != pos ? fm : sm;
-            if (arr[i][j] + mn < fm2) {
-                sm2 = fm2;
-                fm2 = arr[i][j] + mn;
+            auto mn = j != pos ? fmin : smin;
+            if (arr[i][j] + mn < fmin2) {
+                smin2 = fmin2;
+                fmin2 = arr[i][j] + mn;
                 pos2 = j;
-            } else sm2 = min(sm2, arr[i][j] + mn);
+            } else smin2 = min(smin2, arr[i][j] + mn);
         }
-        fm = fm2, sm = sm2, pos = pos2;
+        fmin = fmin2, smin = smin2, pos = pos2;
     }
-    return fm;
+    return fmin;
 }
 };
