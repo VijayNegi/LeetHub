a1dc179671f5c67e7b83af05ad56463483d451fb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-04-28 13:40:15 +0530

    Time: 280 ms (28.89%), Space: 149.4 MB (5.18%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -11,8 +11,8 @@ public:
             tree[e[0]].insert(e[1]);
             tree[e[1]].insert(e[0]);
         }
-        dfs(0, -1);
-        dfs2(0, -1);
+        dfs(0, -1); // pre Traversal : To collect initial data, childs and solve for first node.
+        dfs2(0, -1); // Post Traversal : Use previously collected data and solve for other nodes.
         return res;
 
     }
@@ -21,15 +21,16 @@ public:
         for (auto i : tree[root]) {
             if (i == pre) continue;
             dfs(i, root);
-            count[root] += count[i];
-            res[root] += res[i] + count[i];
+            count[root] += count[i];   // add childs of i including i.
+            res[root] += res[i] + count[i]; // add distance for all nodes, note: we need to add count as we need to travel same path for all child nodes
         }
     }
 
     void dfs2(int root, int pre) {
         for (auto i : tree[root]) {
             if (i == pre) continue;
-            res[i] = res[root] - count[i] + count.size() - count[i];
+            res[i] = res[root] - count[i] + count.size() - count[i]; // res[root] - count[i] : nodes moving closer too
+                                                                     // N - count[i] : nodes moving 
             dfs2(i, root);
         }
     }
