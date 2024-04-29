d0f51ed44e6ba6023a22fc1a459b546a74b302a5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-04-29 23:01:48 +0530

    Time: 106 ms (71.03%), Space: 91.3 MB (85.86%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
-     int minOperations(vector<int>& A, int k) {
+     int minOperations1(vector<int>& A, int k) {
         for (int a : A)
             k ^= a;
         return __builtin_popcount(k);
+    }
+    int minOperations(vector<int>& n, unsigned int k) 
+    {
+        return popcount(reduce(begin(n), end(n), k, bit_xor<>()));
+    }
+    int minOperations2(vector<int>& A, unsigned int k) 
+    {
+        for (int x: A) {
+            k ^= x;
+        }
+        int res = 0;
+        while (k > 0) {
+            res += (k & 1);
+            k >>= 1;
+        }
+        return res;
     }
 };
