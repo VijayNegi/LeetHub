eba8cd88cf7fe2a4ef76de1e0ec734b64828cc23
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-05-07 10:21:43 +0530

    Time: 177 ms (55.15%), Space: 120.7 MB (87.76%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/discuss/3901771/JavaC%2B%2BPython-One-Pass-no-Reverse-8-lines
+   ListNode* doubleIt(ListNode* head) {
+        if (head->val > 4)
+            head = new ListNode(0, head);
+        for(ListNode* node = head; node; node = node->next) {
+            node->val = (node->val * 2) % 10;
+            if (node->next && node->next->val > 4)
+                node->val++;
+        }
+        return head;
+    }
+};
