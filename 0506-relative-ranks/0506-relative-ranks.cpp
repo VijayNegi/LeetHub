10e8e2b84b64e6a686f5a272b21c47b636f4be26
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-05-08 07:57:11 +0530

    Time: 10 ms (51.87%), Space: 13.4 MB (94.43%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<string> findRelativeRanks(vector<int>& nums) {
+    vector<string> findRelativeRanks1(vector<int>& nums) {
         priority_queue<pair<int,int> > pq;
         for(int i=0;i<nums.size();i++)
         {
@@ -17,5 +17,14 @@ public:
             else res[p.second] = to_string(i+1);
         }
         return res;
+    }
+    vector<string> findRelativeRanks(vector<int>& nums) {
+      vector<short> idx(nums.size());
+      vector<string> res(nums.size());
+      iota(begin(idx), end(idx), 0);
+      sort(begin(idx), end(idx), [&nums](int i, int j) { return nums[i] > nums[j]; });
+      for (auto i = 0; i < nums.size(); ++i)
+        res[idx[i]] = i == 0 ? "Gold Medal" : i == 1 ? "Silver Medal" : i == 2 ? "Bronze Medal" : to_string(i + 1);
+      return res;
     }
 };
