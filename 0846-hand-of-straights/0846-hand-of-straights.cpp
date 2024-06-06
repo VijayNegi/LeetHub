a2b41c82b91df54dea2a7ecaf6be36a48a6c618e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-06-07 00:50:01 +0530

    Time: 55 ms (71.36%), Space: 30.5 MB (78.12%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
+    // nlogn
+    bool isNStraightHand1(vector<int>& hand, int groupSize) {
         int handSize = hand.size();
 
         if (handSize % groupSize != 0) {
@@ -28,6 +29,44 @@ public:
                     // Remove the card value if its occurrences are exhausted
                     cardCount.erase(currentCard + i);
                 }
+            }
+        }
+
+        return true;
+    }
+    // O(n)
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+
+        // Map to store the count of each card value
+        unordered_map<int, int> cardCount;
+        for (int card : hand) {
+            cardCount[card]++;
+        }
+
+        for (int card : hand) {
+            int startCard = card;
+            // Find the start of the potential straight sequence
+            while (cardCount[startCard - 1]) {
+                startCard--;
+            }
+
+            // Process the sequence starting from startCard
+            while (startCard <= card) {
+                while (cardCount[startCard]) {
+                    // Check if we can form a consecutive sequence of
+                    // groupSize cards
+                    for (int nextCard = startCard;
+                         nextCard < startCard + groupSize; nextCard++) {
+                        if (!cardCount[nextCard]) {
+                            return false;
+                        }
+                        cardCount[nextCard]--;
+                    }
+                }
+                startCard++;
             }
         }
 
