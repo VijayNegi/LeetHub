45c4f5567d73e1d964b26e23256a7039132407c6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-06-08 18:07:10 +0530

    Time: 201 ms (23.26%), Space: 134.7 MB (59.36%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
-    // self
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_set<int> seen;
-        long sum=0;
-        long pre=0;
-        for(auto& n:nums){
-            sum += n;
-            sum %= k;
-            if(seen.count(sum))
-                return true;
-            seen.insert(pre);
-            pre = sum;
-        }
-        return false;
-    }
-    bool checkSubarraySum1(vector<int>& nums, int k) {
-        int n= nums.size();
-        unordered_map<int,int> mp;
-        mp[0]=1;
-        for(int i=1;i<n;++i)
-        {
-            if(i>=2)
-                mp[nums[i-2]%k]=1;
-            nums[i] += nums[i-1];
-            int m = nums[i]%k;
-            if(mp[m]==1)
-                return true;
-        }
-        return false;
-    }
-    // bruteforce : TLE
-    bool checkSubarraySum2(vector<int>& nums, int k) {
-        vector<int> sums = nums;
-        int n= nums.size();
-        int m=1;
-        while(m<n)
-        {
-            for(int i=0;i<n-m;++i)
-            {
-                sums[i] += nums[i+m];
-                if(sums[i]%k==0)
-                    return true;
+        int prefixMod = 0;
+        unordered_map<int, int> modSeen;
+        modSeen[0] = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefixMod = (prefixMod + nums[i]) % k;
+
+            if (modSeen.find(prefixMod) != modSeen.end()) {
+                // ensures that the size of subarray is atleast 2
+                if (i - modSeen[prefixMod] > 1) {
+                    return 1;
+                }
+            } else {
+                // mark the value of prefixMod with the current index.
+                modSeen[prefixMod] = i;
             }
-            ++m;
         }
-        return false;
+
+        return 0;
     }
 };
